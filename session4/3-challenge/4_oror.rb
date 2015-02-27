4cc4947e0c293e4d15fd0d10e1775b09a355092c
Jordan Poulton <jordan@oxfordentrepreneurs.co.uk>
2015-02-27 15:47:31 +0000

    Update 4_oror.rb

--- session4/3-challenge/4_oror.rb
+++ session4/3-challenge/4_oror.rb
@@ -1,3 +1,5 @@
+# If you struggle on this question for ~30 minutes and aren't getting anywhere, look at the solutions file, try to understand the code, then close the file, come back here, and try again to solve it.
+
 # Write a method first_object, that takes three parameters
 # and returns the first one that is true.
 #
@@ -9,5 +11,5 @@
 # first_object nil, nil, nil  # => nil
 
 def first_object(arg1, arg2, arg3)
-  arg1 || arg2 || arg3 || nil
+  
 end
