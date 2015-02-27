facd78bd91066fd50dd25015f01544d0d5da8649
Jordan Poulton <jordan@oxfordentrepreneurs.co.uk>
2015-02-27 15:46:55 +0000

    Update 2_subclassing_require.rb

--- session4/3-challenge/2_subclassing_require.rb
+++ session4/3-challenge/2_subclassing_require.rb
@@ -1,3 +1,5 @@
+# If you struggle on this question for ~30 minutes and aren't getting anywhere, look at the solutions file, try to understand the code, then close the file, come back here, and try again to solve it.
+
 # Require the stack code from challenge 1
 # The code you added causes it to do this
 #
