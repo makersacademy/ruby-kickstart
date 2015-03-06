e93f93ebb23de74f2888532dfa52e2fff6448280
sanjsanj <sanjsanj@hotmail.com>
2015-03-06 14:55:50 +0000

    Complete upto 1:6

--- session1/3-challenge/6_string.rb
+++ session1/3-challenge/6_string.rb
@@ -6,5 +6,16 @@
 #
 
 def odds_and_evens(string, return_odds)
-
+  mystring = ""
+  x = 0
+  if return_odds
+    string.size.times do |i|
+      i.even? ? next : mystring << string[i]
+    end
+  else
+    string.size.times do |i|
+      i.odd? ? next : mystring << string[i]
+    end
+  end
+  mystring
 end
