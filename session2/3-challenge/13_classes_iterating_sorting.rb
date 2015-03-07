c640ba0c468251d27bf8ff11fd613a69dc08e8ed
sanjsanj <sanjsanj@hotmail.com>
2015-03-07 18:10:01 +0000

    Complete 2:13, 2:11 left to do

--- session2/3-challenge/13_classes_iterating_sorting.rb
+++ session2/3-challenge/13_classes_iterating_sorting.rb
@@ -69,17 +69,22 @@ require 'date'
 
 class User
   attr_accessor :username
+  attr_accessor :blogs
   
   def initialize(username)
-    @username = username
+    self.username = username
+    self.blogs = []
   end
   
   def add_blog(date, text)
-    
+    blogentry = Blog.new(date, self, text)
+    blogs << blogentry
+    @blogs = blogs.sort_by {|blog| blog.date }.reverse
+    blogentry
   end
   
   def blogs
-    
+    @blogs
   end
 end
 
@@ -87,5 +92,23 @@ class Blog
   attr_accessor :text
   attr_accessor :date
   attr_accessor :user
+  
+  def initialize(date, user, text)
+    self.date = date
+    self.user = user
+    self.text = text
+  end
+  
+  def summary
+    self.text.split(/\s+/, 11)[0...10].join(' ')
+  end
+  
+  def entry
+    "#{user.username} #{date}\n#{text}"
+  end
+  
+  def ==(other)
+    return self.date == other.date && self.user == other.user && self.text == other.text
+  end
   
 end
