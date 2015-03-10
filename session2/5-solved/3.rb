af31d2467267394de3dff8a2841df6a88a8595b9
James Miller <james.fl.miller@live.co.uk>
2015-03-10 16:45:18 +0000

    Removed the original solution files to leave only my own

--- session2/5-solved/3.rb
+++
@@ -1,9 +0,0 @@
-class String
-  def every_other_char
-    to_return = ''
-    each_char.with_index do |char, index|
-      to_return << char if index.even?
-    end
-    to_return
-  end
-end
--- session2/5-solved/5.rb
+++
@@ -1,3 +0,0 @@
-def mod_three(numbers)
-  numbers.select { |number| number % 3 != 0 }.map { |number| number % 3 }
-end
