4636b485bac598571eae5768b18989444ee50039
paulwallis42 <paulwallis42@gmail.com>
2015-03-15 16:16:57 +0000

    ses1_ses2

--- session2/3-challenge/2_input_output_control.rb
+++ session2/3-challenge/2_input_output_control.rb
@@ -1,34 +1,19 @@
-# Prompt the user for a number, then read it in and print out "hi" that many times
-
-# Repeat this process until the user submits "bye", then say "goodbye" and end the program
-# HINT: Check out example 2 if you get stuck
-
-# example:
-# PROGRAM: Enter a number
-# USER:    4
-# PROGRAM: hi hi hi hi
-# PROGRAM: Enter a number
-# USER:    2
-# PROGRAM: hi hi
-# PROGRAM: Enter a number
-# USER:    bye
-# PROGRAM: goodbye
-
-
-# remember you can try your program out with              $ ruby 2_input_output_control.rb
-# and when you think it is correct, you can test it with  $ rake 2:2
-
 def prompt
-  puts 'Enter a number or bye:'
+  puts 'Enter a number or bye'
 end
 
 def hi_hi_goodbye
   prompt
-  while (line = gets) && (line != /bye/)
-    line.to_i.times { puts 'hi'}
-    puts prompt
+  while (line = gets) && (line !~ /bye/) # first is an assignment statement that returns a line or nil, and is thus boolean
+    line.to_i.times { print 'hi ' }
+    puts
+    prompt
   end
-  puts 'Goodbye!'
+  puts "Goodbye!"
 end
 
-hi_hi_goodbye if $0 == __FILE__
+
+
+
+
+hi_hi_goodbye if $0 == __FILE__  # a little magic so that you can run with "$ ruby 2_input_output_control.rb" but it will still work for our tests
--- session2/3-challenge/7_array.rb
+++ session2/3-challenge/7_array.rb
@@ -1,27 +1,12 @@
-
-
-
-
-
-# Given a sentence, return an array containing every other word.
-# Punctuation is not part of the word unless it is a contraction.
-# In order to not have to write an actual language parser, there won't be any punctuation too complex.
-# There will be no "'" that is not part of a contraction.
-# Assume each of these  charactsrs are not to be considered: ! @ $ # % ^ & * ( ) - = _ + [ ] : ; , . / < > ? \ |
-#
-# Examples
-# alternate_words("Lorem ipsum dolor sit amet.")  # => ["Lorem", "dolor", "amet"]
-# alternate_words("Can't we all get along?")      # => ["Can't", "all", "along"]
-# alternate_words("Elementary, my dear Watson!")  # => ["Elementary", "dear"]
-
-
-def alternate_words(string)
-
-new_string = string.split
-
-newer_string = new_string.map { |x| x.gsub(/[^0-9a-z']/i, '') }
-
-newer_string.values_at(* newer_string.each_index.select {|i| i.even?})
-
+def alternate_words(sentence)
+  # this will get better when we learn regular expressions :)
+  '!@$#%^&*()-=_+[]:;,./<>?\\|'.split(//).each do |char|
+    sentence = sentence.gsub(char, ' ')
+  end
+  words = sentence.split
+  solution = []
+  words.each_with_index do |word, index|
+    solution << word if index.even?
+  end
+  solution
 end
-# Works apart from the . . . in one of the test examples in rake
