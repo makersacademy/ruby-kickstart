dd7728da9710985e3d1b50fc05c1dcc7084be936
MollieS <mollie.c.stephenson@gmail.com>
2015-04-15 14:13:09 +0100

    session 1 challenges completed

--- session1/3-challenge/7_string.rb
+++ session1/3-challenge/7_string.rb
@@ -5,28 +5,20 @@
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
 def pirates_say_arrrrrrrrr(string)
-	returns = ""
+	returns = []
 	#makes an empty string for returned letters
 	letters = string.split''
 	#splits the given string into an array
-	next_letter = nil
 	letters.each_with_index do |l, i|
 		if (l == 'r' || l == 'R')
 			next_letter = letters[i+1]
 			returns << next_letter
-			
-			
 		end
-
-		end
-
-		puts returns
-
-	
-
-	
+	end
+	output = returns.join('')
+	return output
 end
 
 pirates_say_arrrrrrrrr("are you really learning Ruby?")
 pirates_say_arrrrrrrrr("Katy Perry is on the radio!")
-pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr.") 
+pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr") 
