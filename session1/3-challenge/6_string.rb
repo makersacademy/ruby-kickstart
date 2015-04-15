6c135198f2b8b4a1d265337d4201f4a801680c35
MollieS <mollie.c.stephenson@gmail.com>
2015-04-15 12:17:02 +0100

    exercise 6 completed

--- session1/3-challenge/6_string.rb
+++ session1/3-challenge/6_string.rb
@@ -6,5 +6,20 @@
 #
 
 def odds_and_evens(string, return_odds)
-
+	newstring = ""
+	#create new string to hold return of method
+	string.length.times do |i|
+		#finds length of string and to each part of that length (index) does the following...
+		next if i.even? && return_odds == true
+		#next iterates through the string to find even numbers if the boolean is true
+		next if i.odd? && return_odds == false
+		#if boolean is false, next finds odd numbers
+		newstring << string[i]
+		#pushs them to the string
+	end
+	return newstring
 end
+	
+
+odds_and_evens("abcdefg", true)
+odds_and_evens("willthiswork", false)
