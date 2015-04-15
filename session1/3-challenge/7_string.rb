b95d018ca6b8d12bb3182a5f17743010638cca3a
MollieS <mollie.c.stephenson@gmail.com>
2015-04-15 12:44:10 +0100

    exercise 7 completed

--- session1/3-challenge/7_string.rb
+++ session1/3-challenge/7_string.rb
@@ -5,5 +5,17 @@
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
 def pirates_say_arrrrrrrrr(string)
+	rstring = ""
+	isitr = nil
+	string.length.times do |i|
+		letters = string[i]
+		rstring << letters if isitr	
+		isitr = (letters =='r' || letters == 'R')
+	end 
 
+return rstring
 end
+
+pirates_say_arrrrrrrrr("are you really learning Ruby?")
+pirates_say_arrrrrrrrr("Katy Perry is on the radio!")
+pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")  
