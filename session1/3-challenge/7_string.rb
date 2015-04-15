2af86ae830a92229012f6c5f32d9b38541bae230
MollieS <mollie.c.stephenson@gmail.com>
2015-04-15 14:01:22 +0100

    almost working exercise 7

--- session1/3-challenge/7_string.rb
+++ session1/3-challenge/7_string.rb
@@ -5,17 +5,28 @@
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
 def pirates_say_arrrrrrrrr(string)
-	rstring = ""
-	isitr = nil
-	string.length.times do |i|
-		letters = string[i]
-		rstring << letters if isitr	
-		isitr = (letters =='r' || letters == 'R')
-	end 
+	returns = ""
+	#makes an empty string for returned letters
+	letters = string.split''
+	#splits the given string into an array
+	next_letter = nil
+	letters.each_with_index do |l, i|
+		if (l == 'r' || l == 'R')
+			next_letter = letters[i+1]
+			returns << next_letter
+			
+			
+		end
 
-return rstring
+		end
+
+		puts returns
+
+	
+
+	
 end
 
 pirates_say_arrrrrrrrr("are you really learning Ruby?")
 pirates_say_arrrrrrrrr("Katy Perry is on the radio!")
-pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")  
+pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr.") 
