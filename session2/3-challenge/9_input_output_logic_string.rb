b73f5039b58c99e795f26446c68117edad2cc958
MollieS <mollie.c.stephenson@gmail.com>
2015-04-17 13:21:59 +0100

    exercise 9 completed

--- session2/3-challenge/9_input_output_logic_string.rb
+++ session2/3-challenge/9_input_output_logic_string.rb
@@ -24,6 +24,16 @@
 # USER:     BYE
 
 def deaf_grandma
+	input = gets.chomp
+	while input != "BYE"
+		if (input == input.upcase) && (input != "BYE") && (input != "")
+			puts "NO, NOT SINCE 1938!"
+		else 
+			puts "HUH?! SPEAK UP, SONNY!"
+		end
+		input = gets.chomp
+	end
+
 
 end
 
