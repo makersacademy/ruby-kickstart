1328894a5e89f2bb9456a33f6062bdbcac98f1a4
Ashleigh Maund <ashleighmaund@gmail.com>
2015-04-22 15:55:57 +0100

    Update 3_array.rb

--- session2/3-challenge/3_array.rb
+++ session2/3-challenge/3_array.rb
@@ -7,6 +7,27 @@
 
 class String
   def every_other_char
+<<<<<<< HEAD
+  	out = ""
+  	each_char.each_with_index.select do |c,i|
+  	  if i.even? 
+  	    out << c #adds the 
+  	  end
+    end
+    puts out #outputs the resulting array with every other character
+  end
+end
+
+
+"abcdefg".every_other_char
+"".every_other_char
+"Haeblcldoe ftghheirjek!l".every_other_char
+
+
+
+
+=======
   end
   
 end
+>>>>>>> c6e3db68b1696c9b1ce9890d9a36cc8d971f72ab
