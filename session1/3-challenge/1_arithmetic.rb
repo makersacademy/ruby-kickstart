a7a0d949d20c0f1a58ca79bce3d1923773df188f
Ashleigh Maund <ashleighmaund@gmail.com>
2015-04-22 15:28:16 +0100

    Update 1_arithmetic.rb

--- session1/3-challenge/1_arithmetic.rb
+++ session1/3-challenge/1_arithmetic.rb
@@ -9,5 +9,14 @@
 # arithmetic1(-6)     # => -50
 
 def arithmetic1(n)
+<<<<<<< HEAD
+  puts "#{(n*5)-20}"
 end
 
+arithmetic1(10)
+arithmetic1(10.5)
+arithmetic1(-6)
+=======
+end
+
+>>>>>>> c6e3db68b1696c9b1ce9890d9a36cc8d971f72ab
