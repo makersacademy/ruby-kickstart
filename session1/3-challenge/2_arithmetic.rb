6ffa62c5f9bce3b38ddafeb6c9764941fd4de3ab
Ashleigh Maund <ashleighmaund@gmail.com>
2015-04-22 15:32:21 +0100

    Update 2_arithmetic.rb

--- session1/3-challenge/2_arithmetic.rb
+++ session1/3-challenge/2_arithmetic.rb
@@ -9,4 +9,14 @@
 # arithmetic2(-6, -7)  # => -3.5
 
 def arithmetic2(a, b)
+<<<<<<< HEAD
+  lowest = [a.to_f,b.to_f].min
+  puts "#{(lowest/2)}"
 end
+
+arithmetic2(1,2)
+arithmetic2(19,10)
+arithmetic2(-6,-7)
+=======
+end
+>>>>>>> c6e3db68b1696c9b1ce9890d9a36cc8d971f72ab
