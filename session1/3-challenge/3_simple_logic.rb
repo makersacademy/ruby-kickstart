c631e2246f7dd4efe2d5f62afe3bf86e0978db36
Ashleigh Maund <ashleighmaund@gmail.com>
2015-04-22 15:32:58 +0100

    Update 3_simple_logic.rb

--- session1/3-challenge/3_simple_logic.rb
+++ session1/3-challenge/3_simple_logic.rb
@@ -8,4 +8,17 @@
 # ten_twenty(6) # => 10
 
 def ten_twenty(n)
+<<<<<<< HEAD
+  if
+    n % 2 == 0
+    puts "20"
+  else
+    puts "10"
+  end
 end
+
+ten_twenty(5)
+ten_twenty(6)
+=======
+end
+>>>>>>> c6e3db68b1696c9b1ce9890d9a36cc8d971f72ab
