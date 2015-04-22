4593d276074556ddf0a9367d3c9ad53eed4a91ed
Ashleigh Maund <ashleighmaund@gmail.com>
2015-04-22 16:03:11 +0100

    Update 10_classes.rb

--- session2/3-challenge/10_classes.rb
+++ session2/3-challenge/10_classes.rb
@@ -16,5 +16,37 @@
 #
 
 class Person
+<<<<<<< HEAD
+  
+  attr_accessor 'name', 'age'
+  
+  def initialize(name, age)
+    @name = name
+    @age = age
+  end
+  
+  def birthday
+    @age += 1
+  end
 
 end
+
+ash = Person.new 'Ash', 24
+
+puts ash.name
+puts ash.age
+
+ash.name = 'Ashleigh'
+
+puts ash.name
+
+ash.birthday
+puts ash.age
+
+ash.birthday
+puts ash.age
+
+=======
+
+end
+>>>>>>> c6e3db68b1696c9b1ce9890d9a36cc8d971f72ab
