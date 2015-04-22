212e539c9228eb346e6b85530ee700e8f9a62cbd
Ashleigh Maund <ashleighmaund@gmail.com>
2015-04-22 15:54:14 +0100

    Update 2_input_output_control.rb

--- session2/3-challenge/2_input_output_control.rb
+++ session2/3-challenge/2_input_output_control.rb
@@ -18,15 +18,34 @@
 # remember you can try your program out with              $ ruby 2_input_output_control.rb
 # and when you think it is correct, you can test it with  $ rake 2:2
 
+<<<<<<< HEAD
+puts "Hi! Please input a whole number\:"
+
+while number = gets.chomp
+  def hi_hi_goodbye(number)
+    number_not_string = number.to_i
+    puts "hi "*number_not_string
+  end
+    hi_hi_goodbye(number)
+    puts "Please enter another number, or \"bye\" if you want to exit\:"
+break if number.chomp == "bye"
+end
+  puts "goodbye" 
+=======
 def hi_hi_goodbye
   # your code here
 
 end
 
+>>>>>>> c6e3db68b1696c9b1ce9890d9a36cc8d971f72ab
 
 
 
 # This will just invoke the method if you run this program directly
 # This way you can try it out by running "$ ruby 2_input_output_control.rb"
 # but it will still work for our tests
+<<<<<<< HEAD
+#hi_hi_goodbye if $0 == __FILE__
+=======
 hi_hi_goodbye if $0 == __FILE__
+>>>>>>> c6e3db68b1696c9b1ce9890d9a36cc8d971f72ab
