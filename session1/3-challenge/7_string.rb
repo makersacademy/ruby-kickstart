1cba670dafb903e962c8ba7451077a38725e2cc9
HowieDL <howiedl@yahoo.co.uk>
2015-05-05 21:37:10 +0100

    Answers to Session 1

--- session1/3-challenge/7_string.rb
+++ session1/3-challenge/7_string.rb
@@ -5,5 +5,18 @@
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
 def pirates_say_arrrrrrrrr(string)
+  # This is as far as I got before checking the answer
+  # to_return ""
+  # string.each_char do |c|
+  #   puts next if c == "r" || c == "R"
+  # end
+  to_return = ""
+  add_next = false
+  string.each_char do |c| # kept my .each_char method & c instead of index - still works
+    current_char = string[c]
+    to_return << current_char if add_next
+    add_next = (current_char == "r" || current_char == "R")
+  end
+  to_return
 
 end
