ce745d3006029e51a0eb4a66a6ab988faffdf785
Simon Legg <leggsimon@gmail.com>
2015-05-08 22:16:40 +0100

    Updated Session 1 - Challenge 7 answer

--- session1/3-challenge/7_string.rb
+++ session1/3-challenge/7_string.rb
@@ -6,18 +6,18 @@
 
 def pirates_say_arrrrrrrrr(string)
   arr = string.split(//)
-  to_return = ""
-   arr.each do |r|
-     if r == /r/i
-       match = arr.index(r)
-      to_return << arr[match+1].to_s
+    to_return = []
+
+    arr.each_with_index do |x, i|
+      if x == "r" or x == "R"
+        to_return << arr[i+1]
      else
-       puts "No 'r's found!"
+      "No 'r's found!"
      end
    end
-
+  return to_return.join.to_s
 end
 
-puts pirates_say_arrrrrrrrr("are you really learning Ruby?") # => "eenu"
-puts pirates_say_arrrrrrrrr("Katy Perry is on the radio!")   # => "rya"
-puts pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
+pirates_say_arrrrrrrrr("are you really learning Ruby?") # => "eenu"
+pirates_say_arrrrrrrrr("Katy Perry is on the radio!")   # => "rya"
+pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
