92431653db3819e5847ebed7a0368a313b4881f0
Winifred Au <winifredau1990@gmail.com>
2015-07-09 12:42:14 -0400

    added 6_string.rb

--- session1/3-challenge/7_string.rb
+++ session1/3-challenge/7_string.rb
@@ -5,5 +5,11 @@
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
 def pirates_say_arrrrrrrrr(string)
-
+  to_return = ""
+  string.size.times do |index|
+    next if string[index] != "r"
+      to_return << string[index]
+    end
+    to_return
 end
+puts pirates_say_arrrrrrrrr("What are you making me do here?")
