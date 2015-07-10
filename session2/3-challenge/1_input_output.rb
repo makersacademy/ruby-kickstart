55197e3d7152b06cdfca1328f761bd7cd01772db
giuseppe de santis <giusdesan@gmail.com>
2015-07-10 12:06:43 +0200

    updated code for 1_input_output.rb

--- session2/3-challenge/1_input_output.rb
+++ session2/3-challenge/1_input_output.rb
@@ -9,8 +9,8 @@
 
 def sum_difference_product
   # your code goes here
-  x, y = gets.split.map {|num| num.to_i}
-  puts x + y
-  puts x - y
-  puts x * y
+  integers = gets.split.map {|num| num.to_i}
+  puts integers[0] + integers[1]
+  puts integers[0] - integers[1]
+  puts integers[0] * integers[1]
 end
