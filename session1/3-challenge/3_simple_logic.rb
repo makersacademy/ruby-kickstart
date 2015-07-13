0a84647f842d0969f8febc87d00865a2fc6e12e3
zlahham <zlahham@gmail.com>
2015-07-13 15:24:18 +0100

    added session 1 - Completed the challenges

--- session1/3-challenge/3_simple_logic.rb
+++ session1/3-challenge/3_simple_logic.rb
@@ -8,4 +8,9 @@
 # ten_twenty(6) # => 10
 
 def ten_twenty(n)
+  if n.odd?
+    20
+  else
+    10
+  end
 end
--- session1/3-challenge/4_logic.rb
+++ session1/3-challenge/4_logic.rb
@@ -13,6 +13,18 @@
 
 
 def grade(num_books, has_read_books)
+  if has_read_books
+    return "C" if num_books < 10
+    return "B" if num_books <=20
+    return "A" if num_books > 20
+  else
+    return "D" if num_books < 10
+    return "C" if num_books <=20
+    return "B" if num_books > 20
+  end
 end
 
 
+
+grade(15, true)
+grade(0, false)
