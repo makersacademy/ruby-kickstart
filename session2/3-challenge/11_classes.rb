d477e3caa06cf32ef9900b8ac05f37732ccfbd9d
Antonio Voce <antonio.v0c3@gmail.com>
2015-07-14 14:39:37 +0100

    complete challenge 2:11

--- session2/3-challenge/11_classes.rb
+++ session2/3-challenge/11_classes.rb
@@ -21,22 +21,53 @@
 
 class BeerSong
   def initialize(starting_bottles)
+    @bottles = starting_bottles
     @bottles = 0 if starting_bottles < 0
     @bottles = 99 if starting_bottles > 99
-    @bottles = starting_bottles
+
+  end
+  def print_song
+    while @bottles >= 2 do
+      initial_number = number_to_human(@bottles).capitalize
+      reduced_number = number_to_human(@bottles - 1).capitalize
+      puts "#{initial_number} bottles of beer on the wall,"
+      puts "#{initial_number} bottles of beer,"
+      puts "Take one down, pass it around,"
+      if (@bottles - 1) > 1
+        puts "#{reduced_number} bottles of beer on the wall."
+      else
+        puts "One bottle of beer on the wall."
+      end
+
+      @bottles -= 1
+    end
+    if @bottles == 1
+      puts "One bottle of beer on the wall,"
+      puts "One bottle of beer,"
+      puts "Take one down, pass it around,"
+      puts "Zero bottles of beer on the wall."
+    end
   end
 end
 
-def print_song
-  while bottles > 2 do
-    puts "#{@bottles} of beer on the wall, #{@bottles} of beer. Take one down and pass it around, #{@bottles - 1} of beer on the wall."
-    bottles -= 1
-  end
-  puts "One bottle of beer on the wall, one bottle of beer.
-    Take one down and pass it around, one bottle of beer on the wall."
+def number_to_human(number)
 
-  puts "No more bottles of beer on the wall, no more bottles of beer.
-    Go to the store and buy some more, ninety-nine bottles of beer on the wall."
+  numbers = { 1 => "one", 2 => "two", 3 => "three", 4 => "four", 5 => "five", 6 => "six", 7 => "seven", 8 => "eight", 9 => "nine", 10 => "ten", 11 => "eleven", 12 => "twelve", 13 => "thirteen", 14 => "fourteen", 15 => "fifteen", 16 => "sixteen", 17 => "seventeen", 18 => "eighteen", 19 => "nineteen", 20 => "twenty" , 20 => "twenty", 30 => "thirty", 40 => "forty", 50 => "fifty", 60 => "sixty", 70 => "seventy", 80 => "eighty", 90 => "ninety" }
+
+  return numbers[number] if numbers.include?(number)
+
+  number_of_ones = number % 10
+  number_of_tens = number - number_of_ones
+
+  "#{numbers[number_of_tens]}-#{numbers[number_of_ones]}"
 end
+
+
+
+song = BeerSong.new 2
+song.print_song
+
+
+
 
 
