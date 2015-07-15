8a4c1611eb657d96e0665eb36a09f789e551529c
Toby Clarke <teeohbee@gmail.com>
2015-07-15 21:22:28 +0100

    Update challenge 2.6 to 2.11

--- session2/3-challenge/7_array.rb
+++ session2/3-challenge/7_array.rb
@@ -9,3 +9,28 @@
 # alternate_words("Can't we all get along?")      # => ["Can't", "all", "along"]
 # alternate_words("Elementary, my dear Watson!")  # => ["Elementary", "dear"]
 
+def alternate_words (sentence)
+
+  no_punct = sentence.gsub(/-/, " ")  # Changes hyphenated words into two words"
+  no_punct = no_punct.gsub(/[!@#$%^&*()=_+|;:",.<>?]/, "") # Removes all other punctuation
+
+  array = no_punct.split # Splits the words into an array
+
+  result = [] # Defines the variable where the final array will be
+
+  array.each_with_index do |value, index| # Take the value and index of each entry in array
+    if index % 2 == 0 # Check to see if even, eg. takes every other item
+      result.push(value) # Pushes that item to the results array
+    end
+  end
+
+p result
+
+end
+
+# alternate_words("This isn't a sentence yeah? Thanks")
+# alternate_words("Lorem ipsum dolor sit amet.")  # => ["Lorem", "dolor", "amet"]
+# alternate_words("Can't we all get along?")      # => ["Can't", "all", "along"]
+# alternate_words("Elementary, my dear Watson!")  # => ["Elementary", "dear"]
+# alternate_words "If you want to accomplish something in the world, idealism is not enough--you need to choose a method that works to achieve the goal. In other words, you need to be pragmatic."
+# alternate_words "If in my lifetime the problem of non-free software is solved, I could perhaps relax and write software again. But I might instead try to help deal with the world's larger problems. Standing up to an evil system is exhilarating, and now I have a taste for it."
