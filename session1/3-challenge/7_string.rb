1f2a85a8e33cf343bf0c7412c66977e7fa9a613a
TJQKAs <a26052013@gmail.com>
2015-08-17 19:31:27 +0000

    update

--- session1/3-challenge/7_string.rb
+++ session1/3-challenge/7_string.rb
@@ -5,5 +5,12 @@
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
 def pirates_say_arrrrrrrrr(string)
-
+    to_return = ""               
+  string.size.times do |index|                                              #size times we checkout our string 
+    current_char = string[index]                                            #first time - current_char = index[0]
+    next_char = string[index + 1] || ""                                     #next_char = index[1]  - cycle repeats size times.
+    to_return << next_char if (current_char == "r" || current_char == "R")  #we add next_char to our result if the previous char 
+  end                                                                       # is "r" or "R" 
+  to_return                                                                 # the result which we output                          
 end
+
