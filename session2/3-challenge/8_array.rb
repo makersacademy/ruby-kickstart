9d3fc9a2b63bb8b238b186357e2fc151578ae995
TJQKAs <a26052013@gmail.com>
2015-08-18 19:27:55 +0000

    update8

--- session2/3-challenge/8_array.rb
+++ session2/3-challenge/8_array.rb
@@ -6,3 +6,7 @@
 # got_three? ['a', 'a', 'a']  # => true
 # got_three? [1, 2, 1, 1]     # => false
 
+ def got_three? array
+   array.each.with_index{|x,i| return true if (x == array[i+1] && x == array[i+2])} # compare each element with current, current+1, current+2, if all 3 elements are equal return true
+ return false
+ end
