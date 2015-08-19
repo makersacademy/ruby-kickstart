263413d91015e96b8653a325085ea8679e1013c4
Harry Venables <venablesh@yahoo.co.uk>
2015-08-19 19:35:07 +0100

    edited challenge 6

--- session1/3-challenge/6_string.rb
+++ session1/3-challenge/6_string.rb
@@ -6,5 +6,11 @@
 #
 
 def odds_and_evens(string, return_odds)
-
+	if return_odds % 2 != 0
+		puts string[1,3,5,7,9]
+	else
+		puts string[2,4,6,8,10]
+	end
 end
+
+odds_and_evens("My name is harry", false)
