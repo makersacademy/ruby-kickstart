5ff9cd3b0911080771c285d7b9d87eeec6141ac5
Dom Batten <dominic.batten@googlemail.com>
2015-08-20 12:34:07 +0100

    Updated challenge 9

--- session3/3-challenge/9_hashes.rb
+++ session3/3-challenge/9_hashes.rb
@@ -28,4 +28,30 @@
 # shared [1,2,:c], ['a','b',:c]      # => [{1=>[true, nil], 2=>[true, nil], :c=>[true, true], "a"=>[nil, true], "b"=>[nil, true]}, [:c]]
 # shared [1,2,3], [3,2,1]            # => [{1=>[true, true], 2=>[true, true], 3=>[true, true]}, [1, 2, 3]]
 
+def shared (a, b)
+	toReturn = {}
 
+	a.each do |element|
+		toReturn[element] ||= [nil, nil]
+		toReturn[element][0] = true 
+	end
+
+	b.each do |element|
+		toReturn[element] ||= [nil, nil]
+		toReturn[element][1] = true
+	end
+
+	return toReturn, toReturn.keys.sort
+
+end
+		
+
+
+
+
+
+
+
+
+
+	
