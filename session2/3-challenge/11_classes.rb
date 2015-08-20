4c7e11aa87a0c22d27beca991d8e014a0e7b46a8
Dom Batten <dominic.batten@googlemail.com>
2015-08-20 10:39:33 +0100

    Updated challenge 11

--- session2/3-challenge/11_classes.rb
+++ session2/3-challenge/11_classes.rb
@@ -30,7 +30,38 @@ class BeerSong
 		self.bottles = bottles	
 	end
 
+	def print_song
+		bottles.downto 1 do |i|
+			print_stanza i
+		end
+	end
 
+	def print_stanza(n)
+		if n.zero?
+			String.new
+		else
+			puts "#{translate n} #{bottle n} of beer on the wall,"        ,
+           "#{translate n} #{bottle n} of beer,"                    ,
+           "Take one down, pass it around,"                         ,
+           "#{translate n - 1} #{bottle n-1} of beer on the wall."
+       end
+	end
+
+	# returns "bottle" or "bottles"
+  	def bottle(n)
+  		if n == 1 then 'bottle' else 'bottles' end
+  	end
+
+  	# translates number to English
+  	def translate(n)
+    if 0 <= n && n <= 19
+      %w(zero one two three four five six seven eight nine ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen)[n]
+    elsif n % 10 == 0
+      %w(zero ten twenty thirty forty fifty sixty seventy eighty ninety)[n/10]
+    else
+      "#{translate n/10*10}-#{translate n%10}".downcase
+    end.capitalize
+  end
 end
 
-#struggling with this one. had a look at the solution and most of it makes sense
+
