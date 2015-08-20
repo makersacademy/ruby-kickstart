e43f54604a79ab93e6a03741ca88c217a82252d5
zsid <zhivko.siderov@gmail.com>
2015-08-20 15:30:18 +0100

    Some progress

--- session2/3-challenge/11_classes.rb
+++ session2/3-challenge/11_classes.rb
@@ -20,3 +20,41 @@
 # if the parameter is greater than 99, set the number of beer bottles to 99
 # Then make a public method called print_song that outputs all stanzas from the number of bottles of beer down to zero.
 # Add any additional methods you find helpful.
+
+module Words
+	BIGGER = {9 => "ninety-", 8 => "eighty-", 7 => "seventy-", 6 => "sixty-"
+							5 => "fifty", 4 => "forty-", 3 => "thirty", 2 => "twenty" }
+	BIG = { 19 => "ninteen", 18 => "eighteen", 17 => "seventeen", 16 => "sixteen",
+					15 => "fifteen", 14 => "forteen", 13 => "thirteen", 12 => "twelve", 11 => "eleven"}
+	SMALL = { 10 => "ten", 9 => "nine", 8 => "eight", 7 => "seven", 6 => "six", 5 => "five",
+						4 => "four", 3 => "three", 2 => "two", 1 => "one"}
+	ZERO = { 0 => "zero"}
+end						
+
+class BeerSong
+
+	def initialize(parameter)
+		@bottles = 0 if parameter <= 0
+		@bottles = 99 if parameter >= 99
+		@bottles = parameter if parameter > 0
+	end
+
+	def sing
+		@bottles.downto(0) do |i|
+			song(i)
+		end
+	end 
+
+	def song
+		while @bottles >= 0
+			puts "#{@bottles} bottles of beer on the wall,"
+			break if @bottles == 0
+			puts "#{@bottles} bottles of beer,"
+			puts "Take one down, pass it around,"
+			@bottles -= 1
+		end
+	end
+end 
+
+beer = BeerSong.new(154)
+beer.song  
