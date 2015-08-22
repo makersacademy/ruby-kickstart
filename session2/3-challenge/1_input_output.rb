ada85c231b45d4e9855856557432a5d7b1bfd9a0
ana <adscarlos@gmail.com>
2015-08-22 20:04:58 +0100

    adds somes notes to solution of 2:1

--- session2/3-challenge/1_input_output.rb
+++ session2/3-challenge/1_input_output.rb
@@ -7,7 +7,23 @@
 # Standard input will be like "9 2\n" and will expect you to print
 # "11\n7\n18\n" to standard output.
 
-def sum_difference_product
-  # your code goes here
-  
-end
+# I couldn't figure this one out on my own. I sort of understand how the method def works,
+# but couldn't figure out how to get the required output. I will return to this later.
+
+# with the commented-out code below, I get the puts lines returned. Why?
+
+# puts "give me 2 numbers, with a space between them"
+#
+# class String
+
+  def sum_difference_product
+    a , b = gets.split.map { |num| num.to_i }
+    puts a + b
+    puts a - b
+    puts a * b
+  end
+
+# input = ""
+# input.sum_difference_product
+
+# end
