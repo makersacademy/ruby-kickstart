123a08c9a3eae4b6c57e781a91994f2e7cb343d6
zsid <zhivko.siderov@gmail.com>
2015-08-22 19:26:21 +0100

    Beer game done! Yahooo

--- session2/3-challenge/11_classes.rb
+++ session2/3-challenge/11_classes.rb
@@ -22,16 +22,17 @@
 # Add any additional methods you find helpful.
 
 module Words
-	BIGGER = {9 => "ninety-", 8 => "eighty-", 7 => "seventy-", 6 => "sixty-",
-							5 => "fifty", 4 => "forty-", 3 => "thirty", 2 => "twenty"}
+	BIGGER = {9 => "ninety", 8 => "eighty", 7 => "seventy", 6 => "sixty",
+							5 => "fifty", 4 => "forty", 3 => "thirty", 2 => "twenty"}
 	BIG = {19 => "ninteen", 18 => "eighteen", 17 => "seventeen", 16 => "sixteen",
 					15 => "fifteen", 14 => "forteen", 13 => "thirteen", 12 => "twelve", 11 => "eleven"}
 	SMALL = {10 => "ten", 9 => "nine", 8 => "eight", 7 => "seven", 6 => "six", 5 => "five",
 						4 => "four", 3 => "three", 2 => "two", 1 => "one"}
-	ZERO = {0 => "zero"}
 end						
 
 class BeerSong
+
+	include Words
 
 	def initialize(parameter)
 		@bottles = 0 if parameter <= 0
@@ -40,22 +41,27 @@ class BeerSong
 	end
 
 	def sing
-		@bottles.downto(0) do |i|
-			song(i)
-		end
-	end 
-
-	def song
-		while @bottles >= 0
-			puts "#{@bottles} bottles of beer on the wall,"
-			break if @bottles == 0
-			puts "#{@bottles} bottles of beer,"
-			puts "Take one down, pass it around,"
+		@bottles.downto(20) do |number|
+			puts "#{BIGGER[(number.to_i/10)].capitalize} #{SMALL[(number.to_i % 10)]} bottles of beer on the wall,"
+			puts "#{BIGGER[(number.to_i/10)].capitalize} #{SMALL[(number.to_i % 10)]} bottles of beer,"
+			puts "Take one down, pass it around"
 			@bottles -= 1
 		end
-	end
+		@bottles.downto(11) do |number|
+			puts "#{BIG[number.to_i].capitalize} bottles of beer on the wall,"
+			puts "#{BIG[number.to_i].capitalize} bottles of beer,"
+			puts "Take one down, turn it around"
+			@bottles -= 1
+		end
+		@bottles.downto(1) do |number|
+			puts "#{SMALL[number.to_i].capitalize} bottles of beer on the wall,"
+			puts "#{SMALL[number.to_i].capitalize} bottles of beer"
+			puts "Take one down, turn it around"
+		end
+		puts "Zero bottles of beer on the wall" 
+	end 
 end 
 
 
-beer = BeerSong.new(20)
-beer.song  
+beer = BeerSong.new(18)
+beer.sing
