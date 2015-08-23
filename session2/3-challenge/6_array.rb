4529b342414297ed421ddf886579685985b5a20c
ana <adscarlos@gmail.com>
2015-08-23 19:10:10 +0100

    adds 2:6

--- session2/3-challenge/6_array.rb
+++ session2/3-challenge/6_array.rb
@@ -15,3 +15,18 @@
 # prime_chars? ['a', 'bcd']       # => false
 # prime_chars? ['a', 'b', 'cd']   # => false
 
+# OMG, such MATH! I could never get to this solution. I'll have to get some help to understand this one. Will get back to ir later.
+
+class Integer
+  def prime?
+    return false if self < 2
+    2.upto Math.sqrt(self) do |i|
+      return false if self % i == 0
+    end
+    true
+  end
+end
+
+def prime_chars?(strings)
+  strings.join.length.prime?
+end
