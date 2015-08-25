e37c2373eb117f38a0703ffed2103ad0f872e078
Michael Roger <bodgeuk12@gmail.com>
2015-08-25 20:55:35 +0100

    Session 3 - Challenge 3

--- session3/3-challenge/3_hashes.rb
+++ session3/3-challenge/3_hashes.rb
@@ -6,3 +6,17 @@
 #
 # Example:
 # word_count "The dog and the cat" # => {"the" => 2, "dog" => 1, "and" => 1, "cat" => 1}
+
+
+
+def word_count(str)
+	hash = {}
+	arr = str.downcase.scan(/\b\w+/)
+	arr.each do |a|
+		hash[a] = arr.count(a)
+	end
+	hash
+end
+
+#p word_count("the dog is a fat dog")
+#p word_count "The dog and the cat"
