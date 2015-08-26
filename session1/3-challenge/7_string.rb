5a0556012872b37073646fe22ce91972b35e6264
catherinestevenson <catherine.stevenson14@yahoo.co.uk>
2015-08-26 10:27:57 +0100

    completed last session 1 challenge

--- session1/3-challenge/7_string.rb
+++ session1/3-challenge/7_string.rb
@@ -5,5 +5,16 @@
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
 def pirates_say_arrrrrrrrr(string)
-
+	to_return = ""
+	add_next = false
+	string.size.times do |index|
+	current_char = string[index] 
+    to_return << current_char if add_next
+    add_next = (current_char == "r" || current_char == "R")
+  end
+  to_return
 end
+
+pirates_say_arrrrrrrrr("are you really learning Ruby?")
+pirates_say_arrrrrrrrr("Katy Perry is on the radio!")
+pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")
