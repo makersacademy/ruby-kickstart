dae9517a02821a217dde5322173bb91a4442d53b
Chuka Ebi <chuka.ebi@outlook.com>
2015-10-03 17:57:21 +0100

    5_array.rb

--- session2/3-challenge/5_array.rb
+++ session2/3-challenge/5_array.rb
@@ -17,13 +17,7 @@
 def mod_three array
   s = []
   array.each do |x|
-    if x%3 == 0
-      next
-    else
-      s << x%3
-    end
+    x%3 == 0 ? next : s << x%3
   end
   s
 end
-
-mod_three [0,1,2,3,4,5,6,7,8,9]
