09d48057b139b206ce67ad20b376723718b1c386
Hamza Sheikh <hsheikhm@gmail.com>
2015-10-03 20:15:36 +0100

    Completed Challenge 2/13 (Session 2)

--- session2/3-challenge/2_input_output_control.rb
+++ session2/3-challenge/2_input_output_control.rb
@@ -1,32 +1,32 @@
-# Prompt the user for a number, then read it in and print out "hi" that many times
-#
-# Repeat this process until the user submits "bye", then say "goodbye" and end the program
-# HINT: Check out example 2 if you get stuck
-
-# example:
-# PROGRAM: Enter a number
-# USER:    4
-# PROGRAM: hi hi hi hi
-# PROGRAM: Enter a number
-# USER:    2
-# PROGRAM: hi hi
-# PROGRAM: Enter a number
-# USER:    bye
-# PROGRAM: goodbye
-
-
-# remember you can try your program out with              $ ruby 2_input_output_control.rb
-# and when you think it is correct, you can test it with  $ rake 2:2
+=begin (below is my version which works fine but
+for some reason doesn't seem to pass the rspec tests!)
 
 def hi_hi_goodbye
-  # your code here
+  loop do
+  puts "Enter a number or bye"
+  number = gets.chomp
+  print "hi " * number.to_i
+  if number.chomp == "bye"
+    puts "Goodbye!"
+    exit
+  end
+  end
+end
+=end
 
+def prompt
+  puts 'Enter a number or bye'
+end
+
+def hi_hi_goodbye
+  prompt
+  while (line = gets) && (line !~ /bye/) # first is an assignment statement that returns a line or nil, and is thus boolean
+    line.to_i.times { print 'hi ' }
+    puts
+    prompt
+  end
+  puts "Goodbye!"
 end
 
 
-
-
-# This will just invoke the method if you run this program directly
-# This way you can try it out by running "$ ruby 2_input_output_control.rb"
-# but it will still work for our tests
 hi_hi_goodbye if $0 == __FILE__
