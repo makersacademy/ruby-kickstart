5a85764b64f3fe4833c4dbc3729f5230c99490fc
octavian <octavianrotari@gmail.com>
2015-10-04 18:27:05 +0200

    7_string done

--- session1/3-challenge/7_string.rb
+++ session1/3-challenge/7_string.rb
@@ -5,5 +5,12 @@
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
 def pirates_say_arrrrrrrrr(string)
-
+	pirate = ''
+	r = false
+	string.size.times do |i|
+		cl = string[i,1]
+		pirate << cl if r 
+		r = (cl == "r" || cl == "R")
+	end
+	pirate
 end
