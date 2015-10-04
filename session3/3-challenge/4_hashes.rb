d1fb24357d0500a1a95c6a166f74d844f0b0b121
Chuka Ebi <chuka.ebi@outlook.com>
2015-10-04 16:15:26 +0100

    Updated 4_hashes.rb

--- session3/3-challenge/4_hashes.rb
+++ session3/3-challenge/4_hashes.rb
@@ -6,3 +6,12 @@
 #
 # first_pos "The dog and the cat and the cow" # => {"The" => 0, "dog" => 1, "and" => 2, "the" => 3, "cat" => 4, "cow" => 7}
 
+def first_pos string
+  str = Hash.new(0)
+  r = string.split
+  l = r.length
+  (0...l).each do |x|
+    str.has_key?(r[x]) ? next : str[r[x]] = x
+  end
+  str
+end
