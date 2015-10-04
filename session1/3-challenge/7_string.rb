5d081c55c612e1d19c3060e213be6c6bb2688d5c
Mattia46 <mattia.assogna@gmail.com>
2015-10-04 11:58:47 +0100

    S1_07

--- session1/3-challenge/7_string.rb
+++ session1/3-challenge/7_string.rb
@@ -5,5 +5,14 @@
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
 def pirates_say_arrrrrrrrr(string)
-
+  lett = ''         # output finale
+  add_next = false  # boolean da utilizzare nella fx successiva
+  string.size.times do |x|  # size method è come length per gli array. Conta quante lettere
+    current_char = string[x]  # current_char è una var temporanea per salvare le singole lettere e il loro index
+    lett << current_char if add_next # aggiunge le lettere se il bool è true
+    add_next = (current_char == 'r' || current_char == 'R') # qui decido quando farlo diventare true
 end
+lett
+end
+
+puts pirates_say_arrrrrrrrr 'ciarforro'
