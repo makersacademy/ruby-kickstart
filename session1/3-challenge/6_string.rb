1c5df3037e507f36b27ef0a04f4b889a5b6f6c06
jamierose <jrose111284@gmail.com>
2015-10-04 10:16:31 +0100

    session 1

--- session1/3-challenge/6_string.rb
+++ session1/3-challenge/6_string.rb
@@ -4,7 +4,25 @@
 #
 # If you have no idea where to begin, remember to check out the cheatsheets for string and logic/control
 #
-
+=begin
 def odds_and_evens(string, return_odds)
-
+  odd = ''
+  even = ''
+  string.each do |item|
+    if item % 2 == 0
+      even << item
+    elsif item % 2 == 1
+      odd << item
+    end
+    return odd
+end
+=end
+def odds_and_evens(string, return_odds)
+to_return = ""
+string.size.times do |index|
+  next if return_odds && index.even?
+  next if !return_odds && index.odd?
+  to_return << string[index]
+end
+to_return
 end
