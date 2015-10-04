708f3c516332e539735cb04a40bf73243c0cd6f9
Mattia46 <mattia.assogna@gmail.com>
2015-10-04 19:53:38 +0100

    S2_05

--- session2/3-challenge/5_array.rb
+++ session2/3-challenge/5_array.rb
@@ -14,3 +14,12 @@
 #
 # mod_three [0,1,2,3,4,5,6,7] # => [1, 2, 1, 2, 1]
 
+def mod_three arr
+
+  new_arr = arr.select {|num| num % 3 != 0}
+  output = []
+  new_arr.each do |x|
+     output << x % 3
+  end
+ output 
+end
