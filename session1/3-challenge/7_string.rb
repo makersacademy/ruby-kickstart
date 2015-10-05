601118b20f77062999a1faafa349fc2bed876810
jbhdeconinck <jbhdeconinck.gmail.com>
2015-10-05 13:32:45 +0100

    commit, challenge

--- session1/3-challenge/7_string.rb
+++ session1/3-challenge/7_string.rb
@@ -5,5 +5,19 @@
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
 def pirates_say_arrrrrrrrr(string)
-
+    new_string = ""
+    current_string = (string + " ").split("")
+    i = 0
+    while i <= current_string.length
+      if current_string[i] == "r" || current_string[i] == "R"
+        new_string << current_string[i+1]
+      end
+      i += 1
+    end
+    new_string
 end
+
+
+puts pirates_say_arrrrrrrrr("are you really learning Ruby?") # => "eenu"
+puts pirates_say_arrrrrrrrr("Katy Perry is on the radio!")   # => "rya"
+puts pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")
