e6cd9e965ba1cc45c873d655631fe58df47cd26e
jamierose <jrose111284@gmail.com>
2015-10-05 08:12:07 +0100

    session 2

--- session2/3-challenge/2_input_output_control.rb
+++ session2/3-challenge/2_input_output_control.rb
@@ -17,11 +17,22 @@
 
 # remember you can try your program out with              $ ruby 2_input_output_control.rb
 # and when you think it is correct, you can test it with  $ rake 2:2
+def loops
+  puts "please enter a 'number' or 'goodbye'"
+end
 
 def hi_hi_goodbye
-  # your code here
-
+  loops
+  number = gets.chomp
+    if number == "bye"
+      puts "goodbye"
+      exit
+    else number.to_i.times {print "hi "}
+    end
+    puts
+  hi_hi_goodbye
 end
+
 
 
 
--- session2/3-challenge/5_array.rb
+++ session2/3-challenge/5_array.rb
@@ -13,4 +13,6 @@
 # mod_three [7]  # => [1]
 #
 # mod_three [0,1,2,3,4,5,6,7] # => [1, 2, 1, 2, 1]
-
+def mod_three(array)
+  array.select {|x| x % 3 != 0}.map { |x| x % 3 }
+end
