1e153a66d32ae33feff91516007c22b22016c9c6
Alaan Rahim <alaanzr@gmail.com>
2015-10-06 18:09:23 +0100

    Update 09-procs-can-see-their-environment.rb

--- session3/1-notes/09-procs-can-see-their-environment.rb
+++ session3/1-notes/09-procs-can-see-their-environment.rb
@@ -8,6 +8,9 @@ count                         # => 1
 incrementer.call
 count                         # => 2
 
+# In this case, our Proc has access to the 'count' variable.
+# If we tried to defined a method that incremented the count value by 1 however, we'd get an 'undefined method '+' for
+# nil:NilClass' error, as 'count' does not exist within the scope of the method.
 
 # They keep the environment they were defined in
 # even if they get passed into a new scope.
