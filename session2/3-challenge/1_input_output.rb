7930ce6c9f0829068df38479e4864afc938ec24c
yevdyko <evgeny.dyko@gmail.com>
2015-10-08 01:40:58 +0300

    Added session2-1_input_output

--- session2/3-challenge/1_input_output.rb
+++ session2/3-challenge/1_input_output.rb
@@ -1,13 +1,16 @@
 # Remember you can test this code with
 #   $ rake 2:1
 
-# Write a program that reads in two integers typed on the keybaord
+# Write a program that reads in two integers typed on the keyboard
 # and outputs their sum, difference, and product
 #
 # Standard input will be like "9 2\n" and will expect you to print
 # "11\n7\n18\n" to standard output.
 
 def sum_difference_product
-  # your code goes here
-  
+  input = gets.chomp.split.map { |num| num.to_i }
+  sum = input[0] + input[1]
+  diff = input[0] - input[1]
+  prod = input[0] * input[1]
+  print "#{sum}\n#{diff}\n#{prod}\n"
 end
