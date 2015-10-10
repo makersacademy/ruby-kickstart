5a21479bc40586733f07010824a33df786811d7c
samover <samuel.overloop@gmail.com>
2015-10-10 16:24:16 +0100

    added solution to 3:16

--- session3/3-challenge/16_hash.rb
+++ session3/3-challenge/16_hash.rb
@@ -2,33 +2,23 @@
 
 # Print to stdout, each element in a hash based linked list.
 # If you don't know what that is, go check out the previous problem.
-#
-# EXAMPLES:
+
 # head = {:data => 1, :next => nil}
-# print_list head                    # >> "1\n"
+# print_list head
 # head = {:data => 2, :next => head}
-# print_list head                    # >> "2\n1\n"
+# print_list head
 # head = {:data => 3, :next => head}
-# print_list head                    # >> "3\n2\n1\n"
+# print_list head
 # head = {:data => 4, :next => head}
-# print_list head                    # >> "4\n3\n2\n1\n"
+# print_list head
 # head = {:data => 5, :next => head}
-# print_list head                    # >> "5\n4\n3\n2\n1\n"
+# print_list head
 # head = {:data => 6, :next => head}
-# print_list head                    # >> "6\n5\n4\n3\n2\n1\n"
+# print_list head
 
-def print_list(node)
-
+def print_list(node, list = '')
+  list += "#{node[:data]}\n"
+  return puts list if node[:next].nil?
+  print_list(node[:next], list)
 end
-head = {:data => 1, :next => nil}
-print_list head                    # >> "1\n"
-head = {:data => 2, :next => head}
-print_list head                    # >> "2\n1\n"
-head = {:data => 3, :next => head}
-print_list head                    # >> "3\n2\n1\n"
-head = {:data => 4, :next => head}
-print_list head                    # >> "4\n3\n2\n1\n"
-head = {:data => 5, :next => head}
-print_list head                    # >> "5\n4\n3\n2\n1\n"
-head = {:data => 6, :next => head}
-print_list head                    # >> "6\n5\n4\n3\n2\n1\n"
+
