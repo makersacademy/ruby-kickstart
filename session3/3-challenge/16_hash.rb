5bf57e01b7c7f803cc9f79e2cc9cc8cde8f326c6
samover <samuel.overloop@gmail.com>
2015-10-10 16:42:17 +0100

    added solution to 3-17 and refactored 3-16

--- session3/3-challenge/16_hash.rb
+++ session3/3-challenge/16_hash.rb
@@ -4,7 +4,7 @@
 # If you don't know what that is, go check out the previous problem.
 
 # head = {:data => 1, :next => nil}
-# print_list head
+# print_list head # =>
 # head = {:data => 2, :next => head}
 # print_list head
 # head = {:data => 3, :next => head}
@@ -16,9 +16,8 @@
 # head = {:data => 6, :next => head}
 # print_list head
 
-def print_list(node, list = '')
-  list += "#{node[:data]}\n"
-  return puts list if node[:next].nil?
-  print_list(node[:next], list)
+def print_list(node)
+  p node[:data]
+  print_list(node[:next]) if node[:next]
 end
 
