c4dbbeeedf2112d1aac7f4fbee5f9700629cec67
samover <samuel.overloop@gmail.com>
2015-10-11 21:53:45 +0100

    added solution to 4-4

--- session4/3-challenge/4_oror.rb
+++ session4/3-challenge/4_oror.rb
@@ -11,5 +11,8 @@
 # first_object nil, nil, nil  # => nil
 
 def first_object(arg1, arg2, arg3)
-  
+  # return arg1 if arg1
+  # return arg2 if arg2
+  # return arg3 if arg3
+  arg1 || arg2 || arg3 || nil
 end
