b1a2c439a6af7ae7ede25b78f269933244b6b0e7
samover <samuel.overloop@gmail.com>
2015-10-20 00:31:04 +0100

    solution to 9:7

---
+++ tree_catalog.rb
@@ -0,0 +1,4 @@
+catalog = [["American Redbud Tree", "1 to 2 feet", "$5.95"], ["  Autumn Flowering Cherry", "2 to 4 feet", "$8.95"], ["  Black Walnut Tree", "1 to 2 feet", "$4.95"], ["  Cherokee Chief Dogwood", "2 to 4 feet", "$8.95"], ["  English Walnut Tree", "1 to 2 feet", "$10.95"], ["  Flowering Peach Tree", "2 to 4 feet", "$7.95"], ["  Flowering Pear Tree", "2 to 4 feet", "$8.50"], ["  Hydrangea Tree", "1 to 2 feet", "$5.95"], ["  Pecan Tree", "1 to 2 feet", "$5.95"], ["  Purple Leaf Plum Tree", "2 to 3 feet", "$5.95"], ["  Royal Empress Tree", "2 to 4 feet", "$4.50"], ["  White Dogwood Tree", "2 to 3 feet", "$5.95"]]
+
+string = ''
+puts catalog.each { |listing| 
