c1eb15867274eec12e5aa6e90164f659bcf2cbc4
gareth4192 <gareth-123@live.com>
2015-11-03 15:54:55 +0000

    added pirates_say_arrrrrrrrr method to 1.7

--- session1/3-challenge/7_string.rb
+++ session1/3-challenge/7_string.rb
@@ -5,5 +5,17 @@
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
 def pirates_say_arrrrrrrrr(string)
+  result = ""
+     next_char = false #next_char = false because the first letter doesnt follow r or R
 
+     string.length.times do |i| #loop through the string
+         current = string[i] #set the current character as current
+         if next_char #if next_char is true
+           result << current #add current character to result string
+         end
+        (current == "r" || current == "R") ? next_char = true : next_char = false
+        #set next_char to true if current character is r or R
+     end
+
+     return result #return result string
 end
