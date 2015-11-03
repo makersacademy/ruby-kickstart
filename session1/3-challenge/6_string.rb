4959c38b2b843f2d9317d750521a2599ef6056a9
Alan Gabbianelli <alan.gabbianelli@gmail.com>
2015-11-03 23:37:50 +0000

    Added challenge 6 session 1

--- session1/3-challenge/6_string.rb
+++ session1/3-challenge/6_string.rb
@@ -6,5 +6,21 @@
 #
 
 def odds_and_evens(string, return_odds)
+  new_string_odds = ""
+  string.length.times do |odd|
+    next if odd.even?
+  new_string_odds << string[odd]
+  end
 
+  new_string_even = ""
+  string.length.times do |even|
+    next if even.odd?
+  new_string_even << string[even]
+  end
+
+  if return_odds
+    new_string_odds
+  else
+    new_string_even
+  end
 end
