3cdddc5af9b87a016173c3b4b446f5d4df29e723
Chris Wynne <chris.wynndow@gmail.com>
2015-11-04 07:25:25 +0000

    Updates 2:7 to show thought process

--- session2/3-challenge/7_array.rb
+++ session2/3-challenge/7_array.rb
@@ -10,14 +10,14 @@
 # alternate_words("Elementary, my dear Watson!")  # => ["Elementary", "dear"]
 
 def alternate_words(str)
-'/[!@$#%^&*()-=+\[\]:;,.\/<>?\|]/'.split(//).each do |x|
-  str = str.gsub(x, " ")
-end
-
-# I took the idea for this code block from the solution. The solution I came up
+#'/[!@$#%^&*()-=+\[\]:;,.\/<>?\|]/'.split(//).each do |x|
+#  str = str.gsub(x, " ")
+#
+# I took the idea for the code block above from the solution. The solution I came up
 # with (below) was giving me a 'Can't modify frozen string' error when run with
 # Rspec. It works if you just run it in Ruby though
-#str.gsub!(/[!@$#%^&*()-=+\[\]:;,.\/<>?\|]/, "")  => This was my original code.
+#str.gsub!(/[!@$#%^&*()-=+\[\]:;,.\/<>?\|]/, " ")  #=> This was my original code.
+str = str.gsub(/[^A-Za-z0-9'’]/, " ")  #=> This was a new solution I came to with help
 words = str.split
 output = []
 for i in 0..(words.length-1) do
@@ -25,3 +25,6 @@ for i in 0..(words.length-1) do
 end
 output
 end
+
+#puts alternate_words("Would a dating service on the net be frowned upon . . . ? I hope not. But even if it is, don’t let that stop you from notifying me via net mail if you start one.")
+#puts alternate_words("don't")
