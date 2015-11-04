7947d3b4eb29f9fd8db460566a82b33ab549c1d7
Alan Gabbianelli <alan.gabbianelli@gmail.com>
2015-11-04 16:13:18 +0000

    Added challenge 7 session 1

--- session1/3-challenge/7_string.rb
+++ session1/3-challenge/7_string.rb
@@ -5,5 +5,12 @@
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
 def pirates_say_arrrrrrrrr(string)
-
+  return_string = ""
+  string = string.split("")
+  string.each_index do |index_num|
+    if (string[index_num] == "r" || string[index_num] == "R") && string[index_num+1] != nil
+      return_string << string[index_num+1]
+    end
+  end
+    return_string
 end
