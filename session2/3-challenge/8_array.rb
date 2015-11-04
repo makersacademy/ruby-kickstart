341140b4271653f024db6178cef154f77748ed39
Charlie Person <charlie.person@hyperisland.se>
2015-11-04 21:47:58 +0000

    2:8

--- session2/3-challenge/8_array.rb
+++ session2/3-challenge/8_array.rb
@@ -6,3 +6,24 @@
 # got_three? ['a', 'a', 'a']  # => true
 # got_three? [1, 2, 1, 1]     # => false
 
+def got_three?(array)
+  count = 0
+  array.each_with_index do |a, index|
+    if count == 3
+      return true
+    end
+    if a == a[index+1]
+      count += 1
+    else
+      count = 0
+    end
+    return false
+  end
+end
+
+def got_three?(array)
+  array.each_cons(3) do |a, b, c|
+    return true if a==b && b==c
+  end
+  return false
+end
