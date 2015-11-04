c585e75e42e50dabe3a30344a64214ac3a0b0d6d
emmabeynon <emma.beynon@gmail.com>
2015-11-04 17:04:44 +0000

    Answer to 2:3_array.rb

--- session2/3-challenge/3_array.rb
+++ session2/3-challenge/3_array.rb
@@ -7,6 +7,15 @@
 
 class String
   def every_other_char
+    original_characters = self.split("")
+    filtered_characters = []
+    i = 0
+    original_characters.each do
+      filtered_characters << original_characters[i] if i % 2 == 0
+      i +=1
+    end
+    filtered_characters.join("")
   end
-  
 end
+
+"abcdefg".every_other_char
