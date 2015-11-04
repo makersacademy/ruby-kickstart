9fce0d5c54fdbfe97a356447bc40f6f78a32eedc
Will Johnson <william.jl.johnson@gmail.com>
2015-11-04 23:29:02 +0000

    Challenge 1:5 Updated

--- session1/3-challenge/5_string.rb
+++ session1/3-challenge/5_string.rb
@@ -5,5 +5,11 @@
 # add_more_ruby("Sad times are ahead!")         # => "Happy times are ahead!"
 
 def add_more_ruby(string)
-  string.gsub!("sad", "happy")
+  if string.include? "sad"
+    return string.gsub!(/sad/, "happy")
+  else
+    return string.gsub!(/Sad/, "Happy")
 end
+end
+
+#sadSadsadsad => happySadhappyhappy
