b676d9884a5f09f1252b613a6ff7f3ab6a7cf37d
edwardkerry <edwardkerry@gmail.com>
2015-11-05 18:11:45 +0000

    solved session 2 challenge 2

--- session2/3-challenge/2_input_output_control.rb
+++ session2/3-challenge/2_input_output_control.rb
@@ -18,11 +18,19 @@
 # remember you can try your program out with              $ ruby 2_input_output_control.rb
 # and when you think it is correct, you can test it with  $ rake 2:2
 
-def hi_hi_goodbye
-  # your code here
-
+def ask
+    puts "Give me a number or say bye"
 end
 
+def hi_hi_goodbye
+    ask
+    while (num=gets) && (num.downcase != "bye") 
+      puts "Hi " * num.to_i
+      ask
+    end
+  puts "goodbye"
+end
+  
 
 
 
