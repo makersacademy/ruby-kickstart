d4cc4dd3d4834d38cce8339cc7bc6e621f5935aa
Alan Gabbianelli <alan.gabbianelli@gmail.com>
2015-11-05 11:22:24 +0000

    Added challenge 2:2 and updated example 2:2

--- session2/2-examples/2_gets.rb
+++ session2/2-examples/2_gets.rb
@@ -1,8 +1,20 @@
+=begin
 puts "Hello, start typing!"
 
 while line = gets
   puts "You submitted #{line.inspect}"
   break if line.chomp == 'exit'
+end
+
+puts "Goodbye!"
+=end
+
+
+puts "Hello, start typing!"
+
+while line = gets.chomp
+  puts "You submitted #{line}"
+  break if line == 'exit'
 end
 
 puts "Goodbye!"
