11f06623fdfafb4f050aa854f524d1eaca1d0898
Antoine Gordon <antoinegordon86@gmail.com>
2015-11-05 11:39:00 +0000

    commit message

--- session1/3-challenge/7_string.rb
+++ session1/3-challenge/7_string.rb
@@ -5,5 +5,11 @@
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
 def pirates_say_arrrrrrrrr(string)
-
+	string_returned = ""
+	string.size.times do |index|
+		if (string[index] == "r" || string[index] == "R") && string[index+1] != nil
+			string_returned << string[index+1]
+			end
+		end
+string_returned
 end
