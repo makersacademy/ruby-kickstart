91031376d36afe65b01f85d387501f013eb90f44
emmabeynon <emma.beynon@gmail.com>
2015-11-05 18:58:10 +0000

    Answer to 2:11_classes.rb

--- session2/3-challenge/11_classes.rb
+++ session2/3-challenge/11_classes.rb
@@ -20,3 +20,42 @@
 # if the parameter is greater than 99, set the number of beer bottles to 99
 # Then make a public method called print_song that outputs all stanzas from the number of bottles of beer down to zero.
 # Add any additional methods you find helpful.
+
+class BeerSong
+  attr_accessor :starting_bottles
+  def initialize(starting_bottles)
+    if starting_bottles < 0
+      @starting_bottles = 0
+    elsif starting_bottles > 99
+      @starting_bottles = 99
+    else
+      @starting_bottles = starting_bottles
+    end
+  end
+
+  def print_song
+    @starting_bottles.downto(1) { |i| print_stanza(i) }
+  end
+
+  def bottle_english(n)
+    tens_array = ['','ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+    teens_array = ['', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+    ones_array = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+    english = []
+    ones = n % 10
+    tens = (n - ones) / 10
+    english << tens_array[tens] unless tens == 0 || tens == 1 && ones > 0
+    english << teens_array[ones] if tens == 1 && ones != 0
+    english << ones_array[ones] unless tens == 1 || tens >= 1 && ones == 0
+    english.join("-").capitalize
+  end
+
+  def bottle_noun(n)
+    n == 1 ? "bottle" : "bottles"
+  end
+
+  def print_stanza(n)
+    puts "#{bottle_english(n)} #{bottle_noun(n)} of beer on the wall,\n#{bottle_english(n)} #{bottle_noun(n)} of beer,\nTake one down, pass it around,"
+    puts "#{bottle_english(n-1)} #{bottle_noun(n-1)} of beer on the wall."
+  end
+end
