2d2bd8e303ba5cdf8ac0dc8a38b49e7f1f6416f9
michaellennox <michaelctlennox@gmail.com>
2015-11-05 18:24:09 +0000

    Completed challenge 2:11

--- session2/3-challenge/11_classes.rb
+++ session2/3-challenge/11_classes.rb
@@ -20,3 +20,64 @@
 # if the parameter is greater than 99, set the number of beer bottles to 99
 # Then make a public method called print_song that outputs all stanzas from the number of bottles of beer down to zero.
 # Add any additional methods you find helpful.
+
+class BeerSong
+
+  def initialize(bottles)
+    bottles = 99 if bottles > 99
+    bottles = 0 if bottles < 0
+    @bottles = bottles
+  end
+
+  def print_song
+    (0...@bottles).each do |i|
+      this_bottles = @bottles - i
+      next_bottles = @bottles - (i + 1)
+      this_plural = plural(this_bottles)
+      next_plural = plural(next_bottles)
+      puts "#{english(this_bottles).capitalize} #{this_plural} of beer on the wall,"
+      puts "#{english(this_bottles).capitalize} #{this_plural} of beer,"
+      puts "Take one down, pass it around,"
+      puts "#{english(next_bottles).capitalize} #{next_plural} of beer on the wall."
+    end
+  end
+
+  def english(n)
+    if n == 0
+      return "zero"
+    end
+
+    eng_num = ""
+
+    singles = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+    tens = ['ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+    teens = ['eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+
+    remaining = n
+
+    ten_push = remaining / 10
+    remaining = remaining - ten_push * 10
+
+    if ten_push > 0
+      if ten_push == 1 && remaining != 0
+        eng_num = teens[remaining - 1]
+        remaining = 0
+      elsif remaining != 0
+        eng_num = tens[ten_push - 1] + "-"
+      else
+        eng_num = tens[ten_push -1]
+      end
+    end
+
+    if remaining != 0
+      eng_num = eng_num + singles[remaining - 1]
+    end
+
+    eng_num
+  end
+
+  def plural(n)
+    n == 1 ? "bottle" : "bottles"
+  end
+
+end
