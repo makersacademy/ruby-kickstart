0e739588074af25ad87fe7978496e01dfdd577c1
Alan Gabbianelli <alan.gabbianelli@gmail.com>
2015-11-06 22:01:18 +0000

    Added challenge 2:6

--- session2/3-challenge/6_array.rb
+++ session2/3-challenge/6_array.rb
@@ -15,3 +15,32 @@
 # prime_chars? ['a', 'bcd']       # => false
 # prime_chars? ['a', 'b', 'cd']   # => false
 
+def is_this_prime(n)
+  n = n.to_i
+  if n < 2
+    return false
+  elsif n == 2
+    return true
+  else
+    for x in 2..(n - 1)
+      if (n % x) == 0
+        return false
+      end
+    end
+    return true
+  end
+end
+
+#require 'prime'
+def prime_chars?(my_string)
+  my_array = Array.new
+  tot_char = 0
+  my_string.each { |c| my_array << c }
+  my_array.each { |x| tot_char += x.length }
+#  tot_char.prime? ? true : false
+  if is_this_prime(tot_char)
+    return true
+  else
+    return false
+  end
+end
