eeadeb73118b940869b9e0123318a19d488f0455
jamiebrown2011 <jamiebrown303@gmail.com>
2015-11-06 22:10:03 +0000

    completed challenge 7

--- session1/3-challenge/7_string.rb
+++ session1/3-challenge/7_string.rb
@@ -5,5 +5,17 @@
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
 def pirates_say_arrrrrrrrr(string)
+final = ""
+c = 0
+
+string.each_char do |x|
+	if (string[c - 1] == "r" || string[c - 1] == "R") && c > 0
+		final << x
+	end
+c += 1
+end
+
+return final
 
 end
+
