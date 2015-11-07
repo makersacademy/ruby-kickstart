5c5c050b30ac2672c54b37bfc1ac8074bc9e4428
Alan Gabbianelli <alan.gabbianelli@gmail.com>
2015-11-07 23:00:28 +0000

    Added challenge 2:7

--- session2/3-challenge/7_array.rb
+++ session2/3-challenge/7_array.rb
@@ -9,3 +9,25 @@
 # alternate_words("Can't we all get along?")      # => ["Can't", "all", "along"]
 # alternate_words("Elementary, my dear Watson!")  # => ["Elementary", "dear"]
 
+def alternate_words(sentence)
+  my_words = []
+  '!@$#%^&*()-=_+[]:;,./<>?\\|'.split("").each do |c|
+    sentence = sentence.gsub(c, " ")
+  end
+  words = sentence.split
+  #words = sentence.split(/[\s!@$^&*()-=_+[\]:;,.\/#%<>?\|]/)       <--What's wrong with this??
+  words.each_with_index do |w, i|
+    next if i.odd?
+    my_words << w
+  end
+  my_words
+end
+
+p alternate_words("I want to go home, I'm to sleepy to stay")
+p alternate_words("We won't come, anyway")
+p alternate_words("You know I like it, but a whole cake is too much also for me!")
+p alternate_words("Don't be silly: of course I'll drive you home!")
+p alternate_words("Are you ok? You seem pale...")
+p alternate_words("Lorem ipsum dolor sit amet.")
+p alternate_words("Can't we all get along?")
+p alternate_words("Elementary, my dear Watson!")
