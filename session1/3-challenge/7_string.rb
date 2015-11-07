5f055897ffa9e6fc12338a95c5c0b27c973d010c
Andrew Burnie <apburnie@hotmail.co.uk>
2015-11-07 19:17:08 +0000

    Session1complete

--- session1/3-challenge/7_string.rb
+++ session1/3-challenge/7_string.rb
@@ -5,5 +5,14 @@
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
 def pirates_say_arrrrrrrrr(string)
+  to_r = ""
+  add_n = false
+  string.size.times do |index|
 
+    current_char = string[index]
+    to_r << current_char if add_n
+    add_n = (current_char == "r" || current_char == "R")
+
+  end
+  to_r
 end
