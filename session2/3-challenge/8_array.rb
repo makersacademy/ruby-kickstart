a2767b5bf4229de2ef5085647e6794da7154bbb0
Alan Gabbianelli <alan.gabbianelli@gmail.com>
2015-11-08 21:10:26 +0000

    Added challenge 2:8

--- session2/3-challenge/8_array.rb
+++ session2/3-challenge/8_array.rb
@@ -6,3 +6,12 @@
 # got_three? ['a', 'a', 'a']  # => true
 # got_three? [1, 2, 1, 1]     # => false
 
+def got_three?(arr)
+  t_or_f = false
+  arr.each_index do |i|
+    if (arr[i] == arr[i+1]) && (arr[i+1] == arr[i+2])
+      t_or_f = true
+    end
+  end
+  return t_or_f
+end
