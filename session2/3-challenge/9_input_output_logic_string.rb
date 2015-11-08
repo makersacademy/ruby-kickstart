9ac0eb245530ff3a44623d8240db6107304a6363
Kirk Smith <kirks@hotmail.co.uk>
2015-11-08 22:28:15 +0000

    2:1-10

--- session2/3-challenge/9_input_output_logic_string.rb
+++ session2/3-challenge/9_input_output_logic_string.rb
@@ -24,7 +24,14 @@
 # USER:     BYE
 
 def deaf_grandma
-
+  while line = gets.chomp
+    break if line == "BYE"
+    if line == line.upcase && line != ""
+    puts "NO, NOT SINCE 1938!"
+    else
+    puts "HUH?! SPEAK UP, SONNY!"
+    end
+  end
 end
 
 
