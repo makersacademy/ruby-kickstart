52148c83a87146c0132787e8cf31e4c7de48f71a
camilla_foodity <camillacolser@hotmail.com>
2015-11-08 17:55:52 +0000

    2:7 not right yet

--- session2/3-challenge/7_array.rb
+++ session2/3-challenge/7_array.rb
@@ -5,24 +5,15 @@
 # Assume each of these  charactsrs are not to be considered: ! @ $ # % ^ & * ( ) - = _ + [ ] : ; , . / < > ? \ |
 
 def alternate_words(sentence)
-  words_uncleaned = sentence.split(/ /)
-  words_cleaned = []
-  alternate_words_cleaned = []
-  words_uncleaned.each do |word|
-    word_check = word.split('')
-    unless word_check == ""
-    word_check.select! {|caracter| caracter != "!" && caracter != "@" && caracter != "$" && caracter != "#" && caracter != "%" && caracter != "^" && caracter != "&" && caracter != "*" && caracter != "(" && caracter != ")" && caracter != "-" }
-    word_check.select! {|caracter| caracter != "=" && caracter != "_" && caracter != "+" && caracter != "[" && caracter != "]" && caracter != ":" && caracter != ";" && caracter != "," && caracter != "\." && caracter != "/" && caracter != "<" }
-    word_check.select! {|caracter| caracter != ">" && caracter != "?" && caracter != "\\" && caracter != "|" && caracter != "}" && caracter != ""}
-    words_cleaned << word_check.join("")
-    end
-  end
+  words = sentence.gsub(/[^a-zA-Z0-9\' ]/, '')
+  words = words.split(" ")
+  alternate_words = []
   i = 0
-  words_cleaned.each do |word|
-    alternate_words_cleaned << word if i.even?
+  words.each do |word|
+    alternate_words << word if i.even?
     i += 1
   end
-  p alternate_words_cleaned
+  p alternate_words
 end
 
 # Examples
@@ -30,4 +21,3 @@ end
 #alternate_words("Can't we all get along?")      # => ["Can't", "all", "along"]
 #alternate_words("Elementary, my dear Watson!")  # => ["Elementary", "dear"]
 #alternate_words "I could have made money this way, and perhaps amused myself writing code. But I knew that at the end of my career, I would look back on"
-alternate_words "Would a dating service on the net be frowned upon . . . ? I hope not. But even if it is, don’t let that stop you from notifying me via net mail if you start one."
