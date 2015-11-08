a6093563fbf6d2a8d3bca6571617a94a801097a5
Richard Moss <richard@englishtutorslive.com>
2015-11-08 18:00:33 +0000

    sess 2 ex 9,10,11

--- session2/3-challenge/11_classes.rb
+++ session2/3-challenge/11_classes.rb
@@ -20,3 +20,47 @@
 # if the parameter is greater than 99, set the number of beer bottles to 99
 # Then make a public method called print_song that outputs all stanzas from the number of bottles of beer down to zero.
 # Add any additional methods you find helpful.
+class Fixnum
+
+  def is_plural
+    self == 1 ? "bottle" : "bottles"
+  end
+
+end
+
+class BeerSong
+
+  def initialize (bottles)
+    bottles = 99 if bottles > 99
+    bottles = 0 if bottles < 0
+    @bottles = bottles
+  end
+
+  def print_song
+    @bottles.downto(1) do |bot_num|
+      puts "#{to_eng(bot_num).capitalize} #{bot_num.is_plural} of beer on the wall,"
+      puts "#{to_eng(bot_num).capitalize} #{bot_num.is_plural} of beer,"
+      puts "Take one down, pass it around,"
+      puts "#{to_eng(bot_num - 1).capitalize} #{(bot_num-1).is_plural} of beer on the wall."
+    end
+  end
+
+  def to_eng(value)
+    numbers_to_name = {90 => "ninety", 80 => "eighty", 70 => "seventy", 60 => "sixty", 50 => "fifty", 40 => "forty", 30 => "thirty", 20 => "twenty", 19 => "nineteen", 18 =>"eighteen", 17 =>"seventeen",  16 => "sixteen", 15=>"fifteen", 14=>"fourteen", 13=>"thirteen", 12=>"twelve", 11 => "eleven", 10 => "ten", 9 => "nine", 8 => "eight", 7 => "seven", 6 => "six", 5 => "five", 4 => "four", 3 => "three", 2 => "two", 1 => "one", 0 => "zero"}
+    if value < 20 || value % 10 == 0
+      return numbers_to_name[value]
+    else
+      tens = value / 10
+      tens_return = tens * 10
+      singles = value - tens * 10
+      return numbers_to_name[tens_return] + "-" + numbers_to_name[singles]
+    end
+  end
+end
+
+singsong = BeerSong.new 99
+
+singsong.print_song
+
+singsong = BeerSong.new 20
+singsong.print_song
--- session2/3-challenge/9_input_output_logic_string.rb
+++ session2/3-challenge/9_input_output_logic_string.rb
@@ -24,13 +24,13 @@
 # USER:     BYE
 
 def deaf_grandma
-	speech = gets.chomp 
+	speech = gets.chomp
 	while speech != "BYE"
 		if speech != speech.upcase
-			puts "HUH?!  SPEAK UP, SONNY!"
+			puts "HUH?! SPEAK UP, SONNY!"
 		elsif speech == ""
-			puts "HUH?!  SPEAK UP, SONNY!"
-		else 
+			puts "HUH?! SPEAK UP, SONNY!"
+		else
 			puts "NO, NOT SINCE 1938!"
 		end
 	speech = gets.chomp
