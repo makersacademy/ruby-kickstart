7d1a97f06a1884ec21c96d1535df5f3a479bb0bf
camilla_foodity <camillacolser@hotmail.com>
2015-11-08 17:22:15 +0000

    th exercise completed

--- session2/3-challenge/6_array.rb
+++ session2/3-challenge/6_array.rb
@@ -6,21 +6,27 @@
 
 def prime_chars?(array_strings)
   sum = array_strings.join('').length.to_i
-  is_sum_prime = true
+  is_the_num_prime = true
+  if sum <= 1
+    is_the_num_prime = false
+  else
   for num in 2...sum
-    is_sum_prime = false if sum % num == 0
+    is_the_num_prime = false if sum % num == 0
   end
-  is_sum_prime
+  end
+  is_the_num_prime
 end
 
 # Examples of length three
 # prime_chars? ['abc']            # => true
 # prime_chars? ['a', 'bc']        # => true
 # prime_chars? ['ab', 'c']        # => true
-# prime_chars? ['a', 'b', 'c']    # => true
+# prime_chars? ['a', 'b', 'c']    # => true
 #
 # Examples of length four
-# prime_chars? ['abcd']           # => false
+# prime_chars? ['abcd']           # => false
 # prime_chars? ['ab', 'cd']       # => false
 # prime_chars? ['a', 'bcd']       # => false
-# prime_chars? ['a', 'b', 'cd']   # => false
+
+#prime_chars? ['a', 'b', 'cd']   # => false
+#prime_chars? 45672
