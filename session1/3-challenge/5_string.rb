e9ab75b57229b814f951be68d52cdab4beccb9d6
camilla_foodity <camillacolser@hotmail.com>
2015-11-08 17:42:40 +0000

    1:5 completed

--- session1/3-challenge/5_string.rb
+++ session1/3-challenge/5_string.rb
@@ -1,18 +1,9 @@
 # Given a string, replace every instance of sad to happy
-# 
+#
 # add_more_ruby("The clowns were sad.")         # => "The clowns were happy."
 # add_more_ruby("The sad dad said sad stuff.")  # => "The happy dad said happy stuff."
 # add_more_ruby("Sad times are ahead!")         # => "Happy times are ahead!"
 
 def add_more_ruby(string)
-
-	array = string.split( )
-	array.map do |word|
-	i += 1
-	if word == "sad"
-	array[i] = "happy" 
-	end
-	end
-	return array
-
+	string.gsub('sad', 'happy').gsub('Sad', 'Happy')
 end
