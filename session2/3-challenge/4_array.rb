f0a3c472504842b86048de956a86afac33f34488
Kirk Smith <kirks@hotmail.co.uk>
2015-11-08 18:00:08 +0000

    2:1-8

--- session2/3-challenge/4_array.rb
+++ session2/3-challenge/4_array.rb
@@ -8,5 +8,9 @@
 
 # This time you will have to define the method, it's called: get_squares
 
-
-
+def get_squares(array)
+  to_return = array.select do |num|
+    num if array.include?(num * num)
+  end
+    to_return.sort
+end
