d18ab360e93a12474955761dfc54facf8e5e128d
Alan Gabbianelli <alan.gabbianelli@gmail.com>
2015-11-09 17:02:13 +0000

    Added challenge 2:12

--- session2/3-challenge/12_classes.rb
+++ session2/3-challenge/12_classes.rb
@@ -23,8 +23,24 @@
 # f.to_f               # => 0.5
 
 class Fraction
+  def initialize (numerator, denominator)
+    @numerator = numerator
+    @denominator = denominator
+  end
+  attr_accessor :numerator
+  attr_accessor :denominator
+  def to_f
+    @numerator.to_f / @denominator.to_f
+  end
   def gcd(a,b)
     return a if b == 0
     gcd(b, a%b)
+  end
+  def lowest
+    gcd = gcd(@numerator, @denominator)
+    Fraction.new(@numerator/gcd, @denominator/gcd)
+  end
+  def to_s
+    "#{numerator}/#{denominator}"
   end
 end
