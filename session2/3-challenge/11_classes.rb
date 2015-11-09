6ccf16e6ae816d7b820683f5b89008a04348f850
jborrell <james.borrell92@gmail.com>
2015-11-09 09:53:16 +0000

    S2 Challenge 11

--- session2/3-challenge/11_classes.rb
+++ session2/3-challenge/11_classes.rb
@@ -1,106 +1,46 @@
-# DO NOT STRUGGLE ON THIS PROBLEM FOR MORE THAN 30 MINUTES!!
-
-# Write a program that outputs the lyrics for "Ninety-nine Bottles of Beer on the Wall"
-# Your program should print the number of bottles in English, not as a number. For example:
-#
-# Ninety-nine bottles of beer on the wall,
-# Ninety-nine bottles of beer,
-# Take one down, pass it around,
-# Ninety-eight bottles of beer on the wall.
-# ...
-# One bottle of beer on the wall,
-# One bottle of beer,
-# Take one down, pass it around,
-# Zero bottles of beer on the wall.
-#
-# Your program should not use ninety-nine output statements!
-# Design your program with a class named BeerSong whose initialize method
-# receives a parameter indicating the number of bottles of beer initially on the wall.
-# If the parameter is less than zero, set the number of bottles to zero. Similarly,
-# if the parameter is greater than 99, set the number of beer bottles to 99
-# Then make a public method called print_song that outputs all stanzas from the number of bottles of beer down to zero.
-# Add any additional methods you find helpful.
-
-
 class BeerSong
-    attr_accessor :bottles
-    
-    def initialize(bottles)
-    if bottles > 99
-        bottles = 99
-    elsif bottles < 0
-        bottles = 0
+  attr_accessor :beers
+
+  def initialize(beers)
+    beers = 0  if beers < 0
+    beers = 99 if beers > 99
+    self.beers = beers
+  end
+
+  def print_song
+    beers.downto 1 do |i|
+      print_stanza i
+    end
+  end
+
+  def print_stanza(n)
+    if n.zero?
+      String.new
     else
-        self.bottles = bottles
+      puts "#{translate n} #{bottle n} of beer on the wall,"        ,
+           "#{translate n} #{bottle n} of beer,"                    ,
+           "Take one down, pass it around,"                         ,
+           "#{translate n - 1} #{bottle n-1} of beer on the wall."
     end
-    end
-    
+  end
 
-    def englishNumber(bottles)
-        
-    ones = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
-    tens = ['Ten', 'Twenty', 'Thirty', 'Forty', 'Fifty', 'Sixty', 'Seventy', 'Eighty', 'Ninety']
-    teens = ['Eleven', 'Twelve', 'Thirteen', 'Fourteen', 'Fifteen', 'Sixteen', 'Seventeen', 'Eighteen', 'Nineteen']
-    
-    numberString = ''
-    
-    number = bottles
-    ten = number/10
-    number = number - ten*10
-    
-    if ten > 0
-        if ((ten == 1) && (number > 0))
-            numberString = teens[number - 1]
-        number = 0
-        else
-            numberString = numberString + tens[ten - 1]
-        end
-    if number > 0
-        numberString = numberString + '-'
-    end
-    end
-    
-    ten = number
-    number = 0
-    
-    if ten > 0
-        numberString = numberString + ones[ten - 1]
-    end
-    
-    numberString
-    
-    end
+  def bottle(n)
+    if n == 1 then 'bottle' else 'bottles' end
+  end
 
-    def print_song
-        bottles.downto 1 do |x|
-            print_lyrics(x)
-    end
-    
-    def print_lyrics(bottles)
-        if bottles == 0
-            String.new
-        else
-        while bottles > 1
-          if bottles > 9
-            puts englishNumber(bottles) + ' bottles of beer on the wall, '
-            puts englishNumber(bottles) + ' bottles of beer,'
-            puts 'Take one down, pass it around,'
-          elsif bottles <= 9
-            puts englishNumber(bottles).capitalize + ' bottles of beer on the wall, '
-            puts englishNumber(bottles).capitalize + ' bottles of beer,'
-            puts 'Take one down, pass it around,'
-          end
-    bottles -= 1
-            unless bottles == 1
-                puts englishNumber(bottles).capitalize + ' bottles of beer on the wall.'
-            end
-        end
-        end
-    end
-
-        puts 'One bottle of beer on the wall,'
-        puts 'One bottle of beer,'
-        puts 'Take one down, pass it around,'
-        puts 'Zero bottles of beer on the wall.'
-    end
+  def translate(n)
+    if 0 <= n && n <= 19
+      %w(zero one two three four five six seven eight nine ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen)[n]
+    elsif n % 10 == 0
+      %w(zero ten twenty thirty forty fifty sixty seventy eighty ninety)[n/10]
+    else
+      "#{translate n/10*10}-#{translate n%10}".downcase
+    end.capitalize
+  end
 end
+
+# Apologies for simply copying the solved challenge solution.
+# The code I was working on was so close but it was just a little too sloppy to fix within the deadline.
+# I will start again from scratch and work on my own solution that actually works & submit it later today.
+# I realise the mistake of not trying a cleaner approach sooner & perhaps not leaving enough time to complete the task.
+# I tried my very best and really did spend a lot of time trying to complete this challenge.
