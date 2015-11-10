981f67bdfdbccd9484998bc618d7a81011971ce1
Colin McCulloch <mccolane@gmail.com>
2015-11-10 16:07:15 +0000

    session2, Answer 6

--- session2/3-challenge/6_array.rb
+++ session2/3-challenge/6_array.rb
@@ -3,15 +3,31 @@
 #
 # Remember that a number is prime if the only integers that can divide it with no remainder are 1 and itself.
 #
+
+require 'prime'
+def prime_chars?(array)
+
+
+
+
+num = array.join.length
+
+Prime.instance.prime?(num)
+
+
+
+end
+
+
+
 # Examples of length three
-# prime_chars? ['abc']            # => true
-# prime_chars? ['a', 'bc']        # => true
-# prime_chars? ['ab', 'c']        # => true
-# prime_chars? ['a', 'b', 'c']    # => true
+p prime_chars? ['abc']            # => true
+p prime_chars? ['a', 'bc']        # => true
+p prime_chars? ['ab', 'c']        # => true
+p prime_chars? ['a', 'b', 'c']    # => true
 #
 # Examples of length four
-# prime_chars? ['abcd']           # => false
-# prime_chars? ['ab', 'cd']       # => false
-# prime_chars? ['a', 'bcd']       # => false
-# prime_chars? ['a', 'b', 'cd']   # => false
-
+p prime_chars? ['abcd']           # => false
+p prime_chars? ['ab', 'cd']       # => false
+p prime_chars? ['a', 'bcd']       # => false
+p prime_chars? ['a', 'b', 'cd']   # => false
