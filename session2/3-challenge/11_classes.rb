39bc47bf10a32c84ecf90ac3f60570e65861bce0
William Bautista <williambautista@gmail.com>
2015-11-10 22:07:53 +0000

    Adding Challenge 2:11 and 2:12

--- session2/3-challenge/11_classes.rb
+++ session2/3-challenge/11_classes.rb
@@ -20,3 +20,75 @@
 # if the parameter is greater than 99, set the number of beer bottles to 99
 # Then make a public method called print_song that outputs all stanzas from the number of bottles of beer down to zero.
 # Add any additional methods you find helpful.
+class BeerSong
+
+  attr_accessor 'beer'
+
+  def initialize(beer)
+		if beer > 99
+				@beer = 99
+			elsif beer < 0
+				@beer = 0
+			else
+				@beer = beer
+		end
+  end
+
+	def bottle(n)
+		if n == 1
+			"bottle"
+		else
+			"bottles"
+		end
+	end
+
+
+  def print_song
+    while @beer > 0
+    	puts "#{number(@beer).capitalize} #{bottle(@beer)} of beer on the wall,"
+    	puts "#{number(@beer).capitalize} #{bottle(@beer)} of beer,"
+    	puts "Take one down, pass it around,"
+    	puts "#{number(@beer-1).capitalize} #{bottle(@beer-1)} of beer on the wall."
+			@beer -= 1
+
+			puts
+		#	break if @beer == 0
+		end
+			puts ""
+
+  end
+
+
+	def number(beers)
+  	ones = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+  	teens = ['', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+  	tens = ['', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+
+  string = ''
+
+  	if beers > 19
+    	tensplace = beers / 10
+    	string << tens[tensplace - 1]
+    	beers -= tensplace * 10
+    return string if beers % 10 == 0
+    	string << '-'
+
+  	elsif (beers < 20 && beers > 9)
+    	teensplace = beers % 10
+
+    return string << teens[teensplace+1]
+  	end
+
+  	if beers >= 0
+    	string << ones[beers]
+    	string
+  	end
+	end
+
+
+end
+
+
+bottle = BeerSong.new(200)
+
+bottle.print_song
--- session2/3-challenge/12_classes.rb
+++ session2/3-challenge/12_classes.rb
@@ -22,9 +22,51 @@
 # f.to_s               # => "50/100"
 # f.to_f               # => 0.5
 
+# DO NOT STRUGGLE ON THIS PROBLEM FOR MORE THAN 30 MINUTES!!
+
+# Define a class called Fraction
+# This class is used to represent a ratio of two integers
+# Include setter methods called numerator and denominator that allow the user to change these values
+# Also include a method called to_f that returns the ratio as a float (a number with a decimal point, such as 10.3)
+# Also include a method called lowest, which returns a new Fraction, where the numerator and denominator
+# are reduced to lowest terms (ie 20/60 becomes 1/3)
+# This will require finding the greatest common divisor for the numerator and denominator
+# then dividing both by that number.
+# I have included a greatest common divisor method for you
+# You should also define a method, to_s, that will represent the Fraction as a String
+#class Fraction
 class Fraction
+  attr_accessor 'numerator', 'denominator'
+
+  def initialize(numerator, denominator)
+    self.numerator, self.denominator = numerator, denominator
+  end
+
+  def to_f
+    numerator / denominator.to_f
+  end
+
+  def lowest
+    divisor = gcd(numerator, denominator)
+    Fraction.new(numerator/divisor, denominator/divisor)
+  end
+
+  def to_s
+    "#{numerator}/#{denominator}"
+  end
+
   def gcd(a,b)
     return a if b == 0
-    gcd(b, a%b)
+    gcd(b, a % b)
   end
 end
+# EXAMPLE:
+f = Fraction.new 20, 60
+p f.numerator          # => 20
+p f.denominator        # => 60
+p f.to_s               # => "20/60"
+p f.lowest.to_s        # => "1/3"
+p f.numerator = 50
+p f.denominator = 100
+p f.to_s               # => "50/100"
+p f.to_f               # => 0.5
