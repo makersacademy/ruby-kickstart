6f138a2538dae62651bdbf0e1f4eacfe21741a7f
edwardkerry <edwardkerry@gmail.com>
2015-11-15 21:59:57 +0000

    Solved Session 2 Challenge 11 - removed rogue comma!

--- session2/3-challenge/11_classes.rb
+++ session2/3-challenge/11_classes.rb
@@ -33,12 +33,14 @@ class BeerSong
     def print_song
         if @bottles == 0
             puts ""
-        else @bottles.downto(1) do
-            puts "#{english_number(@bottles)} #{more_than_one?} of beer on the wall,".capitalize
-            puts "#{english_number(@bottles)} #{more_than_one?} of beer,".capitalize
-            puts "Take one down, pass it around,"
-            puts "#{english_number(@bottles-=1)} #{more_than_one?} of beer on the wall.".capitalize end
-        end#not cycling
+        else
+            @bottles.downto(1) do
+                puts"#{english_number(@bottles)} #{more_than_one?} of beer on the wall,".capitalize
+                puts"#{english_number(@bottles)} #{more_than_one?} of beer,".capitalize
+                puts "Take one down, pass it around,"
+                puts "#{english_number(@bottles-=1)} #{more_than_one?} of beer on the wall.".capitalize 
+            end
+        end#
     end
 
     def english_number(number)
@@ -46,26 +48,34 @@ class BeerSong
             return "zero"
         end
         num_string = ''
-        ones = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
-        tens = ['ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
-        teens = ['eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen,' 'seventeen', 'eighteen', 'nineteen']
-        left = number
-        write = left/10
-        left = left - write*10
-        if write > 0
-            if ((write==1) && (left > 0))
-                num_string = num_string + teens[left-1]
-                left = 0
-            else
-                num_string = num_string + tens[left-1]
-            end
-        end
-        write = left
-        left = 0
-        if write > 0
-            num_string = num_string + ones[write-1]
-        end
-    end    
+        ones_place = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+        tens_place = ['ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+        teenagers = ['eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+        
+       	remaining = number
+    	current = remaining/10
+    	remaining = remaining - current*10
+
+	if current > 0
+		if ((current == 1) and (remaining > 0))
+			num_string = num_string + teenagers[remaining - 1] #this is returning ten-nine etc!
+			remaining = 0
+		else
+			num_string = num_string + tens_place[current - 1]
+		end
+		if remaining > 0
+		num_string = num_string + '-'
+		end
+	end
+
+	current = remaining
+	remaining = 0
+
+	if current > 0
+		num_string = num_string + ones_place[current - 1]
+	end
+	num_string
+    end   
                 
     def more_than_one?
         if @bottles == 1
@@ -76,3 +86,6 @@ class BeerSong
     end
   
 end
+
+song = BeerSong.new(99)
+song.print_song
