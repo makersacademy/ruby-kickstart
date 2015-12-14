f1ef5be9684ac48270fe3ef2d7f57bbf1f3ea10c
seanhawkridge <sean@hawkridge.co>
2015-12-14 22:59:34 +0000

    Question 7

--- session1/3-challenge/7_string.rb
+++ session1/3-challenge/7_string.rb
@@ -5,5 +5,18 @@
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
 def pirates_say_arrrrrrrrr(string)
+	
+    letters = ""
 
+    string.size.times do |i|
+        current_letter = string[i]
+        next_letter = string[i + 1] || ""
+        letters << next_letter if (current_letter == "r" || current_letter == "R")
+    end
+
+    letters
 end
+
+puts pirates_say_arrrrrrrrr("are you really learning Ruby?") # => "eenu"
+puts pirates_say_arrrrrrrrr("Katy Perry is on the radio!")   # => "rya"
+puts pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
