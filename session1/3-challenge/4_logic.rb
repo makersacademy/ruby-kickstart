1eaae380f915cc1cec46f3bc6d3b08cb31e7926f
Godfrey Got-Wai Chiu <ggwc82@gmail.com>
2015-12-14 19:28:30 +0000

    1:4 Completed

--- session1/3-challenge/4_logic.rb
+++ session1/3-challenge/4_logic.rb
@@ -13,6 +13,19 @@
 
 
 def grade(num_books, has_read_books)
+	if num_books < 10 && !has_read_books
+		"D"
+	elsif num_books < 21 && !has_read_books
+		"C"
+	elsif num_books > 20 && !has_read_books
+		"B"
+	elsif num_books < 10 && has_read_books
+		"C"
+	elsif num_books < 21 && has_read_books
+		"B"
+	else num_books > 20 && has_read_books
+		"A"
+	end
 end
 
 
