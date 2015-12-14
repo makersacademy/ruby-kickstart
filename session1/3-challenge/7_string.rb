6e4f84fe0ec40ba154a77a94b74203a2987a2d2d
Godfrey Got-Wai Chiu <ggwc82@gmail.com>
2015-12-14 20:44:21 +0000

    1:7 Completed

--- session1/3-challenge/7_string.rb
+++ session1/3-challenge/7_string.rb
@@ -5,5 +5,12 @@
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
 def pirates_say_arrrrrrrrr(string)
-
+	newArray = string.split ""
+	newString = ""
+	i = 0
+	while i < (string.length - 1)
+		newString += newArray[i + 1] if newArray[i] == "r" || newArray[i] == "R"
+		i += 1
+	end
+	return newString
 end
