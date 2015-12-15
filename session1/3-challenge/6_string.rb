8d211ef8c51eca9b85376caeba2bafbebf5451c0
Mark Hill <futonfutonda@gmail.com>
2015-12-15 00:45:26 +0000

    added q6 session 1

--- session1/3-challenge/6_string.rb
+++ session1/3-challenge/6_string.rb
@@ -4,7 +4,17 @@
 #
 # If you have no idea where to begin, remember to check out the cheatsheets for string and logic/control
 #
-
+# ---- Wants odd "key" numbers, not odd characters as read----
 def odds_and_evens(string, return_odds)
+  length = string.length
+  return_string = []
+  array_even = (0...length).step(2).to_a
+  array_odd = (1...length).step(2).to_a
+  if return_odds
+    array_odd.each{|i| return_string << string[i] }
+  else
+    array_even.each{|i| return_string << string[i]}
+  end
+  return_string.join
 
 end
