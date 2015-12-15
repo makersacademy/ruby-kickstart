e7b29f6eb4353c33d4640d907f3fb9585f39f5c9
Wim Hekkelman <j.hekkelman4@upcmail.nl>
2015-12-15 20:00:47 +0100

    Challenge 1:7

--- session1/3-challenge/7_string.rb
+++ session1/3-challenge/7_string.rb
@@ -5,5 +5,13 @@
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
 def pirates_say_arrrrrrrrr(string)
-
+  counter = 0
+  result = ''
+  while counter <= string.length
+    if (string[counter] == "r" || string[counter] == "R") && counter < (string.length - 1)
+      result += string[counter + 1]
+    end
+    counter += 1
+  end
+  result
 end
