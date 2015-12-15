0739b983b1529ec6ecb95e53ed717a87876e434a
seanhawkridge <sean@hawkridge.co>
2015-12-15 07:03:08 +0000

    Updating Q7 with rough debugging

--- session1/3-challenge/7_string.rb
+++ session1/3-challenge/7_string.rb
@@ -5,13 +5,17 @@
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
 def pirates_say_arrrrrrrrr(string)
-	
+
     letters = ""
 
     string.size.times do |i|
+    	puts "index = #{i}"
         current_letter = string[i]
+        puts "current_char = #{current_letter}"
         next_letter = string[i + 1] || ""
+        puts "letters = #{letters}, next_letter = #{next_letter}"
         letters << next_letter if (current_letter == "r" || current_letter == "R")
+        puts "letters = #{letters}"
     end
 
     letters
