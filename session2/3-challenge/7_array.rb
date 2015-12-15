190ecbd55ccacc94fd99a79b2029b845fb210e5c
Iryna Howarth <irynahowarth@gmail.com>
2015-12-15 17:29:00 +0000

    #7 answer for Section2

--- session2/3-challenge/7_array.rb
+++ session2/3-challenge/7_array.rb
@@ -9,3 +9,7 @@
 # alternate_words("Can't we all get along?")      # => ["Can't", "all", "along"]
 # alternate_words("Elementary, my dear Watson!")  # => ["Elementary", "dear"]
 
+
+def alternate_words(string)
+  string.gsub(/[\!@$#%\^&*()-=_+\[\]:;,.\/<>\?\\\|]/," ").split(' ').each_slice(2).to_a.map{|x| x[0]}
+end
