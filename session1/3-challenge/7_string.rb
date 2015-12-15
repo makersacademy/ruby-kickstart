172413f98a4d13a0801fb560fcd7e44c2ccaff0d
Mark Hill <futonfutonda@gmail.com>
2015-12-15 00:51:59 +0000

    added q.7 session 1

--- session1/3-challenge/7_string.rb
+++ session1/3-challenge/7_string.rb
@@ -5,5 +5,14 @@
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
 def pirates_say_arrrrrrrrr(string)
-
+return_array =[]
+string_array = string.chars
+v = 1
+string_array.each do |i|
+  if i == "r" || i == "R"
+    return_array << string_array[v]
+  end
+  v+=1
+end
+return_array.join
 end
