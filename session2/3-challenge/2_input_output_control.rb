fa912689c4870c2af90c7510b5a15d32b2d96a11
frazerWatson <frazerw@tutanota.com>
2015-12-16 21:20:52 +0000

    session2

--- session2/3-challenge/2_input_output_control.rb
+++ session2/3-challenge/2_input_output_control.rb
@@ -1,4 +1,4 @@
-# Prompt the user for a number, then read it in and print out "hi" that many times
+ # Prompt the user for a number, then read it in and print out "hi" that many times
 #
 # Repeat this process until the user submits "bye", then say "goodbye" and end the program
 # HINT: Check out example 2 if you get stuck
@@ -18,10 +18,43 @@
 # remember you can try your program out with              $ ruby 2_input_output_control.rb
 # and when you think it is correct, you can test it with  $ rake 2:2
 
+
+def input
+	puts "Enter a number > "
+end
+	
+
+
 def hi_hi_goodbye
   # your code here
+  input
 
+  while (line = gets) && (line !~ /bye/)
+
+  	line.to_i.times {print "hi "}
+  	puts input
+  end
+  puts "Goodbye"
 end
+
+
+
+
+
+#def hi_hi_goodbye
+  # your code here
+
+
+
+  #while num = gets.chomp.to_i do
+
+  	#puts "Enter a number > "
+
+  #times(input) puts "hi"
+
+#end
+
+#puts "Enter a number > "
 
 
 
