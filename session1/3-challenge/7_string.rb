a68a382df33c165dff465a81c9c3e0f9c105ddc6
rachelsmithcode <rachelcodesmith@outlook.com>
2015-12-16 20:15:51 +0000

    Session 1, seventh challenge done and checked with no failures returned.

--- session1/3-challenge/7_string.rb
+++ session1/3-challenge/7_string.rb
@@ -5,5 +5,21 @@
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
 def pirates_say_arrrrrrrrr(string)
+  string_array = string.split (//)
+  new_string = []
+  index_pos_r = 0
+  index_pos_after_r = 0
+
+  after_r = string_array.collect {|letter| letter == "r" or letter == "R"}
+
+  string_array.length.times do
+      if after_r[index_pos_r]
+        index_pos_after_r = index_pos_r + 1
+        new_string.push string_array[index_pos_after_r]
+      end
+      index_pos_r += 1
+  end
+
+  return new_string.join("")
 
 end
