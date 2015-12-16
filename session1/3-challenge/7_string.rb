6714adb7c182281b3c5929a755cda38de12bb552
Matthew Drake <matdrake@live.co.uk>
2015-12-16 22:38:45 +0000

    Added solution for 1:7

--- session1/3-challenge/7_string.rb
+++ session1/3-challenge/7_string.rb
@@ -5,5 +5,12 @@
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
 def pirates_say_arrrrrrrrr(string)
-
+  return_string = ""
+  char_array = string.split(//)
+  char_array.each_cons(2) { |char, next_char|
+    if char.downcase == "r"
+      return_string << char_array.to_s[next_char]
+    end
+  }
+  return_string
 end
