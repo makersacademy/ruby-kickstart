02c51c4589349f8c5ddab000e442b9f8808da1a2
ViolaCrellin <viola.crellin@gmail.com>
2015-12-16 23:06:26 +0000

    Updated amended 5_string.rb after being able to check with Rake

--- session1/3-challenge/5_string.rb
+++ session1/3-challenge/5_string.rb
@@ -5,5 +5,5 @@
 # add_more_ruby("Sad times are ahead!")         # => "Happy times are ahead!"
 
 def add_more_ruby(string)
-	string.downcase.gsub("sad", "happy").capitalise
+	string.gsub("sad", "happy").gsub("Sad", "Happy")
 end
