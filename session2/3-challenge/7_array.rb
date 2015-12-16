2dddb16955878a0f13e36e0dcfea29c3be6207fa
Yan-Yi Li <liyanyi@gmail.com>
2015-12-16 14:20:35 +0000

    Completed challenge 2:7

--- session2/3-challenge/7_array.rb
+++ session2/3-challenge/7_array.rb
@@ -9,3 +9,11 @@
 # alternate_words("Can't we all get along?")      # => ["Can't", "all", "along"]
 # alternate_words("Elementary, my dear Watson!")  # => ["Elementary", "dear"]
 
+def alternate_words(string)
+    array = string.split(/[^\w+['’]\w+]+/)
+    output = []
+    for i in 0...array.length
+        output << array[i] if i % 2 == 0
+    end
+    output
+end
