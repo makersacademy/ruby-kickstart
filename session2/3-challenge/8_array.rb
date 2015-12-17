865cf75cefd2964e4b45d38f53e90c1d52dd3803
Rufus Raghunath <rufusraghunath@gmail.com>
2015-12-18 03:37:17 +0800

    Added answer to s2q8

--- session2/3-challenge/8_array.rb
+++ session2/3-challenge/8_array.rb
@@ -6,3 +6,11 @@
 # got_three? ['a', 'a', 'a']  # => true
 # got_three? [1, 2, 1, 1]     # => false
 
+def got_three? (array)
+	array.each.with_index do |num, ind|
+		next if ind <= 1
+		return true if num == array[ind-1] && num == array[ind-2]
+	end
+	false
+end
+
