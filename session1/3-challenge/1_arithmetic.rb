6be4f6f8460919def0b3b735b7873764fd5c3cd5
Flight-of-Dragons <jeremy.barrass@gmail.com>
2015-12-17 00:58:13 +0000

    Challenge 1 complete.

--- session1/3-challenge/1_arithmetic.rb
+++ session1/3-challenge/1_arithmetic.rb
@@ -3,11 +3,14 @@
 #  $ rake 1:1
 
 # Given a number, return 20 less than, that number multiplied by 5
-#
+# 
 # arithmetic1(10)     # => 30
-# arithmetic1(10.5)   # => 32.5
-# arithmetic1(-6)     # => -50
+# arithmeitc1(10.5)   # => 32.5
+# arithmeitc1(-6)     # => -50
 
 def arithmetic1(n)
-end
 
+  n * 5 - 20
+
+  # the code for this method goes in here
+end
