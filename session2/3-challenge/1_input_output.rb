5c3a096b34af5174d821bb271a38da82365ce445
Reiss Johnson <reissjohnson@me.com>
2015-12-17 12:06:14 +0000

    added a solution for 2:1

--- session2/3-challenge/1_input_output.rb
+++ session2/3-challenge/1_input_output.rb
@@ -1,13 +1,21 @@
-# Remember you can test this code with
-#   $ rake 2:1
+puts "please enter two integers..."
 
-# Write a program that reads in two integers typed on the keybaord
-# and outputs their sum, difference, and product
-#
-# Standard input will be like "9 2\n" and will expect you to print
-# "11\n7\n18\n" to standard output.
+a = gets.to_i
+b = gets.to_i
 
-def sum_difference_product
-  # your code goes here
-  
-end
+sum = a + b
+diff = a - b
+product = a * b
+
+print "#{sum}"
+print '\n'
+print "#{diff}"
+print'\n'
+print"#{product}"
+puts'\n'
+
+
+
+
+
+
