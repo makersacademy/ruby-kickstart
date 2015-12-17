69a702cc5cea0f441f533de439c6fb3f4ac06abf
Flight-of-Dragons <jeremy.barrass@gmail.com>
2015-12-17 23:41:38 +0000

    Challenge 7 completed - Session1 completed

--- session1/3-challenge/7_string.rb
+++ session1/3-challenge/7_string.rb
@@ -1,9 +1,35 @@
 # given a string, return the character after every letter "r"
 #
-# pirates_say_arrrrrrrrr("are you really learning Ruby?") # => "eenu"
-# pirates_say_arrrrrrrrr("Katy Perry is on the radio!")   # => "rya"
-# pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
+
 
 def pirates_say_arrrrrrrrr(string)
 
+	a = string.split("")
+	
+	b = Array.new
+
+	a.each_index do | w |
+
+		unless w - 1 == -1
+
+			if a.fetch(w - 1) == "r" 
+
+	    		b << a.fetch(w) 	    				    	
+
+			elsif a.fetch(w - 1) == "R"
+
+	    		b << a.fetch(w)
+
+			end
+
+		end
+		
+	end
+
+	b.join
+
 end
+
+ # puts pirates_say_arrrrrrrrr("are you really learning Ruby?") # => "eenu"
+ # puts pirates_say_arrrrrrrrr("Katy Perry is on the radio!")   # => "rya"
+ # puts pirates_say_arrrrrrrrr("Pirates say arrrrrrrr")        # => "arrrrrrrr"
