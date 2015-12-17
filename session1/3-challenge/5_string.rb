5a8964499273d67e24f415bb28a3752ad2cd639a
Flight-of-Dragons <jeremy.barrass@gmail.com>
2015-12-17 01:20:54 +0000

    Challenge 4 complete.

--- session1/3-challenge/5_string.rb
+++ session1/3-challenge/5_string.rb
@@ -1,8 +1,18 @@
 # Given a string, replace every instance of sad to happy
 # 
-# add_more_ruby("The clowns were sad.")         # => "The clowns were happy."
-# add_more_ruby("The sad dad said sad stuff.")  # => "The happy dad said happy stuff."
-# add_more_ruby("Sad times are ahead!")         # => "Happy times are ahead!"
 
 def add_more_ruby(string)
+
+  a = string
+
+  a.gsub! "sad", "happy"
+
+  a.gsub! "Sad", "Happy"
+
+ "#{a}"
+
 end
+
+ puts add_more_ruby("The clowns were sad.")         # => "The clowns were happy."
+ puts add_more_ruby("The sad dad said sad stuff.")  # => "The happy dad said happy stuff."
+ puts add_more_ruby("Sad times are ahead!")         # => "Happy times are ahead!"
