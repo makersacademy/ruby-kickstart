f030e4e99e7b6550d72f6d1fb27d758c5549f7ec
Vicky Gray <vickygray26@hotmail.com>
2015-12-17 17:17:35 +0000

    Session 2 challenge 7 answer

--- session2/3-challenge/7_array.rb
+++ session2/3-challenge/7_array.rb
@@ -9,3 +9,10 @@
 # alternate_words("Can't we all get along?")      # => ["Can't", "all", "along"]
 # alternate_words("Elementary, my dear Watson!")  # => ["Elementary", "dear"]
 
+def alternate_words(string)
+	result = []
+	array = string.split
+		array.select.with_index do |word, i|
+			result << word if i.odd?
+		end
+end
