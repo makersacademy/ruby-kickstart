a3b269f9da459c3f1b7a9c20f3bbf5961f3e7fa2
Flight-of-Dragons <jeremy.barrass@gmail.com>
2015-12-17 11:03:18 +0000

    Challenge 6 completed.  P.S The second Challenge 4 commit should be Challenge 5.

--- session1/3-challenge/6_string.rb
+++ session1/3-challenge/6_string.rb
@@ -1,10 +1,44 @@
 # You'll get a string and a boolean.
 # When the boolean is true, return a new string containing all the odd characters.
 # When the boolean is false, return a new string containing all the even characters.
-#
+# 
 # If you have no idea where to begin, remember to check out the cheatsheets for string and logic/control
-#
+# 
+# odds_and_evens("abcdefg",true)    # => "bdf"
+# odds_and_evens("abcdefg",false)   # => "aceg"
 
 def odds_and_evens(string, return_odds)
+
+    a = string.split("")
+    
+    b = []
+    
+    if return_odds
+
+        a.each_index do | n |
+
+            if n.to_i.odd?
+
+                b << a.fetch(n) 
+
+            end
+
+        end
+
+    elsif !return_odds
+
+        a.each_index do | n |
+
+           if n.to_i.even?
+
+               b << a.fetch(n) 
+
+            end
+
+        end
+            
+    end
+
+    b.join
 
 end
