2d9aa80e0c758151982f80bedce4d04f20f6de90
Matt <mattpgough@gmail.com>
2015-12-17 16:25:55 +0000

    Updated S2 C7 Solution

--- session2/3-challenge/7_array.rb
+++ session2/3-challenge/7_array.rb
@@ -13,5 +13,3 @@ def alternate_words(string)
 	a = "!@$#%^&*()-=_+[]:;,./<>?\\|".chars
 	(string.gsub(/-/," ").chars - a).join.split(" ").select.with_index{|_,i| i.even?}
 end
-
-print alternate_words "If you want to accomplish something in the world, idealism is not enough--you need to choose a method that works to achieve the goal. In other words, you need to be pragmatic."
