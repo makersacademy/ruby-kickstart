47228740a387581d55d38397ce3e2e4eb55def2c
Wim Hekkelman <j.hekkelman4@upcmail.nl>
2015-12-18 10:06:49 +0100

    Challenge 2:7

--- session2/3-challenge/7_array.rb
+++ session2/3-challenge/7_array.rb
@@ -9,3 +9,34 @@
 # alternate_words("Can't we all get along?")      # => ["Can't", "all", "along"]
 # alternate_words("Elementary, my dear Watson!")  # => ["Elementary", "dear"]
 
+def alternate_words(sentence)
+
+  # put exclude characters in an array
+  to_exclude ='! @ $ # % ^ & * ( ) - = _ + [ ] : ; , . / < > ? \ |'.split(" ")
+
+  # replace those characters with space
+  arr_count = 0
+  while arr_count < to_exclude.length
+    to_exclude.each { |sign| sentence = sentence.gsub(sign, " ") }
+    arr_count += 1
+  end
+
+  # put the words in an array
+  words = sentence.split(" ")
+
+  # let's alternate
+  count = 0
+  result = []
+  while count < words.length do
+      result << words[count] if count.even?
+    count += 1
+  end
+  result
+end
+
+# alternate_words("Lorem ipsum dolor sit amet.")
+# alternate_words("Can't we all get along?")      # => ["Can't", "all", "along"]
+# alternate_words("Elementary, my dear Watson!")  # => ["Elementary", "dear"]
+# alternate_words("Would a dating service on the net be frowned upon . . . ? I hope not. But even if it is, don’t let that stop you from notifying me via net mail if you start one.")
+# alternate_words("is not enough--you need to choose")
+# alternate_words("is not enough--you need . . to choose")
