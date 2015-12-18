13fec1b9213d354811958f753c865016267e85d8
ViolaCrellin <viola.crellin@gmail.com>
2015-12-18 22:18:51 +0000

    Added the 'advanced' version of Deaf Granma from Chris Pine (Did it in preparation for interview)

--- session2/3-challenge/9_input_output_logic_string.rb
+++ session2/3-challenge/9_input_output_logic_string.rb
@@ -24,7 +24,19 @@
 # USER:     BYE
 
 def deaf_grandma
-
+puts "Hhh--elllo dear, my you've grown. Hhh-ow are youuu?"
+bye_count = 0
+	while true
+			user_input = gets.chomp
+			if user_input == "BYE"
+					bye_count += 1
+					puts "HI DEAR, IT'S NICE TO SEE YOU TOO."	
+			else puts user_input =~ /[a-z]/ ? "HUH? SPEAK UP SONNY. MY HEARING AID IS PLAYING UP." : "NO DEAR, NOT SINCE 19#{rand(99)}"
+					bye_count = 0
+			end
+		break if bye_count >= 3
+	end
+puts "OH, BYE BYE SWEETIE."
 end
 
 
