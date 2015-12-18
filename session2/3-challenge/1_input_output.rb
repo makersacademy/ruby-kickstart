85f759a792630f096c23a4ed802748a774e3f8c9
innlouvate <l.j.franklin85@googlemail.com>
2015-12-18 21:46:54 +0000

    q1

--- session2/3-challenge/1_input_output.rb
+++ session2/3-challenge/1_input_output.rb
@@ -8,6 +8,14 @@
 # "11\n7\n18\n" to standard output.
 
 def sum_difference_product
-  # your code goes here
-  
+  puts "Give me an integer"
+  num1 = gets.chomp.to_i
+  puts "Ok give me another number"
+  num2 = gets.chomp.to_i
+  sum = num1 + num2
+  diff = (num1 - num2).abs 
+  prod = num1 * num2
+  print "#{sum} #{diff} #{prod}"
 end
+
+sum_difference_product
