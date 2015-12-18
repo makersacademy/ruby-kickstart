b8309bebce16171735f8fb90e747f9d0feefc1ac
Vicky Gray <vickygray26@hotmail.com>
2015-12-18 18:53:33 +0000

    Session 2 challenge 8 answer

--- session2/3-challenge/8_array.rb
+++ session2/3-challenge/8_array.rb
@@ -6,3 +6,16 @@
 # got_three? ['a', 'a', 'a']  # => true
 # got_three? [1, 2, 1, 1]     # => false
 
+
+# returns true if element appears three times...needs to be consecutive
+# def got_three?(array)
+# 	true if array.length.to_i-2 == array.uniq.length.to_i
+# end
+
+
+def got_three?(array)
+	# .each iterates the block for an array of consecutive elements - define the number in ()
+	array.each_cons (3) do | x, y, z| 
+		return true if x == y && y == z
+	end
+end
