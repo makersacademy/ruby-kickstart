25a2ce551e112648c7d91899e42114342bd48402
Wim Hekkelman <j.hekkelman4@upcmail.nl>
2015-12-19 15:00:24 +0100

    Challenge 2:13

--- session2/3-challenge/13_classes_iterating_sorting.rb
+++ session2/3-challenge/13_classes_iterating_sorting.rb
@@ -66,3 +66,104 @@
 # date docs are at: http://ruby-doc.org/core/classes/Date.html
 # don't spend too much time worrying about them :)
 require 'date'
+
+
+
+class User
+
+  def initialize(username)
+    @username = username
+    @blogs = []
+  end
+  attr_accessor 'username', 'blogs'
+
+  def add_blog(date,text)
+    new_blog = Blog.new(date,self.username,text)
+    @blogs << new_blog
+    sort_blogs
+    new_blog
+  end
+
+  def sort_blogs
+    @blogs = @blogs.sort_by {|blog| blog.date}.reverse
+
+  end
+end
+
+class Blog
+
+  def initialize(date, user, text)
+    self.date = date
+    self.user = user
+    self.text = text
+  end
+  attr_accessor 'date'
+  attr_accessor 'user'
+  attr_accessor 'text'
+
+  def summary
+    result = @text.split(" ")
+    if result.length < 10
+      result
+    else
+      result[0..9].join(" ")
+    end
+  end
+
+  def entry
+    "#{@user.username} #{@date}\n#{@text}"
+  end
+
+  def ==(other)
+    date   == other.date &&
+    user == other.user &&
+    text == other.text
+  end
+end
+
+#user = User.new 'QTSort'
+#blog = user.add_blog(Date.today, "text")
+#p blog
+#p user.blogs.first
+# lissa = User.new 'QTSort'
+# lissa.username                  # => "QTSort"
+# lissa.blogs                     # => []
+#
+# lissa.add_blog Date.parse("2010-05-28") , "Sailor Mars is my favourite"
+# lissa.blogs                     # => [ blog1 ]
+#
+# blog1 = lissa.blogs.first
+# blog1.user                      # => lissa
+#
+#blog2 = lissa.add_blog Date.parse("2007-01-02"), "Going dancing!"
+#blog3 = lissa.add_blog Date.parse("2006-01-02"), "For the last time, fuck facebook >.<"
+# blog4 = lissa.add_blog Date.parse("2010-01-02"), "Got a new job, cuz I'm pretty much the best ^_^"
+# lissa.blogs                     # => [ blog1 , blog4 , blog2 , blog3 ]
+# lissa.blogs.first
+#
+# blog5 = Blog.new Date.today, lissa, <<BLOG_ENTRY
+# Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce orci nunc, porta non tristique eu, auctor tincidunt mauris.
+# Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Etiam vitae nibh sapien. Curabitur
+# eget eros bibendum justo congue auctor non at turpis. Aenean feugiat vestibulum mi ac pulvinar. Fusce ut felis justo, in
+# porta lectus.
+#BLOG_ENTRY
+
+#p lissa.blogs
+#
+#p blog5.summary   # => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce orci"
+#puts blog5.entry         # => QTSort 2010-05-28
+#                          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce orci nunc, porta non tristique eu, auctor tincidunt mauris.
+#                          Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Etiam vitae nibh sapien. Curabitur
+#                          eget eros bibendum justo congue auctor non at turpis. Aenean feugiat vestibulum mi ac pulvinar. Fusce ut felis justo, in
+#                          porta lectus.
+#
+# blog5.date = Date.parse('2009-01-02')
+# blog5.user = User.new 'disloyalist.party'
+# blog5.text = "From the school of revision, Comes the standard inventor's rule, Books of subtle notation Compositions, all original\n" \
+#              "I am a pioneer, synthetic engineer, On the brink of discovery, On the eve of historic light, Worked in secret for decades,\n" \
+#              "All my labor will be lost with time"
+#
+#puts blog5.entry      # => disloyalist.party 2009-01-02
+#                       From the school of revision, Comes the standard inventor's rule, Books of subtle notation Compositions, all original
+#                       I am a pioneer, synthetic engineer, On the brink of discovery, On the eve of historic light, Worked in secret for decades,
+#                       All my labor will be lost with time
