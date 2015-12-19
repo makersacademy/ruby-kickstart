315055360a7e3bf6cd6ee12f096fd00e8d24e115
Denys Triasunov <dendden@users.noreply.github.com>
2015-12-19 23:13:22 +0200

    Completed 5_array.rb

--- session2/3-challenge/5_array.rb
+++ session2/3-challenge/5_array.rb
@@ -14,3 +14,11 @@
 #
 # mod_three [0,1,2,3,4,5,6,7] # => [1, 2, 1, 2, 1]
 
+def mod_three(array)
+  result = []
+  array.each do |elem|
+    result << (elem % 3) if (elem % 3) != 0
+  end
+
+  result
+end
