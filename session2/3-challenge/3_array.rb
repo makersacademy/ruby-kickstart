c3408539bb3ed882aaa08ead1892f1409276ae09
Mark Hill <futonfutonda@gmail.com>
2015-12-19 18:00:32 +0000

    2:3 added

--- session2/3-challenge/3_array.rb
+++ session2/3-challenge/3_array.rb
@@ -7,6 +7,15 @@
 
 class String
   def every_other_char
+    char_array=[]
+    i = 1
+    each_char do |x|
+      if i.odd?
+        char_array << x
+      end
+    i +=1
   end
-  
+char_array.join
+  end
+
 end
