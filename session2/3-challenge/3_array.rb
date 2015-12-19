46c9784059602e55f07e524e70f44fe8ff885ee8
Ina Tsetsova <inatsetsova@gmail.com>
2015-12-19 09:42:01 +0000

    solved 3.3

--- session2/3-challenge/3_array.rb
+++ session2/3-challenge/3_array.rb
@@ -7,6 +7,14 @@
 
 class String
   def every_other_char
+    if self.length%2 !=0
+      self << " "
+    end
+    arrays = self.split("").each_slice(2).to_a
+    arrays.each do |array|
+    array.pop
   end
-  
+  arrays.join
+  end
 end
+
