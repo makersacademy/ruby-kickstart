2b3fff8f99dfff1423781e46533bd96acbc66986
Alain Lemaire <jaxdid@gmail.com>
2015-12-20 20:14:18 +1100

    Answered Challenge 2-6

--- session2/3-challenge/6_array.rb
+++ session2/3-challenge/6_array.rb
@@ -14,4 +14,14 @@
 # prime_chars? ['ab', 'cd']       # => false
 # prime_chars? ['a', 'bcd']       # => false
 # prime_chars? ['a', 'b', 'cd']   # => false
+require 'prime'
 
+def prime_chars?(arr)
+  len = 0
+  
+  arr.each do |x|
+    len += x.length
+  end
+
+  Prime.prime?(len)
+end
