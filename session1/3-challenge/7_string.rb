29ef35ceca9726baf0aaac6a41246fb0b7b69756
Heather Campbell <campbell.hf@gmail.com>
2015-12-20 20:21:08 +0000

    1:7 answer

--- session1/3-challenge/7_string.rb
+++ session1/3-challenge/7_string.rb
@@ -5,5 +5,21 @@
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
 def pirates_say_arrrrrrrrr(string)
+	#string_array = string.split("")
+	#string_array.each { |string| "r" +1}
+	#puts string_array 
 
+	string_array = string.split("")
+	new_array = []
+	string_array.each_index do |x| 
+		unless x - 1 < 0
+		if string_array.fetch(x - 1) == 'r'
+			new_array << string_array.fetch(x) 
+		elsif string_array.fetch(x - 1) == 'R' 
+			new_array << string_array.fetch(x) 
+		end
+	end
+	end
+	new_array.join
 end
+puts pirates_say_arrrrrrrrr 'are you really learning Ruby?'
