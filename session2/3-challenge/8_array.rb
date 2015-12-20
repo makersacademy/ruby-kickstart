92ba78b738ccc38d80005900460c7e6b3d7e9dc0
Flight-of-Dragons <jeremy.barrass@gmail.com>
2015-12-20 12:57:44 +0000

    s2c8 complete

--- session2/3-challenge/8_array.rb
+++ session2/3-challenge/8_array.rb
@@ -6,3 +6,25 @@
 # got_three? ['a', 'a', 'a']  # => true
 # got_three? [1, 2, 1, 1]     # => false
 
+def got_three? (ary)
+
+	threeInARow = false
+
+	ary.each_index do |i| 
+
+		unless i - 2 < 0 || i - 1 < 0 
+
+			threeInARow = true if ary.fetch(i) == ary.fetch(i - 2) && ary.fetch(i) == ary.fetch(i - 1) 
+
+		end
+
+	end
+
+	threeInARow
+
+end
+
+puts got_three? [1, 2, 2, 2, 3]  # => true
+puts got_three? ['a', 'a', 'b']  # => false
+puts got_three? ['a', 'a', 'a']  # => true
+puts got_three? [1, 2, 1, 1]     # => false
