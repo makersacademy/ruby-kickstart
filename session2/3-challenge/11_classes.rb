f4c92103deef94283191446f7743a0a5d90d5188
Denys Triasunov <dendden@users.noreply.github.com>
2015-12-20 14:46:02 +0200

    Completed 11_classes.rb (rake complains smth about 'entrie song' but I can't find the problem)

--- session2/3-challenge/11_classes.rb
+++ session2/3-challenge/11_classes.rb
@@ -20,3 +20,84 @@
 # if the parameter is greater than 99, set the number of beer bottles to 99
 # Then make a public method called print_song that outputs all stanzas from the number of bottles of beer down to zero.
 # Add any additional methods you find helpful.
+
+class BeerSong
+  attr_reader :bottles
+
+  def initialize(bottles)
+    if bottles > 99
+      @bottles = 99
+    elsif bottles < 0
+      @bottles = 0
+    else
+      @bottles = bottles
+    end
+  end
+
+  @@capitals = {
+  0 => "Zero",
+  1 => "One",
+  2 => "Two",
+  3 => "Three",
+  4 => "Four",
+  5 => "Five",
+  6 => "Six",
+  7 => "Seven",
+  8 => "Eight",
+  9 => "Nine",
+  10 => "Ten",
+  11 => "Eleven",
+  12 => "Twelve",
+  13 => "Thirteen",
+  14 => "Fourteen",
+  15 => "Fifteen",
+  16 => "Sixteen",
+  17 => "Seventeen",
+  18 => "Eighteen",
+  19 => "Nineteen",
+  20 => "Twenty",
+  30 => "Thirty",
+  40 => "Fourty",
+  50 => "Fifty",
+  60 => "Sixty",
+  70 => "Seventy",
+  80 => "Eighty",
+  90 => "Ninety"
+}
+
+@@digits = {
+  1 => "one",
+  2 => "two",
+  3 => "three",
+  4 => "four",
+  5 => "five",
+  6 => "six",
+  7 => "seven",
+  8 => "eight",
+  9 => "nine"
+}
+
+def num2words(num)
+  return @@capitals[num] if num <= 20
+  ones = num % 10
+  tens = num - ones
+  if ones == 0
+    @@capitals[tens]
+  else
+    @@capitals[tens]+'-'+@@digits[ones]
+  end
+end
+
+  def print_song
+    @bottles.downto(1) do |bottles_left|
+      puts "#{num2words(bottles_left)} #{bottles_left == 1 ? "bottle" : "bottles"} of beer on the wall,"
+      puts "#{num2words(bottles_left)} #{bottles_left == 1 ? "bottle" : "bottles"} of beer,"
+      puts "Take one down, pass it around,"
+      puts "#{num2words(bottles_left-1)} #{(bottles_left-1) == 1 ? "bottle" : "bottles"} of beer on the wall."
+
+    end
+  end
+end
+
+bs = BeerSong.new 32
+bs.print_song
