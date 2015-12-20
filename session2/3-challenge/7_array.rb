ef682da88bd4d46d68405d60087bad942e01f846
Flight-of-Dragons <jeremy.barrass@gmail.com>
2015-12-20 12:18:47 +0000

    s2c7 complete

--- session2/3-challenge/7_array.rb
+++ session2/3-challenge/7_array.rb
@@ -1,11 +1,35 @@
 # Given a sentence, return an array containing every other word.
 # Punctuation is not part of the word unless it is a contraction.
-# In order to not have to write an actual language parser, there won't be any punctuation too complex.
+# In order to not have to write an actual language parser, there won't be any punctuation 
+# too complex.
 # There will be no "'" that is not part of a contraction.
-# Assume each of these  charactsrs are not to be considered: ! @ $ # % ^ & * ( ) - = _ + [ ] : ; , . / < > ? \ |
+# Assume each of these  charactsrs are not to be considered: ! @ $ # % ^ & * ( ) - = _ + [ ] 
+# : ; , . / < > ? \ |
 #
-# Examples
-# alternate_words("Lorem ipsum dolor sit amet.")  # => ["Lorem", "dolor", "amet"]
-# alternate_words("Can't we all get along?")      # => ["Can't", "all", "along"]
-# alternate_words("Elementary, my dear Watson!")  # => ["Elementary", "dear"]
+def alternate_words (string)
 
+	a = string.gsub(/[^'|’|\w|\s]/," ")
+
+	# puts string
+
+	# puts "\n"
+
+	b = a.split(" ")
+
+	# puts "#{a}\n"
+
+	c = []
+	
+	b.each_index {|i| c << b.fetch(i) if i.even?}
+
+	c
+
+end
+
+
+# puts alternate_words("Lorem ipsum dolor sit amet.")  # => ["Lorem", "dolor", "amet"]
+# puts alternate_words("Can't we all get along?")      # => ["Can't", "all", "along"]
+# puts alternate_words("Elementary, my dear Watson!")  # => ["Elementary", "dear"]
+# puts alternate_words("If you want to accomplish something in the world, idealism is not enough--you need to choose a method that works to achieve the goal. In other words, you need to be pragmatic.")
+# puts alternate_words("Can't we all get along?")
+# puts alternate_words("Would a dating service on the net be frowned upon . . . ? I hope not. But even if it is, don’t let that stop you from notifying me via net mail if you start one.")
