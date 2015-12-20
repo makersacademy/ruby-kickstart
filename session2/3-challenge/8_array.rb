f702958f9fbecf99b1ab2b0c5aebcf308f48b5a6
Denys Triasunov <dendden@users.noreply.github.com>
2015-12-20 12:02:31 +0200

    Completed 8_array.rb

--- session2/3-challenge/8_array.rb
+++ session2/3-challenge/8_array.rb
@@ -6,3 +6,13 @@
 # got_three? ['a', 'a', 'a']  # => true
 # got_three? [1, 2, 1, 1]     # => false
 
+def got_three?(array)
+  three_in_a_row = false
+  elems_limit = array.size - 3
+  return three_in_a_row if elems_limit < 0
+  for index in 0..elems_limit
+    three_in_a_row = ( (array[index] == array[index+1]) && (array[index] == array[index+2]) )
+    break if three_in_a_row
+  end
+  three_in_a_row
+end
