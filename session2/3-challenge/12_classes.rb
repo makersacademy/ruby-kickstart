dd4a36774e186203e4b37ec406e2257603f0f47a
Vicky Gray <vickygray26@hotmail.com>
2015-12-21 12:24:33 +0000

    Session 2 challenge 12 answer

--- session2/3-challenge/12_classes.rb
+++ session2/3-challenge/12_classes.rb
@@ -23,8 +23,30 @@
 # f.to_f               # => 0.5
 
 class Fraction
-  def gcd(a,b)
-    return a if b == 0
-    gcd(b, a%b)
-  end
+
+	attr_accessor :numerator, :denominator
+
+	def initialize(rand_int_1, rand_int_2)
+		self.numerator = rand_int_1
+		self.denominator = rand_int_2
+	end
+
+	def to_f
+		numerator/denominator.to_f
+	end
+
+  	def gcd(a,b)
+    	return a if b == 0
+    	gcd(b, a%b)
+  	end
+
+	def lowest
+		divisor = gcd(numerator, denominator)
+		Fraction.new(numerator/divisor, denominator/divisor)
+	end
+
+  	def to_s
+		"#{numerator}/#{denominator}"
+  	end
+
 end
