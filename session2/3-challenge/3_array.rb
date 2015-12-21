c4ebcf5b00ecd8ec07d2b29330608fb90cbac3dd
ZeshanRasul <zeshanrasul90@gmail.com>
2015-12-21 03:02:51 +0000

    Added session 2, challenge 3 solution

--- session2/3-challenge/3_array.rb
+++ session2/3-challenge/3_array.rb
@@ -7,6 +7,18 @@
 
 class String
   def every_other_char
+    other_char = []
+    new_string = self.split("")
+    new_string.each_with_index do |item, index|
+        if (index % 2 == 0) then
+          other_char.push(item)
+        end
+    end
+    if new_string.length > 0
+      newer_string = other_char.join("")
+      newer_string
+    else
+       ""
+    end
   end
-  
 end
