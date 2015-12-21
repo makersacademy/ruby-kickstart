20769379249330c61de647aeab7417f0fae228bc
ZeshanRasul <zeshanrasul90@gmail.com>
2015-12-21 03:38:59 +0000

    Added session 2, challenge 12 solution

--- session2/3-challenge/12_classes.rb
+++ session2/3-challenge/12_classes.rb
@@ -23,8 +23,40 @@
 # f.to_f               # => 0.5
 
 class Fraction
+
+  attr_accessor "numerator", "denominator"
+
+  def initialize (numerator, denominator)
+    @numerator = numerator
+    @denominator = denominator
+  end
+
+  def numerator
+    numerator = @numerator
+  end
+
+  def denominator
+    denominator = @denominator
+  end
+
   def gcd(a,b)
     return a if b == 0
     gcd(b, a%b)
   end
+
+  def to_f
+    @numerator.to_f / @denominator.to_f
+  end
+
+  def lowest
+    top =  @numerator / gcd(@numerator, @denominator)
+    bottom =  @denominator / gcd(@numerator, @denominator)
+    frac = Rational(top, bottom)
+    frac
+  end
+
+  def to_s
+    "#{@numerator}/#{@denominator}"
+  end
+
 end
