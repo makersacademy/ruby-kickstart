9a205dab3941c983efd695bc5d4608fabfb04805
ZeshanRasul <zeshanrasul90@gmail.com>
2015-12-21 03:01:57 +0000

    Added session 2, challenge 2 solution

--- session2/3-challenge/2_input_output_control.rb
+++ session2/3-challenge/2_input_output_control.rb
@@ -19,14 +19,22 @@
 # and when you think it is correct, you can test it with  $ rake 2:2
 
 def hi_hi_goodbye
-  # your code here
-
+  response = " "
+  while response != "bye"
+    puts "Enter a number"
+    response = gets.chomp
+    if response != "bye"
+      puts "hi " * response.to_i
+    end
+    if response == "bye"
+      puts "goodbye"
+    end
+  end
 end
-
-
 
 
 # This will just invoke the method if you run this program directly
 # This way you can try it out by running "$ ruby 2_input_output_control.rb"
 # but it will still work for our tests
+
 hi_hi_goodbye if $0 == __FILE__
