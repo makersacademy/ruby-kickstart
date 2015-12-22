620a12325480772e28d3e382c14a5e795f5df12d
Marion <marion.levisage@gmail.com>
2015-12-22 16:50:36 +0100

    Added answer to question 6

--- session2/3-challenge/6_array.rb
+++ session2/3-challenge/6_array.rb
@@ -15,3 +15,22 @@
 # prime_chars? ['a', 'bcd']       # => false
 # prime_chars? ['a', 'b', 'cd']   # => false
 
+
+# prime_chars? ['abcd', 'efgh', 'i', 'jk'] EXAMPLE
+
+def prime_chars?(array)
+
+  n = array.join.length # => n = 13
+
+  num_array = *(1..n) # => num_array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+  num_array.select! {|divisor| n % divisor == 0} # => [1, 13]
+    # (so I've selected from the num_array all the numbers that go into 13 (n) with no remainder - hmmm, not so many now!)
+  if num_array.size == 2 # the size of num_array now = 2
+    true # YES
+  elsif n < 2
+    false # NO
+  else
+    false # NO
+end
+end
