9f34390e3b315cde76a3f534ee17cf80798dcddc
arnold manzano <arnold.manzano@gmail.com>
2015-12-22 02:08:30 +0000

    Answered challenge 2:2

--- session2/3-challenge/2_input_output_control.rb
+++ session2/3-challenge/2_input_output_control.rb
@@ -19,12 +19,18 @@
 # and when you think it is correct, you can test it with  $ rake 2:2
 
 def hi_hi_goodbye
-  # your code here
 
+  input = ''
+
+  until input == "bye" do
+    puts "Enter a number"
+    input = gets.chomp
+    input.to_i.times { print "hi " }
+    print "\n"
+  end
+
+  puts "goodbye"
 end
-
-
-
 
 # This will just invoke the method if you run this program directly
 # This way you can try it out by running "$ ruby 2_input_output_control.rb"
