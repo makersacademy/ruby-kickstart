523b82283fe0f7ead879333ae3e489ac780f4b14
Flight-of-Dragons <jeremy.barrass@gmail.com>
2015-12-24 21:55:38 +0000

    s2c1 corrected - 1st attempt not checked in rake!

--- session2/3-challenge/1_input_output.rb
+++ session2/3-challenge/1_input_output.rb
@@ -9,11 +9,13 @@
 
 def sum_difference_product
   # your code goes here
-  n1 = gets[0].to_i
+  a = gets.split(" ")
 
-  n2 = gets[2].to_i
+  n1 = a[0].to_i
 
-  puts "#{n1 + n2}\n#{n1 > n2 ? n1 - n2 : n2 - n1}\n#{n1 * n2}\n"
+  n2 = a[1].to_i
+
+  puts "#{n1 + n2}\n#{n1 - n2}\n#{n1 * n2}\n"
 end
 
 # puts sum_difference_product "3 5\n"
