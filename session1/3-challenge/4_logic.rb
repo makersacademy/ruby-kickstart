ba0086b234cee3361e9c2cba5b915dc9d7825486
Marco Bagnasco <admin@hecticmojo.com>
2015-12-30 12:06:09 +0000

    1:4 alt_solutions

--- session1/3-challenge/4_logic.rb
+++ session1/3-challenge/4_logic.rb
@@ -13,19 +13,6 @@
 
 
 def grade(num_books, has_read_books)
-  if num_books < 10 && !has_read_books
-    "D"
-  elsif num_books < 10 && has_read_books
-    "C"
-  elsif (num_books >= 10 && num_books <= 20) && !has_read_books
-    "C"
-  elsif (num_books >= 10 && num_books <= 20) && has_read_books
-    "B"
-  elsif num_books > 20 && !has_read_books
-    "B" 
-  else
-    "A"
-   end
 end
 
 
