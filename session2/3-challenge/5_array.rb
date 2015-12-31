d15aa9927d2b2525270f493815d8829690801b6f
BjoernWagner <bjoern.m.wagner@gmail.com>
2015-12-31 14:56:42 +0100

    Session2 Excercise5 Understood

--- session2/3-challenge/5_array.rb
+++ session2/3-challenge/5_array.rb
@@ -2,6 +2,12 @@
 # and return a new array consisting of their remainder when divided by three.
 # Exclude any numbers which are actually dividible by three.
 #
+# Schreibe eine Funktion namens mod_three welche ein
+# Array von Zahlen aufnimmt
+# und gib ein neues Array zurück, das aus dem Restbetrag
+# nach Division durch drei besteht.
+# Lasse alle Zahlen weg, die durch drei teilbar sind.
+#
 # EXAMPLES:
 # mod_three [0]  # => []
 # mod_three [1]  # => [1]
@@ -14,3 +20,6 @@
 #
 # mod_three [0,1,2,3,4,5,6,7] # => [1, 2, 1, 2, 1]
 
+def mod_three(numbers)
+  numbers.select {|number| number % 3 != 0}.map {|number| number % 3}
+end
