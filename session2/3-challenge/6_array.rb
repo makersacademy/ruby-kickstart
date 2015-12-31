3458aa520203ee6d4f42e0ad44598642f98dda43
BjoernWagner <bjoern.m.wagner@gmail.com>
2015-12-31 15:40:24 +0100

    Session2 Excercise6 Understood

--- session2/3-challenge/6_array.rb
+++ session2/3-challenge/6_array.rb
@@ -3,6 +3,12 @@
 #
 # Remember that a number is prime if the only integers that can divide it with no remainder are 1 and itself.
 #
+# Schreibe eine Methode prime_chars? , die ein Array von Zeichenketten
+# aufnimmt und den Wert true zurückgibt, wenn die Summe eine Primzahl ist.
+#
+# Beachte: Primzahlen sind Zahlen, deren einzige Ganzzahlteiler (Integer) 1
+# und sie selbst sind. Also % == 0 nur bei 1 und sich selbst.
+#
 # Examples of length three
 # prime_chars? ['abc']            # => true
 # prime_chars? ['a', 'bc']        # => true
@@ -15,3 +21,19 @@
 # prime_chars? ['a', 'bcd']       # => false
 # prime_chars? ['a', 'b', 'cd']   # => false
 
+
+class Integer
+
+  def prime?
+
+    return false if self < 2
+    2.upto Math.sqrt(self) do |i|
+      return false if self % i == 0
+    end
+    true
+  end
+end
+
+def prime_chars?(strings)
+  strings.join.length.prime?
+end
