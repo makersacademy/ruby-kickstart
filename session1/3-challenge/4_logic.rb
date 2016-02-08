edb405dc8375e4a7a5a5540403505229a4c24210
sitypop <sityemilyshah@gmail.com>
2016-02-08 15:56:42 +0000

    Adding 4_logic.rb

--- session1/3-challenge/4_logic.rb
+++ session1/3-challenge/4_logic.rb
@@ -11,8 +11,25 @@
 
 # <10 books => D, 10..20 books => C, >20 book =>B
 
-
 def grade(num_books, has_read_books)
+	if has_read_books == true
+		if num_books < 10
+			"C"
+		elsif num_books > 20
+			"A"
+		else
+			"B"
+		end
+	else
+		if num_books < 10
+			"D"
+		elsif num_books > 20
+			"B"
+		else
+			"C"
+		end
+	end
+
 end
 
 
