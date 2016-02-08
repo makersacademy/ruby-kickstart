d16acd8da8b03665219f05cbc15a46aab38c8260
NickMountjoy <nick.mountjoy98@gmail.com>
2016-02-08 20:09:52 +0000

    Answer Q 5

--- session1/3-challenge/5_string.rb
+++ session1/3-challenge/5_string.rb
@@ -5,4 +5,9 @@
 # add_more_ruby("Sad times are ahead!")         # => "Happy times are ahead!"
 
 def add_more_ruby(string)
+	string.match "sad" or "Sad"
+	string.gsub(/sad/, "happy").gsub(/Sad/, "Happy")
 end
+
+
+puts add_more_ruby("The clowns were sad.") 
