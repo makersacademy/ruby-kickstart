6ba4e09c6ea2ea78cd107e82334c0495af93e685
Tobenna <tobennandu@gmail.com>
2016-02-09 14:01:14 +0000

    2:5 challenge complete and refactored

--- session2/3-challenge/5_array.rb
+++ session2/3-challenge/5_array.rb
@@ -14,3 +14,8 @@
 #
 # mod_three [0,1,2,3,4,5,6,7] # => [1, 2, 1, 2, 1]
 
+def mod_three(arr)
+  arr.collect{|num| num%3 }.select{|num| num > 0}
+end
+
+# Note that array collect/map is the method I need to do something to every member of an array
