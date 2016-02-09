c2b867cba289fa6d215814594f5838384c947bef
Junyuan Xue <junyuanxue.is@gmail.com>
2016-02-09 20:54:33 +0000

    Updated challenge 1:4

--- session1/3-challenge/4_logic.rb
+++ session1/3-challenge/4_logic.rb
@@ -13,6 +13,23 @@
 
 
 def grade(num_books, has_read_books)
+  if num_books < 10
+    if has_read_books == true
+      "C"
+    else
+      "D"
+    end
+  elsif num_books >= 10 && num_books <= 20
+    if has_read_books == true
+      "B"
+    else
+      "C"
+    end
+  else
+    if has_read_books == true
+      "A"
+    else
+      "B"
+    end
+  end
 end
-
-
