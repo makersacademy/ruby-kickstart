f304c2ba44af68b4df20f706a3929f93c7fe5aa9
Tobenna <tobennandu@gmail.com>
2016-02-10 00:27:08 +0000

    2:13 challenge complete and refactored

--- session2/3-challenge/13_classes_iterating_sorting.rb
+++ session2/3-challenge/13_classes_iterating_sorting.rb
@@ -17,8 +17,6 @@
 #        return self.date == other.date
 #      end
 
-
-
 # ==========  EXAMPLE  ==========
 #
 # lissa = User.new 'QTSort'
@@ -61,8 +59,40 @@
 #                       I am a pioneer, synthetic engineer, On the brink of discovery, On the eve of historic light, Worked in secret for decades,
 #                       All my labor will be lost with time
 
-
-
 # date docs are at: http://ruby-doc.org/core/classes/Date.html
 # don't spend too much time worrying about them :)
 require 'date'
+
+class User
+  attr_accessor :username, :blogs
+  def initialize(username)
+    @username = username
+    @blogs = []
+  end
+  def add_blog(date, text)
+    blog = Blog.new(date, self, text)
+    blogs << blog
+    self.blogs = blogs.sort_by { |blog| blog.date }.reverse
+    blog
+  end
+end
+
+class Blog
+  attr_accessor :date, :user, :text
+
+  def initialize(date, user, text)
+    @date = date
+    @user = user
+    @text = text
+  end
+  def summary
+    text.split[0..9].join(' ')
+  end
+
+  def entry
+    "#{user.username} #{date}\n#{text}"
+  end
+  def ==(other)
+    date == other.date && user == other.user && text == other.text
+  end
+end
