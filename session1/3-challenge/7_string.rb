b20e4b73502ddfb2c40a53991752d7fad4cbee37
adilw3nomad <adiln1993@gmail.com>
2016-02-10 08:10:06 +0000

    answer 1:7 with comments

--- session1/3-challenge/7_string.rb
+++ session1/3-challenge/7_string.rb
@@ -5,5 +5,9 @@
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
 def pirates_say_arrrrrrrrr(string)
-
+    return_string = ""  #empty string to put every character after "r"
+    (string.size-1).times do |index| # I minus the string size by 1 incase the last character of the string is "r", in which case string[index+1] would return nil
+        return_string << string[index+1] if string[index] == "r" || string[index] == "R" #check to see if current character is "r" or "R", if true then adds the following character to empty string
+    end
+    return_string #returns string with characters after "r"/"R"
 end
