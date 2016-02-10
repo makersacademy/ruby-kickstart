e4c1772d66f7ef062093d19a885d2d798e1bb5a5
Junyuan Xue <junyuanxue.is@gmail.com>
2016-02-10 22:11:18 +0000

    Updated challenge 2:6

--- session2/3-challenge/6_array.rb
+++ session2/3-challenge/6_array.rb
@@ -15,3 +15,19 @@
 # prime_chars? ['a', 'bcd']       # => false
 # prime_chars? ['a', 'b', 'cd']   # => false
 
+def prime_chars? str_arr
+  num = str_arr.join.length
+  if num == 1 || num == 0
+    return false
+  elsif num == 2
+    true
+  else
+    for x in 2..(num - 1)
+      if (num % x) == 0
+        return false
+      else
+        true
+      end
+    end
+  end
+end
