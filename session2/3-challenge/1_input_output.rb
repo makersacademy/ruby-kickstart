bdee032456b2c19756fdf2efc0d21ed0aa0263b7
Tobenna <tobennandu@gmail.com>
2016-02-10 20:09:41 +0000

    Session 2 review

--- session2/3-challenge/1_input_output.rb
+++ session2/3-challenge/1_input_output.rb
@@ -7,10 +7,19 @@
 # Standard input will be like "9 2\n" and will expect you to print
 # "11\n7\n18\n" to standard output.
 
-def sum_difference_product
+def sum_difference_product2
   # your code goes here
   first , second = gets.split.map { |num| num.to_i }
   print "#{first+second}\n#{first-second}\n#{first*second}\n"
 end
 
 #it's cool that you can assign variable using an array.
+def sum_difference_product
+   input = gets.chomp
+   a = input.split[0].to_i
+   b = input.split[1].to_i
+   sum = a + b
+   dif = a - b
+   pro = a * b
+   print "#{sum}\n#{dif}\n#{pro}\n"
+end
--- session2/3-challenge/8_array.rb
+++ session2/3-challenge/8_array.rb
@@ -5,35 +5,6 @@
 # got_three? ['a', 'a', 'b']  # => false
 # got_three? ['a', 'a', 'a']  # => true
 # got_three? [1, 2, 1, 1]     # => false
-
-def got_threee?(arr)
-  count = 1
-  prev = nil
-  arr.each do |value|
-    if value == prev
-      count += 1
-      return true if count == 3
-    else
-      count = 1
-    end
-    prev = value
-  end
-  false
-end
-
-def got_threeee?(arr)
-  retval = false
-  result = []
-  arr.each_cons(3) { |a, b,c| result << [a, b,c] }
-result.length.times do
-  result.each do |value|
-    retval = value.uniq.length == 1 ? true : false
-    return true if retval == true
-  end
-end
-  retval
-end
-
 def got_three?(arr)
   arr.each_cons(3) do |a,b,c|
     return true if a == b && b == c
