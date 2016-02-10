453b765196cae69cfcb84a87be7ffeba3a4c9d22
eripheebs <erikapheby@gmail.com>
2016-02-10 15:23:01 +0000

    Session 1 Challenge 7 Answered

--- session1/3-challenge/7_string.rb
+++ session1/3-challenge/7_string.rb
@@ -5,5 +5,13 @@
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
 def pirates_say_arrrrrrrrr(string)
+	final_string = ""
+	add_next = false
+	string.length.times do |index|
+		current_char = string[index]
+		final_string << current_char if add_next
+		add_next = (current_char == "r" || current_char == "R")
+	end
 
+	final_string
 end
