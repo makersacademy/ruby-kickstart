310cd376ab5d9429ddecdf68d49173ddbb1cd904
NickMountjoy <nick.mountjoy98@gmail.com>
2016-02-10 11:22:57 +0000

    Answer Q 7

--- session1/3-challenge/7_string.rb
+++ session1/3-challenge/7_string.rb
@@ -4,6 +4,30 @@
 # pirates_say_arrrrrrrrr("Katy Perry is on the radio!")   # => "rya"
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
-def pirates_say_arrrrrrrrr(string)
 
+def pirates_say_arrrrrrrrr(string)
+    to_return = ""
+
+    string.size.times do |index|
+        current_char = string[index]
+        next_char = string[index + 1] || ''
+
+        to_return << next_char if (current_char == "r" || current_char == "R")
+    end
+
+    to_return
 end
+
+
+
+
+#def pirates_say_arr(string)
+  #string.gsub(/(?<=!r)./i, '')
+#end
+
+
+
+#def pirates_say_arrrrrrrrr(string)
+	#newstring = ""
+	#string.each.char.with_index |x|
+#end
