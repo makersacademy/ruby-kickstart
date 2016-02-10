4971eee639aa0fbbc18037760683b59f867806a8
claudia@claudiag.me.uk <claudia@claudiag.me.uk>
2016-02-10 12:56:41 +0000

    session 1, challenge 4 complete

--- session1/3-challenge/4_logic.rb
+++ session1/3-challenge/4_logic.rb
@@ -11,8 +11,15 @@
 
 # <10 books => D, 10..20 books => C, >20 book =>B
 
-
 def grade(num_books, has_read_books)
+  if num_books < 10
+    return "C" if has_read_books == true
+    return "D" if has_read_books == false
+  elsif num_books <= 20
+    return "B" if has_read_books == true
+    return "C" if has_read_books == false
+  elsif num_books > 20
+    return "A" if has_read_books == true
+    return "B" if has_read_books == false
+  end
 end
-
-
