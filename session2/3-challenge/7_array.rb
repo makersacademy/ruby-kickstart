67c6607d9d31a09e60f2a01bae64a1573f3fd022
eripheebs <erikapheby@gmail.com>
2016-02-11 12:36:05 +0000

    Session 2 Challenge 7 Answered

--- session2/3-challenge/7_array.rb
+++ session2/3-challenge/7_array.rb
@@ -9,3 +9,11 @@
 # alternate_words("Can't we all get along?")      # => ["Can't", "all", "along"]
 # alternate_words("Elementary, my dear Watson!")  # => ["Elementary", "dear"]
 
+def alternate_words(sentence)
+	punc_array = ["!", "@", "$", "#", "%", "^", "&", "*", "(", ")", "-", "=", "_", "+", "[", "]", ":", ";", ",", ".", "/", "<", ">", "?", "\\", "|" ]
+	punc_array.each { |punc| sentence = sentence.gsub punc, " "}
+	array1 = sentence.split.map
+	array2 = []
+	array1.each_with_index { |word, i| array2 << word if i % 2 == 0 }
+	array2
+end
