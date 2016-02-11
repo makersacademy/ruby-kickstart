ec03b69df2829375023e1ee07497c341f0f7a239
Gabrielle <gabivk@gmail.com>
2016-02-11 00:30:11 +0000

    Answer Challenge 2:6

--- session2/3-challenge/6_array.rb
+++ session2/3-challenge/6_array.rb
@@ -15,3 +15,45 @@
 # prime_chars? ['a', 'bcd']       # => false
 # prime_chars? ['a', 'b', 'cd']   # => false
 
+def prime_chars? (string_array)
+  chars = string_array.join.size
+require 'prime'
+Prime.prime?(chars)
+end
+=begin
+what I started doing before I found the lazy method:
+
+def prime_chars? (string_array)
+  chars = string_array.join.size
+
+   digits = Math.sqrt(chars).floor
+  for m in (2..digits)
+    if (digits % m) == 0
+      puts false
+    else
+      puts true
+    end
+  end
+end
+
+
+if square root of given number
+conditions for prime numbers
+1 -> must be a positive integer larger than 1
+satisfy first condition: 2, 3, 4, 5, 6
+2/2 = 2 || 2/1 = 2
+3/3 = 1 || 3/1 = 3
+
+2 -> when a prime number is divided by another positive integer
+    the result will only be a positive integer when the prime number
+    is divided by itself and the number 1
+
+contradict conditions == not prime
+4/2 = 2
+6/3 = 2
+6/3 = 3
+
+divided by a positive integer other than itself or one, and still produces a positive integer
+
+
+=end
