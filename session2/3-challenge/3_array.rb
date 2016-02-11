120ee8ff7b941f0dbf14ea05c3dccb22ff2a518e
Kyle Buttner <kylebuttner@gmail.com>
2016-02-10 23:44:16 -0500

    answer 2:3

--- session2/3-challenge/3_array.rb
+++ session2/3-challenge/3_array.rb
@@ -7,6 +7,15 @@
 
 class String
   def every_other_char
+    counter = 0
+    answer = ""
+    self.each_char do |i|
+      answer << i if counter.even?
+      answer << "" if counter.odd?
+      counter += 1
+      break if counter > self.length
+    end
+    return answer
   end
-  
+
 end
