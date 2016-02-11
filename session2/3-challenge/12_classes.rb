1f7e6560bb863ba7c5dc13d19f7f34f62973b100
eripheebs <erikapheby@gmail.com>
2016-02-11 21:11:14 +0000

    Session 2 Challenge 12 Answered

--- session2/3-challenge/12_classes.rb
+++ session2/3-challenge/12_classes.rb
@@ -23,8 +23,29 @@
 # f.to_f               # => 0.5
 
 class Fraction
-  def gcd(a,b)
-    return a if b == 0
-    gcd(b, a%b)
-  end
+	attr_accessor :numerator, :denominator
+
+	def initialize(numerator, denominator)
+		self.numerator = numerator
+		self.denominator = denominator
+	end
+
+	def to_f
+		numerator/ denominator.to_f
+	end
+
+	def lowest
+		greatest_common_divisor = gcd(numerator, denominator)
+		Fraction.new(numerator/greatest_common_divisor, denominator/greatest_common_divisor)
+	end
+
+
+  	def gcd(a,b)
+    	return a if b == 0
+    	gcd(b, a%b)
+  	end
+
+  	def to_s
+  		"#{numerator}/#{denominator}"
+  	end
 end
