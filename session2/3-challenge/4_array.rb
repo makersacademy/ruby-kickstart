a28a53768d8e8f413eba2369034796a9fa023cf8
Hanif Fakira <fakira.work@gmail.com>
2016-02-12 16:42:31 +0000

    Update 4_array.rb

--- session2/3-challenge/4_array.rb
+++ session2/3-challenge/4_array.rb
@@ -11,8 +11,3 @@
 def get_squares array
   array.select {|num| array.include? (num**2)}.sort
 end
-
-p get_squares [9]                      # => []
-p get_squares [9,3]                    # => [3]
-p get_squares [9,3,81]                 # => [3, 9]
-p get_squares [25, 4, 9, 6, 50, 16, 5] # => [4, 5]
