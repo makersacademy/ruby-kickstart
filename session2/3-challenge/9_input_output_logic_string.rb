4b0e261baa62c43ab1df43fdef582a346262e41a
yasgreen <yasmin.green@btinternet.com>
2016-02-12 15:46:23 +0000

    Completed session 2 challenge 9

--- session2/3-challenge/9_input_output_logic_string.rb
+++ session2/3-challenge/9_input_output_logic_string.rb
@@ -23,13 +23,55 @@
 # GRANDMA:  HUH?! SPEAK UP, SONNY!
 # USER:     BYE
 
+=begin
 def deaf_grandma
-
+  user = gets.chomp
+  if user != user.upcase
+    loop do
+      puts "HUH?! SPEAK UP, SONNY"
+      user = gets.chomp
+      puts "NO, NOT SINCE 1938" if user == user.upcase
+      break if user == "BYE"
+    end
+  else
+  end
 end
+=end
+
+=begin
+def deaf_grandma
+  user = gets.chomp
+    while user != "BYE"
+      puts "HUH?! SPEAK UP SONNY" if user != user.upcase
+      user = gets.chomp
+      puts "NO, NOT SINCE 1938" if user == user.upcase
+      user = gets.chomp
+    end
+  end
+=end
+
+  def deaf_grandma
+  user = gets.chomp
+    while user != "BYE"
+      if user != user.upcase
+        puts "HUH?! SPEAK UP, SONNY!"
+        user = gets.chomp
+      elsif user == user.upcase && user != ""
+        puts "NO, NOT SINCE 1938!"
+        user = gets.chomp
+      else
+        puts "HUH?! SPEAK UP, SONNY!"
+        user = gets.chomp
+      end
+    end
+  end
 
 
-
-
+=begin
+anything lowercase reply HUH SPEAK UP ...
+anything upcase reply NO NOT SINCE...
+only stop when reply is BYE.
+=end
 
 # This will call your code so you can run it from the terminal.
 # But not call it otherwise, so that it will work with our tests.
