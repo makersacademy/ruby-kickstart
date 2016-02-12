94fd2d7a6870050585f3fbbc55ffe1731dc0540a
yasgreen <yasmin.green@btinternet.com>
2016-02-12 15:07:01 +0000

    Completed 8_array.rb s2 challenge

--- session2/3-challenge/8_array.rb
+++ session2/3-challenge/8_array.rb
@@ -6,3 +6,25 @@
 # got_three? ['a', 'a', 'a']  # => true
 # got_three? [1, 2, 1, 1]     # => false
 
+=begin
+def got_three?(ary)
+  ary.each_cons(3) do |a, b, c|
+    if a == b && b == c
+      return true
+    else
+      return false
+    end
+  end
+end
+
+puts got_three?([1,2,2,2,3])
+=end
+
+def got_three?(ary)
+  ary.each_cons(3) do |a, b, c|
+    return true if a == b && b == c
+  end
+  false
+end
+
+puts got_three?([1,2,2,2,3])
