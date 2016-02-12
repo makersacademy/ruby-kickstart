f5bddd9d766fb23ed0a63ffb516b8e9b2412dd55
Pete BURCH <peteburch223@gmail.com>
2016-02-12 07:42:24 +0100

    Answered session 1 challenge 5

--- session1/3-challenge/5_string.rb
+++ session1/3-challenge/5_string.rb
@@ -5,4 +5,7 @@
 # add_more_ruby("Sad times are ahead!")         # => "Happy times are ahead!"
 
 def add_more_ruby(string)
+
+	string.gsub(/sad/i) {|match| match.start_with?("S") ? "Happy" : "happy"}
+
 end
