7c1a8ce94e96ed5a5995d9b476fb3e897ef102b3
MisaOgura <misa.ogura01@gmail.com>
2016-02-12 22:54:34 +0000

    Refactor 2:9

--- session2/3-challenge/9_input_output_logic_string.rb
+++ session2/3-challenge/9_input_output_logic_string.rb
@@ -24,19 +24,17 @@
 # USER:     BYE
 
 def deaf_grandma
- ans = gets.chomp
- if ans == "BYE"
-  exit
- elsif ans == ans.upcase
-  puts "NO, NOT SINCE 1938!"
-  deaf_grandma
- else
-  puts "HUH?! SPEAK UP, SONNY!"
-  deaf_grandma
+ while ans = gets.chomp
+   break if ans == "BYE"
+   if ans == ans.upcase && ans != ""
+     puts "NO, NOT SINCE 1938!"
+   else
+     puts "HUH?! SPEAK UP, SONNY!"
+   end
  end
 end
 
 
 # This will call your code so you can run it from the terminal.
 # But not call it otherwise, so that it will work with our tests.
-deaf_grandma if $0 == __FILE__
+# deaf_grandma if $0 == __FILE__
