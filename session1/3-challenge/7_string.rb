c939774c09c7a338677efe3c47e53e3f14fff2e9
claudia@claudiag.me.uk <claudia@claudiag.me.uk>
2016-02-12 11:23:12 +0000

    Session 1, challenge 7: complete

--- session1/3-challenge/7_string.rb
+++ session1/3-challenge/7_string.rb
@@ -6,12 +6,14 @@
 
 
 def pirates_say_arrrrrrrrr(string)
+  new_arr = ""
   arr = string.split("")
   arr.each_with_index do |l, i|
     if l == "R" || l == "r"
-      print arr[i+1]
-    # else
-    #   arr.delete(i)
+       new_arr << arr[i+1].to_s
+    else
+      arr.delete(i)
     end
   end
+  new_arr
 end
