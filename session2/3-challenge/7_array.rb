c0bf151f7741187733b49b026ba2de5358324773
Junyuan Xue <junyuanxue.is@gmail.com>
2016-02-13 00:39:46 +0000

    Updated challenge 2:7. All Session 2 challenges passed.

--- session2/3-challenge/7_array.rb
+++ session2/3-challenge/7_array.rb
@@ -9,8 +9,8 @@
 # alternate_words("Can't we all get along?")      # => ["Can't", "all", "along"]
 # alternate_words("Elementary, my dear Watson!")  # => ["Elementary", "dear"]
 
-def every_other_word(sentence)
-  words = sentence.gsub(/[^'a-zA-Z]/, " ").split
+def alternate_words(sentence)
+  words = sentence.gsub(/[^’'a-zA-Z]/, " ").split
   output = []
   words.each_with_index do |word, index|
     output << word if index.even?
