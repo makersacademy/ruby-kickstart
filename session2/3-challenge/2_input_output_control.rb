2bcbf4924bf8b9ace63ca48d85be546b21e22e1f
Hanna <hannaeberhoefer@yahoo.com>
2016-02-13 13:35:47 +0000

    2:2

--- session2/3-challenge/2_input_output_control.rb
+++ session2/3-challenge/2_input_output_control.rb
@@ -14,19 +14,28 @@
 # USER:    bye
 # PROGRAM: goodbye
 
-
 # remember you can try your program out with              $ ruby 2_input_output_control.rb
 # and when you think it is correct, you can test it with  $ rake 2:2
-
-def hi_hi_goodbye
-  # your code here
-
-end
-
-
-
 
 # This will just invoke the method if you run this program directly
 # This way you can try it out by running "$ ruby 2_input_output_control.rb"
 # but it will still work for our tests
-hi_hi_goodbye if $0 == __FILE__
+# hi_hi_goodbye if $0 == __FILE__
+
+def user_input
+    puts "Enter a number please: "
+end
+
+def hi_hi_goodbye
+    user_input
+  while line = gets
+	puts "hi" * line.to_i
+	break if line.chomp == 'bye'
+	user_input
+	end
+	puts "goodbye"
+end
+
+
+Correct
+
