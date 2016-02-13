9d38f1527fba9b8b20cb8433cc45500d89bf25cf
phoebe <phoebe.greig@gmail.com>
2016-02-13 12:59:31 +0000

    session 2 questions 4-6

--- session2/3-challenge/4_array.rb
+++ session2/3-challenge/4_array.rb
@@ -8,5 +8,24 @@
 
 # This time you will have to define the method, it's called: get_squares
 
+=begin  The below worked for 5 out of the 6 tests. It failed the last test. I can see that what my reults have been duplicated compared to the correct answer.
+def get_squares(array)
+	squares =[]
+	if array.length==1
+		squares
+	else 
+		array.each do |x|
+			array.each do |y|
+				if Math.sqrt(y)==x
+					squares << x	
+				end
+			end
+		end
+		squares.sort
+	end
+end
+=end
 
-
+def get_squares(array)
+  array.select { |n| array.include? n*n }.sort
+end
--- session2/3-challenge/5_array.rb
+++ session2/3-challenge/5_array.rb
@@ -14,3 +14,11 @@
 #
 # mod_three [0,1,2,3,4,5,6,7] # => [1, 2, 1, 2, 1]
 
+def mod_three(array)
+	mod_array=[]
+	array.each do |x| 		
+			mod_num = x%3
+			mod_num!=0 && x!=0? mod_array << mod_num : mod_array		
+	end
+	mod_array
+end
