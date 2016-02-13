33bb12409f724c7131fe714f6bebca5c4e8ee030
sitypop <sityemilyshah@gmail.com>
2016-02-13 17:59:44 +0000

    Adding Session2: 8_array.rb

--- session2/3-challenge/8_array.rb
+++ session2/3-challenge/8_array.rb
@@ -6,3 +6,7 @@
 # got_three? ['a', 'a', 'a']  # => true
 # got_three? [1, 2, 1, 1]     # => false
 
+def got_three?(array)
+	array.each_cons(3) {|a, b, c| return true if a == b && b == c} #.each_cons method iterates the given block for each array of consecutive <n> elements, so in this case we have 3 elements we want to iterate
+end
+
