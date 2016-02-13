25cb29d02c7b37053d2b6f1a3961b2ce4240df1f
Pete BURCH <peteburch223@gmail.com>
2016-02-13 12:19:22 +0100

    Answered session 2 challenge 5

--- session2/3-challenge/5_array.rb
+++ session2/3-challenge/5_array.rb
@@ -14,3 +14,13 @@
 #
 # mod_three [0,1,2,3,4,5,6,7] # => [1, 2, 1, 2, 1]
 
+
+def mod_three (ary)
+	result = []
+	ary.each do |value| 
+		result << value % 3  unless value % 3 ==0
+	end
+
+result
+
+end
