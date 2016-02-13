f94e0d6af530bc18fcea34b2402bf9426434a6ea
Andrew Cookson <acookson91@gmail.com>
2016-02-13 15:52:14 +0000

    updated 5 array

--- session2/3-challenge/5_array.rb
+++ session2/3-challenge/5_array.rb
@@ -14,3 +14,12 @@
 #
 # mod_three [0,1,2,3,4,5,6,7] # => [1, 2, 1, 2, 1]
 
+def mod_three(num)
+newnum=[]
+    num.each do |num|
+        if num % 3 != 0
+        newnum << (num % 3)
+        end
+    end
+    puts newnum
+end 
