bcd8c798949e88ff433e8e9cb36ba383a801709f
Hanif Fakira <fakira.work@gmail.com>
2016-02-18 10:42:06 +0000

    sol 3:2

--- session3/3-challenge/2_hashes.rb
+++ session3/3-challenge/2_hashes.rb
@@ -8,6 +8,11 @@
 # staircase 4  # => {1 => [], 3 => [2]}
 # staircase 5  # => {1 => [], 3 => [2], 5 =>[2, 4]}
 
-
-
+def staircase int
+  new_hash ={}
+  (1..int).select(&:odd?).each do |x|
+    new_hash[x] = (1..x).select(&:even?)
+  end
+  new_hash
+end
 
