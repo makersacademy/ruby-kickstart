97a30178f3c4bd31bc3d1a0eae1673e0ee81aa6e
Pete BURCH <peteburch223@gmail.com>
2016-02-18 18:24:09 +0100

    answered session 3 challenges 1-8

--- session3/3-challenge/5_blocks.rb
+++ session3/3-challenge/5_blocks.rb
@@ -22,4 +22,59 @@
 
 
 
+def rotate(a)
+  rotated = (0..a.size-1).map do|colidx|
+    a.map do|row|
+      row[colidx]
+    end.reverse
+  end
+  rotated
+end
+
+def spiral_access (two_d, &block)
+
+  two_d.delete_if{|row| row.all?{|e| e.nil?}}
+  unless two_d.empty?
+    two_d.map do |row|
+      element = row.reverse!.pop
+      block.call(element)
+      row.reverse!
+    end
+
+    two_d = rotate(two_d)
+    spiral_access(two_d,&block)
+  end
+end
+
+#===============================================
+
+# two_d = [[ 1,  2,  3,  4, 5],
+#          [16, 17, 18, 19, 6],
+#          [15, 24, 25, 20, 7],
+#          [14, 23, 22, 21, 8],
+#          [13, 12, 11, 10, 9]]
+#
+# two_d = [[1,2],
+#          [4,3]]
+
+two_d = [[1,2,3],
+         [8,9,4],
+         [7,6,5]]
+
+# two_d = [[ 1 ,  2 ,  3 ,  4 ,  5 ,  6 ],
+#         [ 20 , 21 , 22 , 23 , 24 ,  7 ],
+#         [ 19 , 32 , 33 , 34 , 25 ,  8 ],
+#         [ 18 , 31 , 36 , 35 , 26 ,  9 ],
+#         [ 17 , 30 , 29 , 28 , 27 , 10 ],
+#         [ 16 , 15 , 14 , 13 , 12 , 11 ]]
+
+
+
+order = []
+two_d = two_d.transpose
+spiral_access two_d do |i|
+  order << i
+end
+
+p order
 
