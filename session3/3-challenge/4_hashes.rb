846ad92e1cffcd70005e254a40a25abd3cc37f81
MisaOgura <misa.ogura01@gmail.com>
2016-02-23 17:55:55 +0000

    solves 3:4

--- session3/3-challenge/4_hashes.rb
+++ session3/3-challenge/4_hashes.rb
@@ -6,3 +6,9 @@
 #
 # first_pos "The dog and the cat and the cow" # => {"The" => 0, "dog" => 1, "and" => 2, "the" => 3, "cat" => 4, "cow" => 7}
 
+def first_pos(string)
+  array = string.split(" ")
+  positions = {}
+  array.each {|word| positions[word] = array.index(word)}
+  positions
+end
