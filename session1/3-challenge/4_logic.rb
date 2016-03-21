ab9d5f57e5637e01e966210f0590ee9044f70907
Amy Nicholson <missamynicholson@gmail.com>
2016-03-21 11:34:08 +0000

    4_logic.rb

--- session1/3-challenge/4_logic.rb
+++ session1/3-challenge/4_logic.rb
@@ -11,8 +11,15 @@
 
 # <10 books => D, 10..20 books => C, >20 book =>B
 
-
 def grade(num_books, has_read_books)
+  case num_books
+  when 0...10
+     has_read_books ? "C" : "D"
+  when 10..20
+    has_read_books ? "B" : "C"
+  else
+    has_read_books ? "A" : "B"
+  end
 end
 
 
