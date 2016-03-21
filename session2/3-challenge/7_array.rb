fa752fa5c70ee93c89169e8dbb597610b9be6cb8
sitypop <sityemilyshah@gmail.com>
2016-03-21 16:45:17 +0000

    2:7

--- session2/3-challenge/7_array.rb
+++ session2/3-challenge/7_array.rb
@@ -9,3 +9,11 @@
 # alternate_words("Can't we all get along?")      # => ["Can't", "all", "along"]
 # alternate_words("Elementary, my dear Watson!")  # => ["Elementary", "dear"]
 
+def alternate_words(string)
+	no_syms = string.gsub(/[!@$#%^&*()\-=_+\[\]:;.,\/<>?\\\|]/, " ")
+	array = []
+	no_syms.split.each.with_index {|x, i| array << x if i.even?}
+	array
+end
+
+
