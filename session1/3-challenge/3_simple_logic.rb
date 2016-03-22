2c01da758be279c6b52e81de73d0052ef67b921a
Lucy <l.l.c.clarke@gmail.com>
2016-03-22 16:21:09 +0100

    not working with rake, but does work?

--- session1/3-challenge/3_simple_logic.rb
+++ session1/3-challenge/3_simple_logic.rb
@@ -8,4 +8,13 @@
 # ten_twenty(6) # => 10
 
 def ten_twenty(n)
+	if n.even?
+		puts 10
+	else n.odd?
+		puts 20
+	end
 end
+ten_twenty(5) # => 20
+ten_twenty(6) # => 10
+ten_twenty(10) 
+ten_twenty(99) 
