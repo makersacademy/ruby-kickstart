79b48e363cf75ac5aa59aff315d09ec24acebe9b
sitypop <sityemilyshah@gmail.com>
2016-03-22 14:09:49 +0000

    2:8

--- session2/3-challenge/8_array.rb
+++ session2/3-challenge/8_array.rb
@@ -6,3 +6,6 @@
 # got_three? ['a', 'a', 'a']  # => true
 # got_three? [1, 2, 1, 1]     # => false
 
+def got_three?(array)
+	array.each_index.any?{|i|array[i] == array[i + 1] && array[i] == array[i + 2]} ? true : false
+end
