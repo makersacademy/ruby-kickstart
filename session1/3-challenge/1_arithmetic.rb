20bd21f1e0f12285935d401aa2df6cf79fb97ab2
Elia Bardashevich <ebardashevich@gmail.com>
2016-03-22 14:50:48 +0000

    added 1:1

--- session1/3-challenge/1_arithmetic.rb
+++ session1/3-challenge/1_arithmetic.rb
@@ -3,11 +3,14 @@
 #  $ rake 1:1
 
 # Given a number, return 20 less than, that number multiplied by 5
-#
+# 
 # arithmetic1(10)     # => 30
-# arithmetic1(10.5)   # => 32.5
-# arithmetic1(-6)     # => -50
+# arithmeitc1(10.5)   # => 32.5
+# arithmeitc1(-6)     # => -50
 
 def arithmetic1(n)
+  (n * 5) - 20 # the code for this method goes in here
 end
-
+puts arithmetic1(10)     
+puts arithmetic1(10.5)  
+puts arithmetic1(-6)
