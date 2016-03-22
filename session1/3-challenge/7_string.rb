276e91b32141e9535299c90514b7e5d66c6ece85
chriselevate <coates.c@hotmail.com>
2016-03-22 14:06:27 +0000

    completed tasks 2-7

--- session1/3-challenge/7_string.rb
+++ session1/3-challenge/7_string.rb
@@ -4,6 +4,15 @@
 # pirates_say_arrrrrrrrr("Katy Perry is on the radio!")   # => "rya"
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
-def pirates_say_arrrrrrrrr(string)
+def pirates_say_arrrrrrrrr(string)                                        #defines a method with 1 parameter
+  answer = ""                                                             #sets up a blank variable for the answer
+  add_next_letter = false                                                 #sets up a variable with false
+  string.length.times do |count|                                          #finds length of string and creates a loop to work that many times
+    the_character = string[count]                                         #puts the count character into the the_character
+    answer << the_character if add_next_letter                            #appends the answer if
+    add_next_letter = (the_character == "r" || the_character == "R")      #adds in next letter if the next character is R or R.
+  end                                                                     #ends the loop
+  answer                                                                  #returns the answer
+end                                                                       #ends the method
 
-end
+
