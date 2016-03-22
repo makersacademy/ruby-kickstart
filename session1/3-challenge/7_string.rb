b2b6697ed1f3ed5d1f21aa31968afba0cda3bac9
Michael Harrison <harrim91@hotmail.co.uk>
2016-03-22 13:23:11 +0000

    solved 1.7

--- session1/3-challenge/7_string.rb
+++ session1/3-challenge/7_string.rb
@@ -5,5 +5,10 @@
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
 def pirates_say_arrrrrrrrr(string)
-
+  arrrrrrrr = ""
+  string.length.times do |n|
+    add_letter = (string[n].downcase == "r")
+    arrrrrrrr << string[n+1] if (add_letter && n+1 != string.length)
+  end
+  return arrrrrrrr
 end
