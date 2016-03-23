a8a5fb1b80df88acbcdfeff4cb09cb7c8cc213fc
Adam Skuse <a.t.skuse@gmail.com>
2016-03-23 13:49:00 +0800

    Solved 1:2

--- session1/3-challenge/2_arithmetic.rb
+++ session1/3-challenge/2_arithmetic.rb
@@ -9,4 +9,15 @@
 # arithmetic2(-6, -7)  # => -3.5
 
 def arithmetic2(a, b)
+  if a > b
+    b.to_f / 2
+  else
+    a.to_f / 2
+  end
 end
+
+# though the above passes the rake test, what if the two numbers are equal?
+# my prediction is that, it will still work - the first clause of the if statement
+# will resolve as FALSE, so the method will move on to the else statement
+# regardless of whether the two numbers are equal or not. So there is no need to
+# write in an elsif to deal with the eventuality of the numbers being the same.
