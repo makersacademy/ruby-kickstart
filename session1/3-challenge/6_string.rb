97d31b5d2e4d1051459f049af80ea5598bfe84d4
Darrenf <darrenf54321@gmail.com>
2016-03-23 08:40:29 +0000

    Solution 1:6

--- session1/3-challenge/6_string.rb
+++ session1/3-challenge/6_string.rb
@@ -7,4 +7,15 @@
 
 def odds_and_evens(string, return_odds)
 
+array = string.split("")
+
+array_even, array_odd = array.each_with_index.partition{|v|v[1].even?}.map{|v|v.map{|v|v[0]}}
+
+if return_odds == true
+  return array_odd.join
+
+else
+  return array_even.join
+
+end
 end
