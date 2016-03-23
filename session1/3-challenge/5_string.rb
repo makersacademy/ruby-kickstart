5b71bdfc046118c0e57fa1cf20d725dc419f23f0
Elia Bardashevich <ebardashevich@gmail.com>
2016-03-23 11:59:51 +0000

    1:5

--- session1/3-challenge/5_string.rb
+++ session1/3-challenge/5_string.rb
@@ -5,4 +5,8 @@
 # add_more_ruby("Sad times are ahead!")         # => "Happy times are ahead!"
 
 def add_more_ruby(string)
+  string.gsub("sad" , "happy").gsub("Sad", "Happy")
 end
+puts add_more_ruby("The clowns were sad.")         # => "The clowns were happy."
+puts add_more_ruby("The sad dad said sad stuff.")  # => "The happy dad said happy stuff."
+puts add_more_ruby("Sad times are ahead!")
