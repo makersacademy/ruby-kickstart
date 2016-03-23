a4dc45b2ba023ec7be0a98f8ed50a7a85974055f
Mara <m.d.wanot@hotmail.com>
2016-03-23 19:26:34 +0000

    challenges 4-6 for week 1

--- session1/3-challenge/6_string.rb
+++ session1/3-challenge/6_string.rb
@@ -1,10 +1,25 @@
 # You'll get a string and a boolean.
 # When the boolean is true, return a new string containing all the odd characters.
 # When the boolean is false, return a new string containing all the even characters.
-#
+
 # If you have no idea where to begin, remember to check out the cheatsheets for string and logic/control
-#
+
 
 def odds_and_evens(string, return_odds)
+
+	if return_odds 
+    i = 1 
+  else 
+    i = 0
+  end
+
+  stringy = String.new
+
+  while i <= string.length - 1 do
+    stringy += string[i]
+    i += 2
+  end
+
+  return stringy
 
 end
