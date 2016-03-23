8e0b75431e8720e9cb808da433891f411c1035cf
Elia Bardashevich <ebardashevich@gmail.com>
2016-03-23 11:59:08 +0000

    1:4

--- session1/3-challenge/4_logic.rb
+++ session1/3-challenge/4_logic.rb
@@ -1,6 +1,6 @@
 # A grad student at a local university thinks he has discovered a formula to
-# predict what kind of grades a person will get. He says if you own less than
-# 10 books, you will get a "D". If you own 10 to 20 books, you will get a "C",
+# predict what kind of grades a person will get. He says if you own less than 
+# 10 books, you will get a "D". If you own 10 to 20 books, you will get a "C", 
 # and if you own more than 20 books, you will get a "B".
 # He further hypothesizes that if you actually read your books, then you will
 # get a full letter grade higher in every case.
@@ -9,10 +9,18 @@
 # grade(4,  true)   # => "C"
 # grade(15, true)   # => "B"
 
-# <10 books => D, 10..20 books => C, >20 book =>B
-
-
-def grade(num_books, has_read_books)
+def grade(num_books, reads_books)
+  if reads_books
+   return "C" if num_books < 10
+   return "B"  if num_books <=20
+   return "A" if num_books > 20
+ else 
+   return "D" if num_books < 10
+   return "C" if num_books <= 20
+   return "B" if num_books > 20
+    
+ end   
 end
-
-
+puts grade(4,  false)  # => "D"
+puts grade(4,  true)   # => "C"
+puts grade(15, true)
