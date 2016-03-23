8431b0a01cfa51b75e46c9915bf0774346f5a6a2
Nick <nm.richards@yahoo.com>
2016-03-23 20:42:57 +0430

    2:5 complete

--- session2/3-challenge/5_array.rb
+++ session2/3-challenge/5_array.rb
@@ -14,3 +14,11 @@
 #
 # mod_three [0,1,2,3,4,5,6,7] # => [1, 2, 1, 2, 1]
 
+def mod_three(array)
+  new_array = Array.new
+  array.each do |x|
+    mod = x % 3
+    new_array.push(mod)
+  end
+  new_array.delete_if{|x| x == 0}
+end
