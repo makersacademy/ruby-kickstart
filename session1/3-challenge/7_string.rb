a2626121c9f093274a11b40867701943fed5e2ec
MariaRomero <fantasysystems@gmail.com>
2016-03-24 15:21:57 +0000

    finish challenge 7

--- session1/3-challenge/7_string.rb
+++ session1/3-challenge/7_string.rb
@@ -5,5 +5,17 @@
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
 def pirates_say_arrrrrrrrr(string)
+	counter = 0
+	pirate_string = ""
+	while counter < string.length - 1 do 
 
+		if string[counter] == "r"  || string[counter] == "R"
+			pirate_string += string[counter + 1]
+		end	
+
+		counter += 1
+	end
+	 return pirate_string 
 end
+
+ puts pirates_say_arrrrrrrrr("are you really learning Ruby?") 
