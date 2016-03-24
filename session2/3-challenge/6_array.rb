bbbb69f9b04405f5e7efcd4c41c24b475ace9bed
Jackrim1 <jriminton@gmail.com>
2016-03-24 12:26:21 +0000

    no.6 done

--- session2/3-challenge/6_array.rb
+++ session2/3-challenge/6_array.rb
@@ -15,3 +15,24 @@
 # prime_chars? ['a', 'bcd']       # => false
 # prime_chars? ['a', 'b', 'cd']   # => false
 
+def prime_chars?(array)
+if array == ([])
+    return false
+end
+if array == ([''])
+    return false
+end
+
+array2 = array.map {|x| (x.to_s.length) }
+array3 = array2.inject(0) {|x,y| x + y}
+n = array3.to_i
+if n < 2
+  return false
+
+elsif
+        Math.sqrt(n).floor.downto(2).each {|i| return false if n % i == 0}
+    return true
+else
+    return false
+    end
+end
