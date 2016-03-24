a389a8b50e9eddec08d3a63679e12b7aee440acd
josephchin19293 <josephchin19293@gmail.com>
2016-03-24 00:29:32 +0000

    session2 10-11 complete

--- session2/3-challenge/11_classes.rb
+++ session2/3-challenge/11_classes.rb
@@ -20,3 +20,85 @@
 # if the parameter is greater than 99, set the number of beer bottles to 99
 # Then make a public method called print_song that outputs all stanzas from the number of bottles of beer down to zero.
 # Add any additional methods you find helpful.
+
+class BeerSong
+
+	attr_accessor "beer_number"
+
+	def initialize beer_number
+		if beer_number > 99
+			beer_number = 99
+		elsif beer_number <= 0
+			beer_number = 0
+		end
+		self.beer_number = beer_number
+	end
+
+	def englishNumber num
+  		if num < 0
+  			return "No negatives, please"
+  		end
+  		if num == 0
+  			return "zero"
+  		end
+  
+  		ones = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+  		tens = ["twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
+  		teens = ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"]
+  		words = ""
+  		left = num
+  		write = left/10
+  		left = left - write*10
+  
+  		if write > 0
+    		if ((write == 1) and (left >= 0))
+      			words = words + teens[left]
+      			left = 0
+    		else
+      			words = words + tens[write-2]
+    		end
+  	
+  			if left > 0
+  	  			words = words + "-"
+    		end
+  		end
+  
+  		write = left
+  		left = 0
+
+  		if write > 0
+  			words = words + ones[write-1]
+  		end
+
+  		words.capitalize
+	end
+
+	def print_song
+		number = self.beer_number
+		while number > 2 
+  		puts "#{englishNumber(number)} bottles of beer on the wall,"
+  		puts "#{englishNumber(number)} bottles of beer," 
+  		puts "Take one down, pass it around,"
+  		puts "#{englishNumber((number-1))} bottles of beer on the wall."
+
+    	number -= 1
+		end
+
+		if number == 2
+  		puts "Two bottles of beer on the wall," 
+  		puts "Two bottles of beer,"
+  		puts "Take one down, pass it around,"
+  		puts "One bottle of beer on the wall."
+
+  		number -= 1
+		end
+
+		if number == 1
+  		puts "One bottle of beer on the wall," 
+  		puts "One bottle of beer,"
+  		puts "Take one down, pass it around,"
+  		puts "Zero bottles of beer on the wall."
+		end
+	end
+
+end
