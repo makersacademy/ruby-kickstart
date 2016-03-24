4f9e8459c4245fde8c1569cdbd48daf830bb8366
Adam Skuse <a.t.skuse@gmail.com>
2016-03-24 20:02:02 +0800

    2:1 solved

--- session2/3-challenge/1_input_output.rb
+++ session2/3-challenge/1_input_output.rb
@@ -8,6 +8,15 @@
 # "11\n7\n18\n" to standard output.
 
 def sum_difference_product
-  # your code goes here
-  
+input = gets.chomp.split
+puts "#{input[0].to_i + input[1].to_i}\n#{input[0].to_i - input[1].to_i}\n#{input[0].to_i * input[1].to_i}"
 end
+
+# this was fairly straight forward, but got me thinking about new and more succinct
+# ways to do things with methods I already knew. From looking at the
+# given solution, I see it was unecessary for me to use a hash - I have "overcoded" once more.
+# I also did not realise it was possible to get two or more variables from one input
+# using the approach given in the solution i.e. a , b = gets.split.map { |num| num.to_i }
+# it took me a little time to work this out, but a little research and I understand
+# how split and map are combining to separate the numbers given in the STDIN and enumerate
+# to put them into separate variables. Very neat! 
