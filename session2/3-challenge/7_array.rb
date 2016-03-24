255b810efaa3910c4dbdaede925b4b7e0c4611ee
Darrenf <darrenf54321@gmail.com>
2016-03-24 10:43:35 +0000

    Solution 2:7

--- session2/3-challenge/7_array.rb
+++ session2/3-challenge/7_array.rb
@@ -9,3 +9,13 @@
 # alternate_words("Can't we all get along?")      # => ["Can't", "all", "along"]
 # alternate_words("Elementary, my dear Watson!")  # => ["Elementary", "dear"]
 
+def alternate_words(string)
+
+string2 = string.gsub(/[!?@$#%^&*()-=_+:;,.]/," ")
+
+array = string2.split(" ")
+alternate = array.values_at(* array.each_index.select {|i| i.even?})
+
+return alternate
+
+end
