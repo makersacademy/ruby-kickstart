f88e3b4943049f23b0566823d0ed5457db41e447
MariaRomero <fantasysystems@gmail.com>
2016-03-25 23:00:44 +0000

    finish chapter 3

--- session2/3-challenge/4_array.rb
+++ session2/3-challenge/4_array.rb
@@ -8,5 +8,19 @@
 
 # This time you will have to define the method, it's called: get_squares
 
+def get_squares(array_of_num)
+	array_of_num.sort!
+	answer = Array.new
+	counter = 0
+	if array_of_num.length < 2
+		return answer
+	end	
 
-
+	while counter <= array_of_num.length - 1 do
+		 if array_of_num.include?(array_of_num[counter] * array_of_num[counter])
+		 	answer.push(array_of_num[counter])	
+		 end
+		counter += 1	
+	end	
+	return answer
+end
