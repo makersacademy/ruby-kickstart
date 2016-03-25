3ca268bc5618a52b933a7225ff587fc4256c7d89
Hanna <hannaeberhoefer@yahoo.com>
2016-03-25 16:50:16 +0000

    2:8 complete

--- session2/3-challenge/8_array.rb
+++ session2/3-challenge/8_array.rb
@@ -8,8 +8,8 @@
 
 
 def got_three?(elements)
-    elements.each_cons(3) do |a, b, c| 
-        return true if a == b && b == c 
+    elements.each_cons(3) do | a, b, c |
+    return true if  a == b && b == c
     end
-    false
+ false
 end
