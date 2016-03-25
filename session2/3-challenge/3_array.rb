a95335ab3ad88ddfda8675a3d1dc0506378e6237
Emmett Walsh <emmett.finbarr.walsh@gmail.com>
2016-03-25 21:53:09 +0000

    Exercise 2:3 complete

--- session2/3-challenge/3_array.rb
+++ session2/3-challenge/3_array.rb
@@ -7,6 +7,14 @@
 
 class String
   def every_other_char
+    string_to_return = String.new
+
+    each_char.with_index do |char, i|
+      if i.even?
+        string_to_return << char
+      end
+    end
+
+    string_to_return
   end
-  
 end
