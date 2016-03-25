8281c621898f041009f91de4c2c875d3b76e2d76
MariaRomero <fantasysystems@gmail.com>
2016-03-25 23:44:52 +0000

    finish challenge 5

--- session2/3-challenge/5_array.rb
+++ session2/3-challenge/5_array.rb
@@ -14,3 +14,17 @@
 #
 # mod_three [0,1,2,3,4,5,6,7] # => [1, 2, 1, 2, 1]
 
+def mod_three(array)
+	answer = Array.new
+
+	array.each do |x|
+		if x % 3 == 0
+			answer
+		else 	
+			answer.push(x % 3)
+		end
+	end
+	answer
+end
+
+ 
