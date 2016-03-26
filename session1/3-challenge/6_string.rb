ab5bbd4ee4371425ec1dea97806682682af233ed
Ruta <ruta@baltiejus.com>
2016-03-26 09:32:03 +0000

    1:6

--- session1/3-challenge/6_string.rb
+++ session1/3-challenge/6_string.rb
@@ -3,8 +3,15 @@
 # When the boolean is false, return a new string containing all the even characters.
 #
 # If you have no idea where to begin, remember to check out the cheatsheets for string and logic/control
-#
+
 
 def odds_and_evens(string, return_odds)
-
+  array = string.split(//)
+  if return_odds == true
+    new_odd_letters = array.select.each_with_index { |_, i| i.odd?}
+    new_odd_letters.join("")
+  elsif return_odds == false
+  	new_even_letters = array.select.each_with_index { |_, i| i.even?}
+    new_even_letters.join("")
+  end
 end
