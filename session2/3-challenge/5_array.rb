e510b6dd4235a0b70e9f62c03b2a17faec45b60a
Adam Skuse <a.t.skuse@gmail.com>
2016-03-26 17:21:15 +0800

    2:5 solved

--- session2/3-challenge/5_array.rb
+++ session2/3-challenge/5_array.rb
@@ -14,3 +14,12 @@
 #
 # mod_three [0,1,2,3,4,5,6,7] # => [1, 2, 1, 2, 1]
 
+def mod_three(numbers)
+  result = []
+  numbers.each do | test |
+    if test % 3 != 0
+      result << test % 3
+    end
+  end
+  result
+end
