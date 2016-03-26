f7d8a112545fb46fb5a4b636c973bd4af9cc40a1
Emmett Walsh <emmett.finbarr.walsh@gmail.com>
2016-03-26 19:22:31 +0000

    Exercise 2:12 is being weird...

--- session2/3-challenge/12_classes.rb
+++ session2/3-challenge/12_classes.rb
@@ -23,8 +23,32 @@
 # f.to_f               # => 0.5
 
 class Fraction
+  attr_accessor 'numerator', 'denominator'
+
+  def initialize(numerator, denominator)
+    @numerator = numerator
+    @denominator = denominator
+  end
+
   def gcd(a,b)
     return a if b == 0
     gcd(b, a%b)
   end
+
+  def lowest
+    lowest = gcd(@numerator, @denominator)
+    @numerator /= lowest
+    @denominator /= lowest
+  end
+
+  def to_f
+    @numerator.to_f / @denominator.to_f
+  end
+
+  def to_s
+    "#{@numerator}/#{@denominator}"
+  end
+
 end
+
+
