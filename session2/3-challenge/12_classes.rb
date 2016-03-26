0c149c1a9abe562473bd094d8bd9b1b253baeb84
Nick <nm.richards@yahoo.com>
2016-03-26 13:26:50 +0430

    2:12 complete

--- session2/3-challenge/12_classes.rb
+++ session2/3-challenge/12_classes.rb
@@ -23,6 +23,26 @@
 # f.to_f               # => 0.5
 
 class Fraction
+  attr_accessor 'numerator', 'denominator'
+
+  def initialize(num,den)
+    @numerator = num
+    @denominator = den
+  end
+
+  def to_s
+    "#{@numerator}/#{@denominator}"
+  end
+
+  def lowest
+    divisor = gcd(numerator, denominator)
+    Fraction.new(numerator/divisor, denominator/divisor)
+  end
+
+  def to_f
+    @numerator.to_f/ @denominator
+  end
+
   def gcd(a,b)
     return a if b == 0
     gcd(b, a%b)
