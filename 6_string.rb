957644a7dba86a6b4ca5a7120cda9784534ff666
Richard Watkins <richardwatkins1@gmail.com>
2016-03-26 12:35:57 +0000

    Added files via upload

---
+++ 6_string.rb
@@ -0,0 +1,16 @@
+# You'll get a string and a boolean.
+# When the boolean is true, return a new string containing all the odd characters.
+# When the boolean is false, return a new string containing all the even characters.
+#
+# If you have no idea where to begin, remember to check out the cheatsheets for string and logic/control
+#
+
+def odds_and_evens(string, return_odds)
+new_string = ""
+string.size.times do |x|
+    next if return_odds && x.even?
+    next if !return_odds && x.odd?
+    new_string << string[x]
+    end
+new_string
+end
