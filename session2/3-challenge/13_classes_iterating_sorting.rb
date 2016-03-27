0fddd7bbf3f0c836c4c791f40567b13f2087cb9c
Ruta <ruta@baltiejus.com>
2016-03-27 16:25:11 +0000

    2:13

--- session2/3-challenge/13_classes_iterating_sorting.rb
+++ session2/3-challenge/13_classes_iterating_sorting.rb
@@ -1,13 +1,12 @@
 # DO NOT STRUGGLE ON THIS PROBLEM FOR MORE THAN 30 MINUTES!!
 
 # In this exercise you'll need to create two classes.
-
 # One class is called User that keeps track of a person's username
 # it should receive the username when initialized
 # it should have a method add_blog which accepts a date and text
 # it should have a method blogs which returns an array of all blogs the user has written
 # they should be in reverse chronological order (newest first)
-#
+
 # The other class is called Blog that could be used to store an entry for your web log.
 # The class should have a getter and setter methods: text , date , user
 # its initialize method should receive the date, user , and text
@@ -18,8 +17,6 @@
 #      def ==(other)
 #        return self.date == other.date
 #      end
-
-
 
 # ==========  EXAMPLE  ==========
 #
@@ -72,4 +69,55 @@
 
 # date docs are at: http://ruby-doc.org/core/classes/Date.html
 # don't spend too much time worrying about them :)
+
+
 require 'date'
+      
+class User
+  
+  def initialize(username)
+    @username = username
+    @blogs = []
+  end
+  
+  attr_accessor "username", "blogs"
+  
+  def blognums
+    @blogs = blogs.sort_by {|x| x.date}.reverse
+  end
+
+  def add_blog(date, text)
+    @blogadded = Blog.new(date, @username, text)
+    @blogs.push @blogadded
+    blognums
+    @blogadded
+  end
+end
+
+
+class Blog
+  
+  def initialize(date, user, text)
+    @date = date
+    @user = user
+    @text = text
+  end
+  
+  attr_accessor "text", "date", "user"
+  
+  def summary
+    words = @text.split
+    return words[0..9].join(" ")
+  end
+  
+  def entry
+    return "#{@user.username} #{@date}\n#{@text}"
+  end
+    
+  def ==(other)
+    return date == other.date && user == other.user && text == other.text
+  end
+end
+
+
+
