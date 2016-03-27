9b97949ca3d8df8008707e6fed5ea9bdfd039729
Jack Hardy <jackhardy1@gmail.com>
2016-03-27 12:35:45 +0100

    Session2,Challenge11

--- session2/3-challenge/11_classes.rb
+++ session2/3-challenge/11_classes.rb
@@ -1,4 +1,40 @@
 # DO NOT STRUGGLE ON THIS PROBLEM FOR MORE THAN 30 MINUTES!!
+class BeerSong
+  def print_song
+number = 99
+while number >= 1
+  print "#{number_to_words(number)} on the wall,\n"
+  print "#{number_to_words(number)}.\n"
+  print "Take one down, pass it around,\n"
+  print "#{number_to_words(number - 1)} on the wall.\n"
+number -=1
+end
+
+  def number_to_words(number)
+  words_hash = {0=>"zero",1=>"one",2=>"two",3=>"three",4=>"four",5=>"five",6=>"six",7=>"seven",8=>"eight",9=>"nine",
+                      10=>"ten",11=>"eleven",12=>"twelve",13=>"thirteen",14=>"fourteen",15=>"fifteen",16=>"sixteen",
+                       17=>"seventeen", 18=>"eighteen",19=>"nineteen",
+                      20=>"twenty",30=>"thirty",40=>"forty",50=>"fifty",60=>"sixty",70=>"seventy",80=>"eighty",90=>"ninety"}
+  if number == 0
+  print "Zero bottles of beer"
+  elsif number == 1
+    print "One bottle of beer"
+  elsif number <= 20 && number > 1 || number % 10 == 0
+    word_twenty_under = words_hash[number].capitalize
+    print "#{word_twenty_under} bottles of beer"
+  else number > 20 || number < 99
+    number_2 = number % 10
+    number_1 = number - number_2
+    array = number.to_s.scan(/\d/).map {|i| i.to_i}
+    word_1 = words_hash[number_1]
+    word_2 = words_hash[number_2]
+    print "#{word_1.capitalize}-#{word_2} bottles of beer"
+  end
+end
+end
+end
+
+#Works on IRB - doesn't seem to work on Rake
 
 # Write a program that outputs the lyrics for "Ninety-nine Bottles of Beer on the Wall"
 # Your program should print the number of bottles in English, not as a number. For example:
