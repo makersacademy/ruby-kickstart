11afaa5c7b70dfd5a2f143c5e15f0d0ae38e2f73
rahulrama <rahulrama@gmail.com>
2016-04-01 14:11:58 +0100

    Session 2: challenge 7

--- session2/3-challenge/7_array.rb
+++ session2/3-challenge/7_array.rb
@@ -9,3 +9,10 @@
 # alternate_words("Can't we all get along?")      # => ["Can't", "all", "along"]
 # alternate_words("Elementary, my dear Watson!")  # => ["Elementary", "dear"]
 
+def alternate_words(sentence)
+  '!@$#%^&*()-=_+[]:;,./<>?\\|'.split(//).each {|char| sentence.gsub!(char, ' ')}
+  words = sentence.split
+ final_sentence = []
+ words.each_with_index {|word, index| final_sentence << word if index.even?}
+ final_sentence
+ end
