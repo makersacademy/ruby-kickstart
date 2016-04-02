a624dab520f8fed3f111ca8f5ea5c8dd450ec9e4
rahulrama <rahulrama@gmail.com>
2016-04-02 15:13:51 +0100

    Session 3: challenge 4

--- session3/3-challenge/4_hashes.rb
+++ session3/3-challenge/4_hashes.rb
@@ -6,3 +6,12 @@
 #
 # first_pos "The dog and the cat and the cow" # => {"The" => 0, "dog" => 1, "and" => 2, "the" => 3, "cat" => 4, "cow" => 7}
 
+
+def first_pos (string)
+
+to_return={}
+string.split.each_with_index do |word, index|
+  to_return[word] ||= index
+end
+to_return
+end
