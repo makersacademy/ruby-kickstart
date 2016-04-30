07e25dab16cdce13c1cca86c1863ce8c18f7d224
mtaner <mervetaner88@gmail.com>
2016-04-30 19:38:47 +0000

    Session2: Challenge 6 solved

--- session2/3-challenge/6_array.rb
+++ session2/3-challenge/6_array.rb
@@ -15,3 +15,8 @@
 # prime_chars? ['a', 'bcd']       # => false
 # prime_chars? ['a', 'b', 'cd']   # => false
 
+require 'prime'
+
+def prime_chars?(arr)
+    Prime.prime?(arr.join("").length.to_i)
+end
