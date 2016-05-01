93d02fc034220f937758ecbcc3fd72a585bea96f
Alex-Swann <alex_swann@hotmail.co.uk>
2016-05-01 01:36:05 +0100

    session 3 challenges 3 complete

--- session3/3-challenge/3_hashes.rb
+++ session3/3-challenge/3_hashes.rb
@@ -6,3 +6,11 @@
 #
 # Example:
 # word_count "The dog and the cat" # => {"the" => 2, "dog" => 1, "and" => 1, "cat" => 1}
+
+def word_count str
+  counts = Hash.new(0)
+  str.downcase.split.each { |word| counts[word] += 1 }
+  counts
+end
+
+
