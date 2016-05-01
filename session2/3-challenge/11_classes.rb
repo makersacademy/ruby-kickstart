44ef5302b37ea9576398e6ae7b192b1e81f5d8f2
mtaner <mervetaner88@gmail.com>
2016-05-01 08:06:29 +0000

    Session2: Challenge 11 solved

--- session2/3-challenge/11_classes.rb
+++ session2/3-challenge/11_classes.rb
@@ -20,3 +20,37 @@
 # if the parameter is greater than 99, set the number of beer bottles to 99
 # Then make a public method called print_song that outputs all stanzas from the number of bottles of beer down to zero.
 # Add any additional methods you find helpful.
+
+require 'numbers_and_words'
+
+class BeerSong
+    
+    attr_accessor :bottle_num
+    
+    def initialize(bottle_num)
+        @bottle_num = bottle_num 
+    end
+    
+    def print_song
+        if @bottle_num > 99
+            @bottle_num = 99
+        elsif @bottle_num < 0
+            @bottle_num = 0 
+        end
+        
+        return "" if @bottle_num == 0
+        
+        while @bottle_num > 0 
+            if @bottle_num > 2
+                puts "#{@bottle_num.to_words.capitalize} bottles of beer on the wall,\n#{@bottle_num.to_words.capitalize} bottles of beer,\nTake one down, pass it around,\n#{(@bottle_num - 1).to_words.capitalize} bottles of beer on the wall."
+            elsif @bottle_num == 2
+                puts "Two bottles of beer on the wall,\nTwo bottles of beer,\nTake one down, pass it around,\nOne bottle of beer on the wall."
+            elsif @bottle_num == 1
+                puts "One bottle of beer on the wall,\nOne bottle of beer,\nTake one down, pass it around,\nZero bottles of beer on the wall."
+            end
+        @bottle_num -= 1
+        end
+    end
+end
+
+ BeerSong.new(0).print_song   
