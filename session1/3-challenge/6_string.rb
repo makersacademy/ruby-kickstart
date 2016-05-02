91c58fa94240ff5db7932f8f9451533cf41a45b1
apsaxena24 <apsaxena24@gmail.com>
2016-05-02 23:35:28 +0100

    Session1 challenges

--- session1/3-challenge/6_string.rb
+++ session1/3-challenge/6_string.rb
@@ -6,5 +6,34 @@
 #
 
 def odds_and_evens(string, return_odds)
-
+	output = ""
+	if (return_odds)
+		for i in (0..string.length-1)
+	#		print string[i]
+			if (i % 2 != 0)
+				output = output + string[i]
+			end
+		end
+	else
+		for i in (0..string.length-1)
+			if (i % 2 == 0)
+				output = output + string[i]
+			end
+		end
+	end
+	return output
 end
+
+# print "value=", odds_and_evens("abcdef", true)
+=begin
+
+def odds_and_evens(string, return_odds)
+  to_return = ""
+  string.size.times do |index|
+    next if return_odds && index.even?
+    next if !return_odds && index.odd?
+    to_return << string[index]
+  end
+  to_return
+end
+=end
