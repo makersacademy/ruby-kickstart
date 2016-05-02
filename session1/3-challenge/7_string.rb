1f99eb5f9c9874a91ac64a97d16a39fb370b5c3e
Van <singularity@vannio.co.uk>
2016-05-02 14:28:16 +0100

    Complete session 1, challenge 1.7

--- session1/3-challenge/7_string.rb
+++ session1/3-challenge/7_string.rb
@@ -5,5 +5,13 @@
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
 def pirates_say_arrrrrrrrr(string)
+	afterR = String.new
 
+	for i in 0...string.length
+		if string.slice(i).downcase == 'r'
+			afterR = afterR + string.slice(i+1).to_s
+		end
+	end
+
+	return afterR
 end
