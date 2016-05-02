221e73aeb23b854c43f9e62e98efa25cc3cff217
paulkenrick <pkenrick@gmail.com>
2016-05-02 18:25:38 +0700

    Completed Session1:Challenge7

--- session1/3-challenge/7_string.rb
+++ session1/3-challenge/7_string.rb
@@ -5,5 +5,11 @@
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
 def pirates_say_arrrrrrrrr(string)
-
+	new_string = ""
+	string.length.times do |index|
+		if (string[index] == "r" || string[index] == "R") && string[index + 1] != nil
+			new_string += string[index + 1]
+		end
+	end
+	new_string
 end
