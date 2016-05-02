7be1be92788cedec2d33e9ca43116b43fafa5ff5
kpuwal <kpuwal@gmail.com>
2016-05-02 22:08:09 +0100

    4_logic completed

--- session1/3-challenge/4_logic.rb
+++ session1/3-challenge/4_logic.rb
@@ -13,6 +13,22 @@
 
 
 def grade(num_books, has_read_books)
+    if has_read_books==false
+        if num_books<10
+            "D"
+        elsif num_books>=10 && num_books<=20
+            "C"
+        else
+            "B"
+        end
+    else
+        if num_books<10
+            "C"
+        elsif num_books>=10 && num_books<=20
+            "B"
+        else
+            "A"
+        end
+    end
 end
-
 
