91ff52a1c8aff2a3142f12421b93b67c5adc0f0c
joestephens <joe.stephens@live.co.uk>
2016-05-02 19:13:16 +0100

    Defined and populated got_three? method

--- session2/3-challenge/8_array.rb
+++ session2/3-challenge/8_array.rb
@@ -5,4 +5,8 @@
 # got_three? ['a', 'a', 'b']  # => false
 # got_three? ['a', 'a', 'a']  # => true
 # got_three? [1, 2, 1, 1]     # => false
-
+def got_three?(array)
+  result = array.select.with_index { |i, index| array[index+1] == i && array[index+2] == i }
+  return true if result.length > 0
+  false
+end
