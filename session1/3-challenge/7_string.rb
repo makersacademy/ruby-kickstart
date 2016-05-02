e50d9bc0f45eee8b29701ac25a86fce770d722f3
kpuwal <kpuwal@gmail.com>
2016-05-02 22:09:29 +0100

    7_string completed

--- session1/3-challenge/7_string.rb
+++ session1/3-challenge/7_string.rb
@@ -5,5 +5,5 @@
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
 def pirates_say_arrrrrrrrr(string)
-
+    string.length>0 ? string.split("").collect!.with_index {|x, i| string[i+1] if x=="r" || x=="R"}.compact!.join('') : ''
 end
