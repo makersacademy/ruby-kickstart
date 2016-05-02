ede996f453920d461fe596cf3d39ddf249da5d86
joestephens <joe.stephens@live.co.uk>
2016-05-02 14:26:53 +0100

    Populated pirates_say_arrrrrrrrr method

--- session1/3-challenge/7_string.rb
+++ session1/3-challenge/7_string.rb
@@ -5,5 +5,12 @@
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
 def pirates_say_arrrrrrrrr(string)
+  chars = string.downcase.split(//)
+  new_string = ''
 
+  chars.each_with_index do |i, index|
+    new_string << chars[index+1].to_s if i == "r"
+  end
+
+  new_string      
 end
