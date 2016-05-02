16ec4c91125f21fc51aa2e30f74ef9775d02e3fa
Van <singularity@vannio.co.uk>
2016-05-02 23:39:33 +0100

    Complete session 2, challenge 1.8

--- session2/3-challenge/8_array.rb
+++ session2/3-challenge/8_array.rb
@@ -6,3 +6,15 @@
 # got_three? ['a', 'a', 'a']  # => true
 # got_three? [1, 2, 1, 1]     # => false
 
+def got_three? array
+	@got_three = false
+
+	array.each_with_index do |el,i|
+	 	if array[i] == el && array[i+1] == el && array[i+2] == el
+			@got_three = true
+			break
+		end
+	end
+
+	@got_three
+end
