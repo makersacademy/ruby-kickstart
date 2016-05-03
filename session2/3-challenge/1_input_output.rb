35811f9d1d5b75b85ec85f18035483de331bfd6e
paulkenrick <pkenrick@gmail.com>
2016-05-03 12:28:47 +0700

    Completed Session2:Challenge1

--- session2/3-challenge/1_input_output.rb
+++ session2/3-challenge/1_input_output.rb
@@ -8,6 +8,15 @@
 # "11\n7\n18\n" to standard output.
 
 def sum_difference_product
-  # your code goes here
-  
+  input = gets.chomp
+  number_1 = input.split(" ")[0].to_i
+  number_2 = input.split(" ")[1].to_i
+
+  sum = number_1 + number_2
+  diff = number_1 - number_2
+  prod = number_1 * number_2
+
+  print sum.to_s + "\n"
+  print diff.to_s + "\n"
+  print prod.to_s + "\n"
 end
