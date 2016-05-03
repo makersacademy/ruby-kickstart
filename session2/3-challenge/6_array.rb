259ed38e3f8780f0b40234674aab452ef40cc87b
Elena <elena.vilimaite@gmail.com>
2016-05-03 14:56:46 +0100

    session 2, challange 6

--- session2/3-challenge/6_array.rb
+++ session2/3-challenge/6_array.rb
@@ -14,4 +14,8 @@
 # prime_chars? ['ab', 'cd']       # => false
 # prime_chars? ['a', 'bcd']       # => false
 # prime_chars? ['a', 'b', 'cd']   # => false
+require 'prime'
 
+def prime_chars?(stringArray)
+	return Prime.prime? stringArray.join().bytesize 
+end
