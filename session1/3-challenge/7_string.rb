9d4de10b586645a2dc7208eeab73769963eb6690
toby676 <tjcmui@gmail.com>
2016-05-03 01:18:28 +0100

    1:7 complete

--- session1/3-challenge/7_string.rb
+++ session1/3-challenge/7_string.rb
@@ -5,5 +5,15 @@
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
 def pirates_say_arrrrrrrrr(string)
-
+    array = string.split("")
+    newarray = []
+    array.each_with_index do |v,i|
+    	if v == "r" || v == "R"
+    		newarray << i
+    	end
+    end
+    newarray.map! do |x| 
+    	x + 1
+    end
+    array.values_at(*newarray).join
 end
