c230c6313a07826d5c5cef03f68f25f11ec1d945
joestephens <joe.stephens@live.co.uk>
2016-05-03 15:28:40 +0100

    Created and populated BeerSong class

--- session2/3-challenge/11_classes.rb
+++ session2/3-challenge/11_classes.rb
@@ -20,3 +20,40 @@
 # if the parameter is greater than 99, set the number of beer bottles to 99
 # Then make a public method called print_song that outputs all stanzas from the number of bottles of beer down to zero.
 # Add any additional methods you find helpful.
+class BeerSong
+  def initialize(num_bottles=99)
+    if num_bottles < 0
+      @num_bottles = 0
+    elsif num_bottles > 99
+      @num_bottles = 99
+    else
+      @num_bottles = num_bottles
+    end
+  end
+
+  def num_to_s(num, word=" bottle")
+    num == 1 ? ending = word + "" : ending = word + "s"
+    split_num = num.to_s.chars.map(&:to_i)
+    eleven_to_twenty = ['eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+    tens = ['ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+    one_to_nine = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'zero']
+    if num < 10 # 1 to 9
+      return one_to_nine[num-1] + ending
+    elsif num > 10 && num < 20 # 10 to 
+      return eleven_to_twenty[num-11] + ending
+    else
+      return tens[split_num[0]-1] + ending if split_num[1] == 0
+      return tens[split_num[0]-1] + "-" + one_to_nine[split_num[1]-1] + ending
+    end
+  end
+
+  def print_song
+    while @num_bottles > 0 
+      puts num_to_s(@num_bottles).capitalize + " of beer on the wall,"
+      puts num_to_s(@num_bottles).capitalize + " of beer,"
+      puts "Take one down, pass it around,"
+      @num_bottles -= 1
+      puts num_to_s(@num_bottles).capitalize + " of beer on the wall."
+    end 
+  end
+end
