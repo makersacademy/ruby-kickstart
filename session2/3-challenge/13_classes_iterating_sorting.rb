67de23fc41f4a19e08a19dfa3250d302db389feb
Elena <elena.vilimaite@gmail.com>
2016-05-04 15:54:24 +0100

    session 2, challange 13 -- session 2 done :)

--- session2/3-challenge/13_classes_iterating_sorting.rb
+++ session2/3-challenge/13_classes_iterating_sorting.rb
@@ -31,7 +31,7 @@
 # lissa.blogs                     # => [#<Blog:0x007fec28c49b88
 #     @date=#<Date: 2010-05-28 ((2455345j,0s,0n),+0s,2299161j)>,
 #     @text="Sailor Mars is my favourite",
-#       @user=#<User:0x007fec2902e5c8 @blogs=[...], @username="QTSort">>] 
+#       @user=#<User:0x007fec2902e5c8 @blogs=[...], @username="QTSort">>]
 #
 # blog1 = lissa.blogs.first
 # blog1.user                      # => lissa
@@ -73,3 +73,42 @@
 # date docs are at: http://ruby-doc.org/core/classes/Date.html
 # don't spend too much time worrying about them :)
 require 'date'
+
+class User
+	attr_accessor :username, :blogs
+
+	def initialize(username)
+		@username = username
+		@blogs = []
+	end
+
+	def add_blog(date, text)
+		newBlog = Blog.new(date, self, text)
+		@blogs << newBlog
+		@blogs = @blogs.sort! {|x, y| y.date <=> x.date}
+		return newBlog
+	end
+
+end
+
+class Blog
+	attr_accessor :date, :text, :user
+
+	def initialize(date, user, text)
+		@date = date
+		@user = user
+		@text = text
+	end
+
+	def summary
+		return @text.split(' ')[0..9].join(' ')
+
+	end
+	def entry
+		return "#{user.username} #{date}\n#{text}"
+	end
+
+	def ==(other)
+		self.date == other.date && self.text == other.text && self.user == other.user
+	end
+end
