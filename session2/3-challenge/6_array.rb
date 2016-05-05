0c80c31d26ba2b6135c999fc936836a34733bcb3
awye765 <awye765@googlemail.com>
2016-05-05 11:50:35 +0100

    Session 2 - Challenge 6 completed

--- session2/3-challenge/6_array.rb
+++ session2/3-challenge/6_array.rb
@@ -15,3 +15,22 @@
 # prime_chars? ['a', 'bcd']       # => false
 # prime_chars? ['a', 'b', 'cd']   # => false
 
+def prime_chars?(array)
+# Creates a new method that takes one argument,
+# an array in the format [a, b].
+
+  require 'prime'
+  # Requires the prime class, which allows us
+  # to use its methods.
+
+  Prime.prime?(array.join.length)
+  # (1) the code in breackets takes the array
+  #     and joins each substring together into
+  #     a single string, e.g. ["a", "b"] becomes
+  #     "ab"
+  # (2) .length then calculates the length of the
+  #     resulting string.
+  # (3) the prime? method from the Prime class is
+  #     boolean called on the resulting value to
+  #     check if the value is a prime or not.
+end
