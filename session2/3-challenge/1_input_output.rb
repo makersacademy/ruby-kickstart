6c9450a14159341fa97cb68660d8dbd0e534338b
Tim <timchipperfield@gmail.com>
2016-05-06 00:48:06 +0100

    section 2 no 1

--- session2/3-challenge/1_input_output.rb
+++ session2/3-challenge/1_input_output.rb
@@ -8,6 +8,9 @@
 # "11\n7\n18\n" to standard output.
 
 def sum_difference_product
-  # your code goes here
-  
+  input = gets.chomp
+  arr = input.split(" ").map {|x| x.to_i}.to_a
+  print "#{arr[0] + arr[1]}
+  #{arr[0] - arr[1]}
+  #{arr[0] * arr[1]}"
 end
