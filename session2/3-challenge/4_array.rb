8d122d7e691f5b9216fe518bf80183b43ae6eede
kpuwal <kpuwal@gmail.com>
2016-05-05 08:22:35 +0100

    session2, 4_array completed

--- session2/3-challenge/4_array.rb
+++ session2/3-challenge/4_array.rb
@@ -7,6 +7,9 @@
 # get_squares [25, 4, 9, 6, 50, 16, 5] # => [4, 5]
 
 # This time you will have to define the method, it's called: get_squares
-
-
+def get_squares(arr)
+    squares_arr= arr.map {|x| x*x}
+    common_numbers= squares_arr.select {|x| x if arr.include?(x)}
+    common_numbers.map {|x| Math.sqrt(x).to_i}.sort
+end
 
