5b6a7ae6a7fdab2aa490b6131989c6853e09a25f
Hassan Radheyyan <hassan.rad@gmail.com>
2016-05-05 21:01:50 +0400

    Challenge 2:3

--- session2/3-challenge/3_array.rb
+++ session2/3-challenge/3_array.rb
@@ -6,7 +6,16 @@
 # "".every_other_char         # => ""
 
 class String
+ 
   def every_other_char
+  	input = gets.split(//)
+  	input.length.times do |x|
+  		print input[x] if x.even?
+  	end
   end
-  
+
+puts "Enter some characters"
+new_string = String.new
+new_string.every_other_char
+
 end
