a3335b49f57a0bff8d40dc592c5580d5baa0c1e9
Hassan Radheyyan <hassan.rad@gmail.com>
2016-05-05 16:50:01 +0400

    Challenge 1:7

--- session1/3-challenge/7_string.rb
+++ session1/3-challenge/7_string.rb
@@ -5,5 +5,22 @@
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
 def pirates_say_arrrrrrrrr(string)
+string.downcase!
+new_string = ""
+length = string.size
+count = 0
+while length > count do
+	if string[count] == "r"
+		count += 1
+		new_string << string[count,1]
+	else
+		count += 1
+	end
 
 end
+print new_string
+end
+
+pirates_say_arrrrrrrrr("are you really learning Ruby?")
+pirates_say_arrrrrrrrr("Katy Perry is on the radio!")
+pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")
