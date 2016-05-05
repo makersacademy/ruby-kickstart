e0b3edc1fca21e45a9e54f502e03f2cc3a77ed0e
awye765 <awye765@googlemail.com>
2016-05-05 10:08:14 +0100

    Session 2 - Challenge 3 completed

--- session2/3-challenge/3_array.rb
+++ session2/3-challenge/3_array.rb
@@ -7,6 +7,18 @@
 
 class String
   def every_other_char
+    to_return = ""
+    each_char.with_index do | x, i |
+      if i.even?
+        to_return << x
+      end
+
+    end
+
+  to_return
+
   end
-  
+
 end
+
+
