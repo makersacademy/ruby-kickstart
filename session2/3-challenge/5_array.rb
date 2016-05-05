d31d25269e1baf1e1ef44b0c991f6008362b0e95
Stephen Dawes <sdawes@outlook.com>
2016-05-05 12:21:34 +0100

    session2. 3-challenge. 5_array. Dawes submission. OMG I solved one completely on my own! :-)

--- session2/3-challenge/5_array.rb
+++ session2/3-challenge/5_array.rb
@@ -14,3 +14,8 @@
 #
 # mod_three [0,1,2,3,4,5,6,7] # => [1, 2, 1, 2, 1]
 
+def mod_three(arr)
+	new_arr = arr.map { |index| index % 3 }
+	new_arr = new_arr.reject { |index| index == 0 }
+	new_arr
+end
