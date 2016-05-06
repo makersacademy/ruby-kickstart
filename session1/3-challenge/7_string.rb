7df735241e5956ae1d6711b2c6833d0df211ccf8
Andy Bell <andy.bell.github@gmail.com>
2016-05-06 16:26:29 +0000

    Completed challenge 1:7

--- session1/3-challenge/7_string.rb
+++ session1/3-challenge/7_string.rb
@@ -5,5 +5,8 @@
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
 def pirates_say_arrrrrrrrr(string)
-
+        arr = []
+        temp = string.downcase.split(//)
+	temp.each.with_index{|char,index| arr.push(temp[index+1]) if char === "r" }
+        arr.join
 end
