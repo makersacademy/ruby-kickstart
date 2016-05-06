9cf5e764e4b9baddb751c656dae7db85d276381b
Chris Lawrence <chris.lawrence@gmx.com>
2016-05-06 18:48:20 +0100

    2:6 complete

--- session2/3-challenge/6_array.rb
+++ session2/3-challenge/6_array.rb
@@ -15,3 +15,9 @@
 # prime_chars? ['a', 'bcd']       # => false
 # prime_chars? ['a', 'b', 'cd']   # => false
 
+def prime_chars?(arr)
+	num = arr.join.length
+	return false if num <= 1
+	Math.sqrt(num).to_i.downto(2).each {|x| return false if num % x == 0}
+	true
+end
