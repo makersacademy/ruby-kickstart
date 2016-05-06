b4ef225d7a7314d1d15af27a74671bcc9d76ce1f
toby676 <tjcmui@gmail.com>
2016-05-06 11:35:43 +0100

    3:3 complete

--- session3/3-challenge/3_hashes.rb
+++ session3/3-challenge/3_hashes.rb
@@ -6,3 +6,9 @@
 #
 # Example:
 # word_count "The dog and the cat" # => {"the" => 2, "dog" => 1, "and" => 1, "cat" => 1}
+def word_count(string)
+	array = string.downcase.split(" ")
+     hash = Hash.new(0)
+     array.each{|key| hash[key] += 1}
+     hash
+end
