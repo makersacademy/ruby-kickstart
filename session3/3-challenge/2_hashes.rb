1e850f284f277944ea56caeb6e3f160c1b4e1436
toby676 <tjcmui@gmail.com>
2016-05-06 11:21:52 +0100

    3:2 completed

--- session3/3-challenge/2_hashes.rb
+++ session3/3-challenge/2_hashes.rb
@@ -8,6 +8,17 @@
 # staircase 4  # => {1 => [], 3 => [2]}
 # staircase 5  # => {1 => [], 3 => [2], 5 =>[2, 4]}
 
-
-
-
+def staircase(n)
+   hash = {}
+   def evens(x)
+    [*1..x].delete_if {|x| x.odd?}	
+   end
+   1.upto n do |v|
+   	if v == 1
+   		hash[v] = []	
+   	elsif v.odd?
+   		hash[v]=evens(v)
+    end
+   end
+   hash
+end
