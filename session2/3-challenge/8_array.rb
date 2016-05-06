aac30c5693c53f477270ec3e2d4a87ec2569f340
Thomas Harker <tom.harker@hotmail.co.uk>
2016-05-06 15:13:22 +0100

    Add completed challenge 2:8

--- session2/3-challenge/8_array.rb
+++ session2/3-challenge/8_array.rb
@@ -6,3 +6,22 @@
 # got_three? ['a', 'a', 'a']  # => true
 # got_three? [1, 2, 1, 1]     # => false
 
+def got_three?(ary)
+
+	outcome = false
+
+	for index in 0..(ary.length-2)
+
+# For the elements of the array in the range of index 0 to the second to last value...
+		
+		outcome = (ary[index] == ary[index + 1]) && (ary[index] == ary[index + 2])
+
+# For this conditional to be true the current element must have the same value as the two elements following it.
+
+		break if outcome
+
+	end
+
+	outcome
+
+end
