282549289e41e04b01989c8cab1ecd4cb725b1a7
Tim <timchipperfield@gmail.com>
2016-05-06 08:50:28 +0100

    Session 2 challenge 10

--- session2/3-challenge/10_classes.rb
+++ session2/3-challenge/10_classes.rb
@@ -16,5 +16,29 @@
 #
 
 class Person
+  def initialize(name, age)
+    @name = name
+    @age = age
+  end
+
+  def birthday
+    @age += 1
+  end
+
+  def name
+    @name
+  end
+
+  def age
+    @age
+  end
+
+  def name=(newname)
+    @name = newname
+  end
+
+  def age=(newage)
+    @age = newage
+  end
 
 end
