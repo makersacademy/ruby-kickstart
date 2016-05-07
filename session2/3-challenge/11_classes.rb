ca58c09155dec8e4a7eeda727e61d9a38d550846
Thomas Harker <tom.harker@hotmail.co.uk>
2016-05-07 21:12:36 +0100

    Add INCOMPLETE challenge 2:12 - successfully runs the first 5 rake tests but fails the sixth

--- session2/3-challenge/11_classes.rb
+++ session2/3-challenge/11_classes.rb
@@ -20,3 +20,82 @@
 # if the parameter is greater than 99, set the number of beer bottles to 99
 # Then make a public method called print_song that outputs all stanzas from the number of bottles of beer down to zero.
 # Add any additional methods you find helpful.
+
+
+class BeerSong
+	def initialize(number)
+		if number <= 0
+			@bottles = 0
+		elsif number > 99
+			@bottles = 99
+		else @bottles = number
+		end
+
+		@one = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+		@ten = ['ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+		@teen = ['eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+
+	end
+
+	def english_number
+
+		if @bottles < 10
+			@number_string = @one[@bottles - 1]
+		
+		elsif @bottles < 20
+			@number_string = @teen [@bottles -1]
+
+		else
+			@array = @bottles.to_s.chars
+
+			@number_string = @tens[@array[0].to_i - 1]
+
+			unless @array[1] == '0'
+				@number_string = @number_string + '-'
+
+				@number_string = @one[@array[1].to_i -1]
+
+			end
+		end
+
+		if @bottles > 1
+			@ref = "bottles"
+		else
+			@ref = "bottle"
+		end
+	end
+
+	def print_song
+
+
+		while @bottles > 0
+			
+			english_number
+
+			puts "#{@number_string.capitalize + ' ' + @ref} of beer on the wall,"
+			puts "#{@number_string.capitalize + ' ' + @ref} of beer,"
+			puts "Take one down, pass it around,"
+
+			if @bottles > 1
+
+				@bottles = @bottles - 1
+
+				english_number
+
+				puts "#{@number_string.capitalize + ' ' + @ref} of beer on the wall."
+
+			else
+				@bottles = @bottles - 1
+
+				puts "Zero bottles of beer on the wall."
+			end
+		end
+	end
+end
+
+
+
+
+	
+
+
