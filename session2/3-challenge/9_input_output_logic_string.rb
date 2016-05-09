a9a8c5cbbaa77c5714a864e4b835fb4611c9329c
Faisal Mahmood <fahmahmood@gmail.com>
2016-05-09 04:28:52 +0100

    2:9 grandma

--- session2/3-challenge/9_input_output_logic_string.rb
+++ session2/3-challenge/9_input_output_logic_string.rb
@@ -24,9 +24,17 @@
 # USER:     BYE
 
 def deaf_grandma
-
+  input = gets.chomp
+  until input == "BYE"
+    if input.upcase == input && input != ""  then
+      puts "NO, NOT SINCE 1938!"
+  else 
+  	puts "HUH?! SPEAK UP, SONNY!"
+    end 
+    input = gets.chomp
 end
 
+end 
 
 
 
