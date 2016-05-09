c4018568ce5db24a99cef29ef08ca5a69f679ad6
Lukasz <lukasz.grychowski@gmail.com>
2016-05-09 09:53:05 +0100

    13_classes_iterating_sorting.rb failing rake

--- session2/3-challenge/13_classes_iterating_sorting.rb
+++ session2/3-challenge/13_classes_iterating_sorting.rb
@@ -31,7 +31,7 @@
 # lissa.blogs                     # => [#<Blog:0x007fec28c49b88
 #     @date=#<Date: 2010-05-28 ((2455345j,0s,0n),+0s,2299161j)>,
 #     @text="Sailor Mars is my favourite",
-#       @user=#<User:0x007fec2902e5c8 @blogs=[...], @username="QTSort">>] 
+#       @user=#<User:0x007fec2902e5c8 @blogs=[...], @username="QTSort">>]
 #
 # blog1 = lissa.blogs.first
 # blog1.user                      # => lissa
@@ -73,3 +73,83 @@
 # date docs are at: http://ruby-doc.org/core/classes/Date.html
 # don't spend too much time worrying about them :)
 require 'date'
+
+class User
+  attr_accessor :username, :blogs
+
+  def initialize(username)
+    @username = username
+    @blogs = []
+  end
+
+  def add_blog(date, text)
+    new_blog = Blog.new(date, @username, text)
+    @blogs << new_blog
+    @blogs = @blogs.sort_by {|blog| blog.date}.reverse
+    new_blog
+  end
+end
+
+class Blog
+  attr_accessor :text, :date, :user
+
+  def initilaize(text, date, user)
+    @text = text
+    @date = date
+    @user = user
+  end
+
+  def summary
+    @text.split[0...10].join(" ")
+  end
+
+  def entry
+    "#{user.username} #{date}\n#{text}"
+  end
+
+  def ==(other)
+    @text == other.text &&
+    @date == other.date &&
+    @user == other.user
+  end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
