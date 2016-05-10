776ab87f234bdfdc0f3eaee6d193f0ac01826d41
alex <alexander.mellor@gmail.com>
2016-05-10 20:53:13 +0000

    2:11 answer

--- session2/3-challenge/11_classes.rb
+++ session2/3-challenge/11_classes.rb
@@ -22,15 +22,52 @@
 # Add any additional methods you find helpful.
 
 class BeerSong
-  
-  def initialize(number_of_beers)
-    
-      if number_of_beers < 0
-        @number_of_beers = 0 
-      elsif number_of_beers > 99
-        @number_of_beers = 99
-      else
-        @number_of_beers = number_of_beers
-      end
-  
+  attr_accessor :beers
+
+  def initialize(beers)
+    beers = 0  if beers < 0
+    beers = 99 if beers > 99
+    @beers = beers
+  end
+
+def translate(n)
+  singles = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"]
+  tens = ["zero", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
+  if n == 0
+    return "Zero"
+  elsif n < 20                      # less than 20 bottles
+    return singles[n].capitalize
+  elsif n >= 20 && n%10 == 0        #  20, 30, 40, 50, 60, 70, 80, 90
+    return tens[n/10].capitalize
+  else 
+    return (tens[n/10] + "-" + singles[n%10]).capitalize    # 21..29, 31..39 etc
+  end
+end
+
+  def bottle(n)   # plural of bottles for song
+    if n == 1 
+       'bottle' 
+    else 
+       'bottles' 
+    end
+  end
+
+  def song_words(n)
+    if n == 0 
+      puts "Zero bottles of been on the wall."
+    else
+      puts "#{translate(n)} #{bottle(n)} of beer on the wall,"
+      puts "#{translate(n)} #{bottle(n)} of beer,"
+      puts "Take one down, pass it around,"
+      puts "#{translate(n - 1)} #{bottle(n - 1)} of beer on the wall."
+   end
+  end
+
+  def print_song
+    until @beers == 0
+      song_words(@beers)
+      @beers -= 1
+    end
+  end
+
 end
