eb008d7a82eec00692915e4b7acf6dfca79ee213
alex <alexander.mellor@gmail.com>
2016-05-11 09:02:26 +0000

    2:13 answer

--- session2/3-challenge/13_classes_iterating_sorting.rb
+++ session2/3-challenge/13_classes_iterating_sorting.rb
@@ -76,11 +76,11 @@ require 'date'
 
 class User
   attr_accessor 'username', 'blogs'
-  
+
   def initialize(username)
     @username, @blogs = username, []
   end
-  
+
   def add_blog(date, text)
     added_blog = Blog.new(date, self, text)
     blogs << added_blog
@@ -89,24 +89,26 @@ class User
   end
 end
 
+
+
 class Blog
   attr_accessor 'date', 'user', 'text'
-  
+
   def initialize(date, user, text)
     @date, @user, @text = date, user, text
   end
-  
+
   def summary
     text.split[0..9].join(' ')
   end
-  
+
   def entry
     "#{user.username} #{date}\n#{text}"
   end
-  
-  def == (other)
-    date == other.date &&
-    user == other.user &&
-    text == other.text
+
+  def ==(other)
+    date   == other.date &&
+      user == other.user &&
+      text == other.text
   end
 end
