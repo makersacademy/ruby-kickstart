e2ba1c97e954bdf7a092fdd29764713dfb7472bc
and1cos <cosminandone@icloud.com>
2016-05-17 21:52:23 +0100

    session 2
    these are the challenges for session 2

--- session2/3-challenge/12_classes.rb
+++ session2/3-challenge/12_classes.rb
@@ -23,8 +23,30 @@
 # f.to_f               # => 0.5
 
 class Fraction
+  attr_accessor :numerator, :denominator
+
+  def initialize(numerator, denominator)
+    self.numerator = numerator
+    self.denominator = denominator
+  end
+
+  def to_s
+    "#{numerator}/#{denominator}"
+  end
+
+  def to_f
+    numerator / denominator.to_f
+  end
+
   def gcd(a,b)
     return a if b == 0
     gcd(b, a%b)
   end
+
+  def lowest
+    divisor = gcd(numerator,denominator)
+    Fraction.new(numerator/divisor, denominator/divisor)
+  end
 end
+
+rake 2:12
