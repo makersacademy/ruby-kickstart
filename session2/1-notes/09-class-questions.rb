73c6f876571f29825e119bc4a61f92f4e42365a6
Joel Thompson <joel.thompson@intercom.io>
2016-06-13 17:47:56 -0700

    something something

--- session2/1-notes/09-class-questions.rb
+++ session2/1-notes/09-class-questions.rb
@@ -1,13 +1,21 @@
 # Between Yankees and Baseball team
 # which is a class and which is an instance?
 
+#baseball team is the class, yankees is the instance
+
 # Between Person and Enoch
 # which is a class and which is an instance?
 
+#had to google enoch because WTF is an enoch
+#person is the class, enoch is the instance
+
 # Can you think of another class and several instances?
 
-
+#car : porche, toyota
+#
 
 # Are classes objects?
 # If no, where do you think their methods are stored?
 # If yes, what class are they instances of?
+
+#classes aren't objects, they are a way to describe objects?
