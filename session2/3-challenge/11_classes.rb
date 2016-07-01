42194548b5ec140ff42d1f72a9d36cab8d1c2348
chuckwalla <jeelthompson@gmail.com>
2016-06-30 22:25:22 -0700

    started on 2:11

--- session2/3-challenge/11_classes.rb
+++ session2/3-challenge/11_classes.rb
@@ -20,3 +20,39 @@
 # if the parameter is greater than 99, set the number of beer bottles to 99
 # Then make a public method called print_song that outputs all stanzas from the number of bottles of beer down to zero.
 # Add any additional methods you find helpful.
+
+class BeerSong
+
+	attr_reader :bottles
+
+	def initialize(bottles)
+		if bottles > 99
+			@bottles = 99
+		elsif bottles < 0
+			@bottles = 0
+		else
+			@bottles = bottles
+		end
+	end
+
+	def num_to_string(num)
+		#takes a string and returns the proper number
+	end
+
+	def print_song
+		bottles_left = bottles
+
+		while bottles_left >= 0
+			puts "Ninety-nine bottles of beer on the wall,"
+			puts "Ninety-nine bottles of beer,"
+			puts "Take one down, pass it around,"
+			bottles_left -= 1
+			puts "Ninety-eight bottles of beer on the wall."
+			puts
+
+		end
+	end
+
+end
+
+song = BeerSong.new(5).print_song
