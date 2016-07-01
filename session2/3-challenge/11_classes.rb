8e510cb9adec786a00192e2badae402e8791f7b4
chuckwalla <jeelthompson@gmail.com>
2016-07-01 07:50:10 -0700

    finished 2:11

--- session2/3-challenge/11_classes.rb
+++ session2/3-challenge/11_classes.rb
@@ -35,24 +35,56 @@ class BeerSong
 		end
 	end
 
-	def num_to_string(num)
+	def bottles_to_string(num)
 		#takes a string and returns the proper number
+		retstring = ""
+		ones_a = %w(one two three four five six seven eight nine)
+  		teens_a = %w(ten eleven twelve thirteen fourteen fifteen sixteen seventeen 
+               eighteen nineteen)
+  		tens_a = %w(ten twenty thirty forty fifty sixty seventy eighty ninety)
+
+		retstring = 'zero' if num == 0
+
+		tens = num/10
+		ones = num - tens*10
+
+		if num == 0 
+			retstring = 'zero'
+		elsif num >= 1 && num <= 9
+			retstring = ones_a[num-1]				
+		elsif num >= 10 && num <= 19
+			retstring = teens_a[num-10]
+		else
+			retstring = "#{tens_a[tens-1]}"
+			retstring << "-#{ones_a[ones-1]}" if ones != 0
+				
+		end
+
+		retstring[0] = retstring[0].upcase if retstring[0] != nil
+		#p retstring
+		return retstring
+	end
+
+	def bottle_builder(num)
+		if num == 1
+			return 'bottle'
+		else
+			return 'bottles'
+		end
 	end
 
 	def print_song
 		bottles_left = bottles
 
-		while bottles_left >= 0
-			puts "Ninety-nine bottles of beer on the wall,"
-			puts "Ninety-nine bottles of beer,"
+		while bottles_left >= 1
+			puts "#{bottles_to_string(bottles_left)} #{bottle_builder(bottles_left)} of beer on the wall,"
+			puts "#{bottles_to_string(bottles_left)} #{bottle_builder(bottles_left)} of beer,"
 			puts "Take one down, pass it around,"
 			bottles_left -= 1
-			puts "Ninety-eight bottles of beer on the wall."
-			puts
-
+			puts "#{bottles_to_string(bottles_left)} #{bottle_builder(bottles_left)} of beer on the wall."
 		end
 	end
 
 end
 
-song = BeerSong.new(5).print_song
+ # song = BeerSong.new(1000).print_song
