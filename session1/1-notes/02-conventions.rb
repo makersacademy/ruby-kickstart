6285f76320f4ffb87b21a3d6bb8a9043a3590d72
Louisa Spicer <Louisaspicer@gmail.com>
2016-08-28 15:54:26 +0100

    Added some notes

--- session1/1-notes/02-conventions.rb
+++ session1/1-notes/02-conventions.rb
@@ -1,7 +1,7 @@
 # In the notes, examples and cheat sheets provided, we use a few conventions in our code to make it easy
 # to see the results of expressions. To show the results of a line, we use the following notation
 
-3 + 8       # => 12
+3 + 9       # => 12
 my_var = 4
 my_var      # => 4
 
