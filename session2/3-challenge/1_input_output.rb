b3949ba8d4f11201b64d16f1f4edac8c6c1b6f10
Louisa Spicer <Louisaspicer@gmail.com>
2016-08-30 22:48:14 +0100

    answered 2:1

--- session2/3-challenge/1_input_output.rb
+++ session2/3-challenge/1_input_output.rb
@@ -7,7 +7,19 @@
 # Standard input will be like "9 2\n" and will expect you to print
 # "11\n7\n18\n" to standard output.
 
+#split the numbers in the input string and put them into an array to work with
+#add the two numbers together and save in a variable
+#subtract one number from the other and transform to absolute value
+
 def sum_difference_product
-  # your code goes here
+  input = gets.split
+  numbers = input.map {|num| num.to_i}
+  sum = numbers.inject(:+)
+  difference = numbers.inject(:-)
+  product = numbers.inject(:*)
+
+  puts sum
+  puts difference
+  puts product
   
 end
