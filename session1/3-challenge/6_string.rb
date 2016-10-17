019915cec327f13c0fadd7bd73adec3be6d914fc
Dan Pelensky <dan@pelensky.com>
2016-10-17 16:22:32 +0100

    challenge 6 solution

--- session1/3-challenge/6_string.rb
+++ session1/3-challenge/6_string.rb
@@ -5,6 +5,27 @@
 # If you have no idea where to begin, remember to check out the cheatsheets for string and logic/control
 #
 
+#I can't figure out why this keeps giving me an error. Moving on.
+=begin
 def odds_and_evens(string, return_odds)
+  split_string = string.split("")
+  if return_odds == true
+    odd_array = split_string.select.with_index { |_, i| i.odd? }
+    odd_array.join
+  else
+    even array = split_string.select.with_index { |_, i| i.even? }
+    even_array.join
+  end
+end
+=end
 
+#solution
+def odds_and_evens(string, return_odds)
+  to_return = ""
+  string.size.times do |index|
+    next if return_odds && index.even?
+    next if !return_odds && index.odd?
+    to_return << string[index]
+  end
+  to_return
 end
