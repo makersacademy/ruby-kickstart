0a8aaa2be65c4629ac45c814af5ad7b32f56df9c
Dan Pelensky <dan@pelensky.com>
2016-10-19 14:45:21 +0100

    challenge 3 completed

--- session2/3-challenge/3_array.rb
+++ session2/3-challenge/3_array.rb
@@ -7,6 +7,16 @@
 
 class String
   def every_other_char
+    #create the new string where the even indexed letters will be
+    new_string =""
+    #each_char passes each character in str to the given block, or returns an enumerator if no block is given.
+    each_char.with_index do |char, index|
+      #adds the character where the index is even to the new_string
+      new_string << char if index.even?
+    end
+    #returns new_string
+    new_string
+
   end
-  
+
 end
