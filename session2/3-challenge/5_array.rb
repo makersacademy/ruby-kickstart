441075237e52a676164bcf02b05da375f9527d3e
Dan Pelensky <dan@pelensky.com>
2016-10-19 15:51:41 +0100

    completed challenge 5

--- session2/3-challenge/5_array.rb
+++ session2/3-challenge/5_array.rb
@@ -14,3 +14,9 @@
 #
 # mod_three [0,1,2,3,4,5,6,7] # => [1, 2, 1, 2, 1]
 
+def mod_three (array_of_numbers)
+  # find which numbers are divisible by 3
+  new_array = array_of_numbers.map { |x| x % 3 }
+  # remove the value zero (ie. number is divisible by 3)
+  new_array -= [0]
+end
