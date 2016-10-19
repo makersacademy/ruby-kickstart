1a7deb38b5b61d9d1ae25809c3b0e2cb55556cb6
Dan Pelensky <dan@pelensky.com>
2016-10-19 16:46:21 +0100

    completed challenge 8

--- session2/3-challenge/8_array.rb
+++ session2/3-challenge/8_array.rb
@@ -6,3 +6,18 @@
 # got_three? ['a', 'a', 'a']  # => true
 # got_three? [1, 2, 1, 1]     # => false
 
+def got_three?(array)
+  #check the array using each consecutive
+array.each_cons(3) do |a, b, c|
+  #checks if a is equal to b, and if b is equal to c
+  if a == b
+    if b == c
+      #returns true if so
+      return true
+    end
+  else
+    #returns false if not
+    false
+  end
+end
+end
