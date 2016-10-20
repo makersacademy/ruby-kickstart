20336ae2ae44a1c1e113ce4164fd62b69f11f58b
Dan Pelensky <dan@pelensky.com>
2016-10-20 13:30:06 +0100

    challenge 1 completed

--- session3/3-challenge/1_blocks.rb
+++ session3/3-challenge/1_blocks.rb
@@ -4,3 +4,8 @@
 # reverse_map(1, 2, 3) { |i| i * 2 }      # => [6, 4, 2]
 # reverse_map(1, 2, 3) { |i| i * 2 }      # => [6, 4, 2]
 
+#define the method, and list optional elements, and a block of code (undefined in this case)
+def reverse_map(*elements, &block)
+  #reverse the order of the elements received, and map it, passing it in to the block
+  elements.reverse.map(&block)
+end
