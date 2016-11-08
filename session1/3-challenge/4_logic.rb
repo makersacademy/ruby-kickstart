49e1ec222b51c6245e9bef2f28508117916551c0
Natalia Stafeeva <n.stafeeva@gmail.com>
2016-11-08 09:53:40 +0000

    logic

--- session1/3-challenge/4_logic.rb
+++ session1/3-challenge/4_logic.rb
@@ -13,6 +13,23 @@
 
 
 def grade(num_books, has_read_books)
+	if has_read_books == false
+		if num_books < 10
+			"D"
+		elsif num_books > 20
+			"B"
+		else
+			"C"
+		end
+	else
+		if num_books < 10
+			"C"
+		elsif num_books > 20
+			"A"
+		else
+			"B"
+		end
+	end
 end
 
 
