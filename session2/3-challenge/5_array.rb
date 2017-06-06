e3c2ef6074c2f0ceb6220af5c7336c6b7edb68d8
Gurjinder Massan <gurjinder.massan@gmail.com>
2017-06-06 19:06:35 +0100

    Session 2 Exercise 5

--- session2/3-challenge/5_array.rb
+++ session2/3-challenge/5_array.rb
@@ -14,3 +14,10 @@
 #
 # mod_three [0,1,2,3,4,5,6,7] # => [1, 2, 1, 2, 1]
 
+def mod_three(list)
+  newlist = list.map {|i| i % 3}
+  newlist.reject! { |i| i == 0 }
+  return newlist
+end
+
+puts mod_three([0, 1, 2, 3, 4, 5, 6, 7])
