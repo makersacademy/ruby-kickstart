5a2562c88d4e894f23c30fe4dcd6bbb03dd96052
arnold879 <machapuchre67@gmail.com>
2017-08-10 12:28:31 +0100

    Update 7_array.rb

--- session2/3-challenge/7_array.rb
+++ session2/3-challenge/7_array.rb
@@ -9,3 +9,11 @@
 # alternate_words("Can't we all get along?")      # => ["Can't", "all", "along"]
 # alternate_words("Elementary, my dear Watson!")  # => ["Elementary", "dear"]
 
+def alternate_words(string)
+  string.gsub(/\W/,"")
+  string.split.each_with_index.map {|x,y|  x.gsub(/\W/,"") if y%2==0}.compact
+end
+#alternate_words("Lorem ipsum dolor sit amet.")  # => ["Lorem", "dolor", "amet"]
+alternate_words("Can't we all get along?")      # => ["Can't", "all", "along"]
+#alternate_words("Elementary, my dear Watson!")  # => ["Elementary", "dear"]
+
