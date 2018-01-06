53b15e982c0524108b11f9498dc8f8be38253ed8
Zoe Kavanagh <kzoe3162@gmail.com>
2018-01-06 22:23:33 +0000

    answer to session 1 challenge 6

--- session1/3-challenge/6_string.rb
+++ session1/3-challenge/6_string.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # You'll get a string and a boolean.
 # When the boolean is true, return a new string containing all the odd characters.
 # When the boolean is false, return a new string containing all the even characters.
@@ -6,5 +8,10 @@
 #
 
 def odds_and_evens(string, return_odds)
-
+  array = string.chars
+  if return_odds == false
+    return array.values_at(* array.each_index.select(&:even?)).join.to_s
+  else
+    return array.values_at(* array.each_index.select(&:odd?)).join.to_s
+  end
 end
