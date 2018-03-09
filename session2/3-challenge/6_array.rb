23226c914ae7aec7b4b36eb708f31b4aa24b7361
diegofontecilla <31539191+diegofontecilla@users.noreply.github.com>
2018-03-09 13:07:38 +0000

    done

--- session2/3-challenge/6_array.rb
+++ session2/3-challenge/6_array.rb
@@ -15,3 +15,14 @@
 # prime_chars? ['a', 'bcd']       # => false
 # prime_chars? ['a', 'b', 'cd']   # => false
 
+def prime_chars?(strings_array)
+  sum = 0
+  new_array = []
+  strings_array.join.split("").each {|x| sum += 1}
+  if sum != 1 && (strings_array != [""] && strings_array != [])
+  	(2..sum/2).map {|a| new_array << sum % a }
+    !(new_array.include? 0) 
+  else
+  	false
+  end
+end
