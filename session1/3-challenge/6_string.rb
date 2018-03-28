acf03d9aa9371690dbecf7bec1b6a749bd1196a4
benji1304 <benjaminstokes@gmail.com>
2018-03-28 09:03:34 +0100

    1:6 done and merged up with upstream

--- session1/3-challenge/6_string.rb
+++ session1/3-challenge/6_string.rb
@@ -8,8 +8,8 @@
 def odds_and_evens(string, return_odds)
     # index_of ? gets the index then return odd index? 
     if return_odds == true
-        string.chars.select.each_with_index{ |_, i| i.odd? }
+        string.chars.select.each_with_index{ |_, i| i.odd? }.join
         elsif return_odds == false
-        string.chars.select.each_with_index{ |_, i| i.even? }
+        string.chars.select.each_with_index{ |_, i| i.even? }.join
       end
 end
