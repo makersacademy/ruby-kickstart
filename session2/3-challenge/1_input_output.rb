63503ddc8c1ae21ce6a11faf3d049e50849dc0e1
benji1304 <benjaminstokes@gmail.com>
2018-04-03 13:46:24 +0100

    2:1 works in repl.it Not in Rake. Needs reworking

--- session2/3-challenge/1_input_output.rb
+++ session2/3-challenge/1_input_output.rb
@@ -7,7 +7,10 @@
 # Standard input will be like "9 2\n" and will expect you to print
 # "11\n7\n18\n" to standard output.
 
-def sum_difference_product
+def sum_difference_product(a,b)
   # your code goes here
-  
+  # use .inspect to get /n like in examples ?
+  # use p as it puts + .inspect 
+  p (a+b).to_s + "\n" + (a-b).to_s +  "\n" + (a*b).to_s
+
 end
