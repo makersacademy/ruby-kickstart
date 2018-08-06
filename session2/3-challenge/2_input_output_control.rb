ff769dc89415fecdbfccc90c880847f9169ec9ab
Serena Hathi <hathiserena@gmail.com>
2018-08-06 14:11:31 +0200

    Exercise 2:2 complete

--- session2/3-challenge/2_input_output_control.rb
+++ session2/3-challenge/2_input_output_control.rb
@@ -19,8 +19,12 @@
 # and when you think it is correct, you can test it with  $ rake 2:2
 
 def hi_hi_goodbye
-  # your code here
-
+  while true do
+    input = gets.chomp
+    puts "hi " * input.to_i
+    break if input == "bye"
+    end
+    puts "goodbye"
 end
 
 
