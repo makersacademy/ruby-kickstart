ec32dda55f702c64b750323c5dda785b173e2537
Samuel Culley <samueljamesculley@gmail.com>
2018-08-13 13:08:01 +0000

    seventh problem finished

--- session1/3-challenge/7_string.rb
+++ session1/3-challenge/7_string.rb
@@ -5,5 +5,14 @@
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
 def pirates_say_arrrrrrrrr(string)
-
+    result = ""
+    array = string.split("")
+    array.each_index do |i|
+        if array[i].downcase == "r"
+            unless array[i+1].nil?
+                result += array[i+1]
+            end
+        end
+    end
+    return result
 end
