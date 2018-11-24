6e4010ec39e80c3ac3399e76cd05a0dacd9dbb69
Nikhil Vijayan <nkhilv@gmail.com>
2018-11-24 10:03:45 +0000

    2:2 done

--- session2/3-challenge/2_input_output_control.rb
+++ session2/3-challenge/2_input_output_control.rb
@@ -17,10 +17,19 @@
 
 # remember you can try your program out with              $ ruby 2_input_output_control.rb
 # and when you think it is correct, you can test it with  $ rake 2:2
+def prompt
+  puts "Enter a number"
+end
 
 def hi_hi_goodbye
   # your code here
-
+  prompt
+  while (line = gets) && (line.to_s !~ /bye/)
+    line.to_i.times {print 'hi '}
+    puts
+    prompt
+  end
+  puts "Goodbye!"
 end
 
 
