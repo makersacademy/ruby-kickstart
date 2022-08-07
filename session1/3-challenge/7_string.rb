bbbc3fe62b96319c9130f0f07d6fbf584b46c182
Philip White <phwhite89@gmail.com>
2022-08-07 15:26:14 +0100

    completed challenge 1:7.

--- session1/3-challenge/7_string.rb
+++ session1/3-challenge/7_string.rb
@@ -5,5 +5,24 @@
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
 def pirates_say_arrrrrrrrr(string)
+  string_split = string.split("")
+  new_array = []
 
+  # split characters from string.
+  # create new array for storing edited string.
+
+  string_split.each_with_index do |l, i|
+    # p "#{i}: #{l}" if l.include?("r")
+    new_array.push(string_split[i + 1]) if l =~ /[Rr]/
+  end
+
+  # find index of each string in split array.
+  # able to print index: letter.
+  # able to print index: letter if l.include?("r").
+  # can push letters to new_array if contain [rR].
+  # can push letters[index + 1] to new_array if contain [rR].
+
+  return new_array.join
 end
+
+p pirates_say_arrrrrrrrr("are you really learning Ruby?")
